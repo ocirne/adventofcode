6ca116e15633a8500bd86cf7f32aecc2405ce8a4
Enrico Pilz <kontakt@enricopilz.de>
2021-01-23 15:32:13 +0100

    py 2020/04

--- python/2015/04/day04.py
+++ python/2015/04/day04.py
@@ -2,45 +2,25 @@
 import hashlib
 
 
-def check(s):
-    return hashlib.md5(s.encode()).hexdigest().startswith(5 * '0')
+def check(s, zeros):
+    return hashlib.md5(s.encode()).hexdigest().startswith(zeros * '0')
 
 
-def run(base):
+def search(base, zeros):
+    """
+    >>> search('abcdef', 5)
+    609043
+    >>> search('pqrstuv', 5)
+    1048970
+    """
     i = 0
     while True:
-        if check(base + str(i)):
+        if check(base + str(i), zeros):
             return i
         i += 1
-
-
-assert run('abcdef') == 609043
-assert run('pqrstuv') == 1048970
-
-
-data = open('input', 'r').readline()
-print(run(data))
-
 
 
 if __name__ == '__main__':
-    print(part1('input'))
-    print(part2('input'))
-
-import hashlib
-
-
-def check(s):
-    return hashlib.md5(s.encode()).hexdigest().startswith(6 * '0')
-
-
-def run(base):
-    i = 0
-    while True:
-        if check(base + str(i)):
-            return i
-        i += 1
-
-
-data = open('input', 'r').readline()
-print(run(data))
+    data = open('input', 'r').readline()
+    print(search(data, 5))
+    print(search(data, 6))
