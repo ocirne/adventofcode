f4d25bd3d019e3de9f9cf95efaf1ccdbc5fb9d4e
Enrico Pilz <kontakt@enricopilz.de>
2021-01-23 18:53:53 +0100

    py 2020/09

--- python/2015/09/day09.py
+++ python/2015/09/day09.py
@@ -19,46 +19,17 @@ def calc_distance(distances, route):
     return sum(distances[(route[i-1], route[i])] for i in range(1, len(route)))
 
 
-def run(filename):
+def run(filename, fun):
+    """
+    >>> run('reference', min)
+    605
+    >>> run('reference', max)
+    982
+    """
     locations, distances = read_data(filename)
-    return min(calc_distance(distances, route) for route in permutations(locations))
-
-
-assert run('reference') == 605
-
-print(run('input'))
-
+    return fun(calc_distance(distances, route) for route in permutations(locations))
 
 
 if __name__ == '__main__':
-    print(part1('input'))
-    print(part2('input'))
-
-from itertools import permutations
-
-
-def read_data(filename):
-    f = open(filename, 'r')
-    locations = {}
-    distances = {}
-    for line in f.readlines():
-        start, _, dest, _, distance = line.split()
-        locations[start] = True
-        locations[dest] = True
-        distances[(start, dest)] = int(distance)
-        distances[(dest, start)] = int(distance)
-    return list(locations.keys()), distances
-
-
-def calc_distance(distances, route):
-    return sum(distances[(route[i-1], route[i])] for i in range(1, len(route)))
-
-
-def run(filename):
-    locations, distances = read_data(filename)
-    return max(calc_distance(distances, route) for route in permutations(locations))
-
-
-assert run('reference') == 982
-
-print(run('input'))
+    print(run('input', min))
+    print(run('input', max))
