d076f7f3b497d7a228f1de6bb24805adcaef1213
Enrico Pilz <kontakt@enricopilz.de>
2021-01-24 18:38:34 +0100

    py 2020/19

--- python/2020/19/day19.py
+++ python/2020/19/day19.py
@@ -1,15 +1,15 @@
-
 import re
+from pathlib import Path
 
 
-def readFile(filename):
+def read_file(filename):
     rules = {}
     messages = []
     f = open(filename)
     line = f.readline()
     while not line.isspace():
-        ruleNo, ruleDesc = line.split(':')
-        rules[ruleNo] = ruleDesc.strip().replace('"', '')
+        rule_no, rule_desc = line.split(':')
+        rules[rule_no] = rule_desc.strip().replace('"', '')
         line = f.readline()
     line = f.readline()
     while line:
@@ -18,57 +18,7 @@ def readFile(filename):
     return rules, messages
 
 
-def recCreateRegex(rules, no):
-    regex = ''
-    for token in rules[no].split(' '):
-        if token == '|':
-            regex += token
-        elif token.isalpha():
-            return token
-        else:
-            regex += recCreateRegex(rules, token)
-    return '(' + regex + ')'
-
-
-def createRegex(rules):
-    regex = recCreateRegex(rules, '0')
-    return r'^' + regex + '$'
-
-
-def run(filename):
-    rules, messages = readFile(filename)
-    pattern = createRegex(rules)
-    total = 0
-    for message in messages:
-        if re.match(pattern, message):
-            total += 1
-    return total
-
-
-assert run('reference_a') == 2
-
-print(run('input_a'))
-
-import re
-
-
-def readFile(filename):
-    rules = {}
-    messages = []
-    f = open(filename)
-    line = f.readline()
-    while not line.isspace():
-        ruleNo, ruleDesc = line.split(':')
-        rules[ruleNo] = ruleDesc.strip().replace('"', '')
-        line = f.readline()
-    line = f.readline()
-    while line:
-        messages.append(line.strip())
-        line = f.readline()
-    return rules, messages
-
-
-def recCreateRegex(rules, no, depth):
+def rec_create_regex(rules, no, depth):
     if depth > 4:
         return ''
     regex = ''
@@ -79,20 +29,26 @@ def recCreateRegex(rules, no, depth):
             return token
         else:
             if no == token:
-                regex += recCreateRegex(rules, token, depth + 1)
+                regex += rec_create_regex(rules, token, depth + 1)
             else:
-                regex += recCreateRegex(rules, token, 0)
+                regex += rec_create_regex(rules, token, 0)
     return '(' + regex + ')'
 
 
-def createRegex(rules):
-    regex = recCreateRegex(rules, '0', 0)
+def create_regex(rules):
+    regex = rec_create_regex(rules, '0', 0)
     return r'^' + regex + '$'
 
 
 def run(filename):
-    rules, messages = readFile(filename)
-    pattern = createRegex(rules)
+    """
+    >>> run(Path(__file__).parent / 'reference_a')
+    2
+    >>> run(Path(__file__).parent / 'reference_b')
+    12
+    """
+    rules, messages = read_file(filename)
+    pattern = create_regex(rules)
     total = 0
     for message in messages:
         if re.match(pattern, message):
@@ -100,6 +56,6 @@ def run(filename):
     return total
 
 
-assert run('reference_b') == 12
-
-print(run('input_b'))
+if __name__ == '__main__':
+    print(run('input_a'))
+    print(run('input_b'))
