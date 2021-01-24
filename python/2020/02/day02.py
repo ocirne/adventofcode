11084a25b82256ab1ba4796a4eaf1c248ce6383f
Enrico Pilz <kontakt@enricopilz.de>
2021-01-24 14:47:24 +0100

    py 2020/02

--- python/2020/02/day02.py
+++ python/2020/02/day02.py
@@ -1,49 +1,55 @@
-
 from collections import Counter
 
 
-def run(filename):
+def extract(line):
+    """
+    >>> extract('1-3 a: abcde')
+    (1, 3, 'a', 'abcde')
+    """
+    pw_range, pw_letter, pw_password = line.split()
+    first, second = map(int, pw_range.split('-'))
+    letter = pw_letter.split(':')[0]
+    return first, second, letter, pw_password
+
+
+def is_valid_part1(line):
+    """
+    >>> is_valid_part1('1-3 a: abcde')
+    True
+    >>> is_valid_part1('1-3 b: cdefg')
+    False
+    >>> is_valid_part1('2-9 c: ccccccccc')
+    True
+    """
+    first, second, letter, password = extract(line)
+    counter = Counter(password)
+    count_letter = int(counter[letter])
+    return first <= count_letter <= second
+
+
+def is_valid_part2(line):
+    """
+    >>> is_valid_part2('1-3 a: abcde')
+    True
+    >>> is_valid_part2('1-3 b: cdefg')
+    False
+    >>> is_valid_part2('2-9 c: ccccccccc')
+    False
+    """
+    first, second, letter, password = extract(line)
+    count = 0
+    if password[first - 1] == letter:
+        count += 1
+    if password[second - 1] == letter:
+        count += 1
+    return count == 1
+
+
+def run(filename, is_valid):
     data = open(filename, 'r').readlines()
-    total_valid = 0
-    for line in data:
-        range, letterw, password = line.split()
-        min, max = map(int, range.split('-'))
-        letter = letterw.split(':')[0]
-        counter = Counter(password)
-        count_letter = int(counter[letter])
-        if min <= count_letter <= max:
-            total_valid += 1
-    return total_valid
-
-
-assert run('reference') == 2
-
-print(run('input'))
-
-
+    return sum(is_valid(line) for line in data)
 
 
 if __name__ == '__main__':
-    print(part1('input'))
-    print(part2('input'))
-
-def run(filename):
-    data = open(filename, 'r').readlines()
-    total_valid = 0
-    for line in data:
-        range, letterw, password = line.split()
-        first, second = map(int, range.split('-'))
-        letter = letterw.split(':')[0]
-        count = 0
-        if password[first - 1] == letter:
-            count += 1
-        if password[second - 1] == letter:
-            count += 1
-        if count == 1:
-            total_valid += 1
-    return total_valid
-
-
-assert run('reference') == 1
-
-print(run('input'))
+    print(run('input', is_valid_part1))
+    print(run('input', is_valid_part2))
