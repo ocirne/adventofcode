736bf21d0b55bef06e6c65178af3598788d1ad61
Enrico Pilz <kontakt@enricopilz.de>
2021-01-24 16:38:44 +0100

    py 2020/05

--- python/2020/05/day05.py
+++ python/2020/05/day05.py
@@ -1,45 +1,36 @@
 
-def toInt(line):
-    binRepr = line \
+def to_int(line):
+    """
+    >>> to_int('FBFBBFFRLR')
+    357
+    >>> to_int('BFFFBBFRRR')
+    567
+    >>> to_int('FFFBBBFRRR')
+    119
+    >>> to_int('BBFFBBFRLL')
+    820
+    """
+    bin_repr = line \
         .replace('F', '0') \
         .replace('B', '1') \
         .replace('R', '1') \
         .replace('L', '0')
-    return int(binRepr, 2)
+    return int(bin_repr, 2)
 
 
-def run(filename):
-    f = open(filename, 'r')
-    lines = map(str.strip, f.readlines())
-    return max(toInt(line) for line in lines)
+def part1(lines):
+    return max(to_int(line) for line in lines)
 
 
-assert run('reference') == 820
-
-print(run('input'))
-
-
-
-if __name__ == '__main__':
-    print(part1('input'))
-    print(part2('input'))
-
-def toInt(seatId):
-    binRepr = seatId \
-        .replace('F', '0') \
-        .replace('B', '1') \
-        .replace('R', '1') \
-        .replace('L', '0')
-    return int(binRepr, 2)
-
-
-def run(filename):
-    f = open(filename, 'r')
-    lines = map(str.strip, f.readlines())
-    seats = sorted(toInt(line) for line in lines)
+def part2(lines):
+    seats = sorted(to_int(line) for line in lines)
     for i in range(1, len(seats)):
         if seats[i-1] != seats[i] - 1:
             return seats[i]-1
 
 
-print(run('input'))
+if __name__ == '__main__':
+    f = open('input', 'r')
+    inputData = list(map(str.strip, f.readlines()))
+    print(part1(inputData))
+    print(part2(inputData))
