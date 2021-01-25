9f166a5366bec8cd29d1c072cacd06702647f9ce
Enrico Pilz <kontakt@enricopilz.de>
2021-01-25 21:39:18 +0100

    py 2015/12

---
+++ python/2015/12/day12.py
@@ -0,0 +1,59 @@
+import json
+
+
+def traverse(node, ignore_red):
+    if isinstance(node, dict):
+        if ignore_red and 'red' in node.values():
+            return 0
+        # keys are never numeric
+        return sum(traverse(value, ignore_red) for value in node.values())
+    elif isinstance(node, list):
+        return sum(traverse(item, ignore_red) for item in node)
+    elif isinstance(node, int):
+        return node
+    else:
+        return 0
+
+
+def part1(data):
+    """
+    >>> part1('[1,2,3]')
+    6
+    >>> part1('{"a":2,"b":4}')
+    6
+    >>> part1('[[[3]]]')
+    3
+    >>> part1('{"a":{"b":4},"c":-1}')
+    3
+    >>> part1('{"a":[-1,1]}')
+    0
+    >>> part1('[-1,{"a":1}]')
+    0
+    >>> part1('[]')
+    0
+    >>> part1('{}')
+    0
+    """
+    root = json.loads(data)
+    return traverse(root, ignore_red=False)
+
+
+def part2(data):
+    """
+    >>> part2('[1,2,3]')
+    6
+    >>> part2('[1,{"c":"red","b":2},3]')
+    4
+    >>> part2('{"d":"red","e":[1,2,3,4],"f":5}')
+    0
+    >>> part2('[1,"red",5]')
+    6
+    """
+    root = json.loads(data)
+    return traverse(root, ignore_red=True)
+
+
+if __name__ == '__main__':
+    inputData = open('input', 'r').readline()
+    print(part1(inputData))
+    print(part2(inputData))
