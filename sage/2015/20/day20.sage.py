46d043ba028176c685ee36e4e20caee8a3cdeb22
Enrico Pilz <kontakt@enricopilz.de>
2021-01-29 21:02:12 +0100

    sage 2015/20

---
+++ sage/2015/20/day20.sage.py
@@ -0,0 +1,25 @@
+
+from sage.all import *
+
+
+def count_part1(house):
+    return sum(divisors(house)) * 10
+
+
+def count_part2(house):
+    return sum(d for d in divisors(house) if house // d <= 50) * 11
+
+
+def run(target, count_presents):
+    house = 1
+    while True:
+        count = count_presents(house)
+        if count >= target:
+            return house
+        house += 1
+
+
+if __name__ == '__main__':
+    puzzleInput = int(open('input', 'r').readline())
+    print(run(puzzleInput, count_part1))
+    print(run(puzzleInput, count_part2))
