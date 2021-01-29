716a1a4b1c02cac03a64dea43e88912e92c6cbf7
Enrico Pilz <kontakt@enricopilz.de>
2021-01-29 19:44:28 +0100

    py 2020/17,18

---
+++ python/2015/17/day17.py
@@ -0,0 +1,42 @@
+from collections import Counter
+
+RESULTS = []
+
+
+def read_data(filename):
+    f = open(filename, 'r')
+    return sorted(int(line) for line in f.readlines())
+
+
+def combine(data, rest, acc=None):
+    if acc is None:
+        acc = []
+    if rest < 0:
+        return 0
+    if rest == 0:
+        RESULTS.append(len(acc))
+        return 1
+    if not data:
+        return 0
+    return combine(data[1:], rest, acc) + combine(data[1:], rest - data[0], acc + [data[0]])
+
+
+def part1(data, total):
+    """
+    >>> part1([5, 5, 10, 15, 20], 25)
+    4
+    >>> part2()
+    3
+    """
+    return combine(data, total)
+
+
+def part2():
+    counts = Counter(RESULTS)
+    return counts[min(counts.keys())]
+
+
+if __name__ == '__main__':
+    inputData = read_data('input')
+    print(part1(inputData, 150))
+    print(part2())
---
+++ python/2015/18/day18.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+
+def corners_on(stay_on, grid, size):
+    if stay_on:
+        grid[(0, 0)] = True
+        grid[(0, size-1)] = True
+        grid[(size-1, 0)] = True
+        grid[(size-1, size-1)] = True
+
+
+def read_data(filename):
+    grid = {}
+    f = open(filename, 'r')
+    for y, line in enumerate(f.readlines()):
+        for x, light in enumerate(line.strip()):
+            if light == '#':
+                grid[(x, y)] = True
+    return grid
+
+
+def count_neighbors(grid, x, y):
+    count = 0
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if dx == 0 and dy == 0:
+                continue
+            if (x+dx, y+dy) in grid:
+                count += 1
+    return count
+
+
+def step(before, size, stay_on):
+    after = {}
+    for x in range(size):
+        for y in range(size):
+            cn = count_neighbors(before, x, y)
+            if (x, y) in before and 2 <= cn <= 3:
+                after[(x, y)] = True
+            if cn == 3:
+                after[(x, y)] = True
+    corners_on(stay_on, after, size)
+    return after
+
+
+def run(filename, size, steps, stay_on):
+    """
+    >>> run(Path(__file__).parent / 'reference', 6, 4, stay_on=False)
+    4
+    >>> run(Path(__file__).parent / 'reference', 6, 5, stay_on=True)
+    17
+    """
+    data = read_data(filename)
+    corners_on(stay_on, data, size)
+    for _ in range(steps):
+        data = step(data, size, stay_on)
+    return len(data)
+
+
+if __name__ == '__main__':
+    print(run('input', 100, 100, stay_on=False))
+    print(run('input', 100, 100, stay_on=True))
