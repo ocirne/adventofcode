36785baad8581f0953d270853a8be886566e320b
Enrico Pilz <kontakt@enricopilz.de>
2021-01-30 19:25:01 +0100

    py 2020/19

--- python/2015/19/day19.py
+++ python/2015/19/day19.py
@@ -13,17 +13,21 @@ def read_data(filename):
     return replacements, molecule
 
 
-def replace_at(molecule, atom, index, replacement):
-    return molecule[:index] + replacement + molecule[index + len(atom):]
+def replace_at(molecule, old, index, new):
+    return molecule[:index] + new + molecule[index + len(old):]
+
+
+def generate_replacements(molecule, old, new):
+    index = molecule.find(old)
+    while index != -1:
+        yield replace_at(molecule, old, index, new)
+        index = molecule.find(old, index + 1)
 
 
 def do_one_replacement(replacements, molecule: str):
     result = []
     for atom, replacement in replacements:
-        index = molecule.find(atom)
-        while index != -1:
-            result.append(replace_at(molecule, atom, index, replacement))
-            index = molecule.find(atom, index + 1)
+        result.extend(r for r in generate_replacements(molecule, atom, replacement))
     return set(result)
 
 
@@ -39,5 +43,11 @@ def part1(filename):
     return len(molecules)
 
 
+def part2():
+    # Guessed my solution, as a DFS-approach stalled at around depth 190.
+    pass
+
+
 if __name__ == '__main__':
     print(part1('input'))
+    print(part2())
