5674069cbe6a8d11f3b3f29069f41765f5ee183b
Enrico Pilz <kontakt@enricopilz.de>
2021-02-10 21:55:38 +0100

    py 2020/14

---
+++ python/2020/14/day14.py
@@ -0,0 +1,81 @@
+import re
+from pathlib import Path
+
+
+def format_bin(x):
+    return '{0:36b}'.format(x)
+
+
+def part1(filename):
+    """
+    >>> part1(Path(__file__).parent / 'reference_a')
+    165
+    """
+    f = open(filename, 'r')
+    and_mask = or_mask = None
+    d = {}
+    for line in f.readlines():
+        if line.startswith('mask'):
+            mask = line.split(' ')[2]
+            and_mask = int(mask.replace('X', '1'), 2)
+            or_mask = int(mask.replace('X', '0'), 2)
+        else:
+            _, address, _, _, str_value = re.split(r'[\[\] ]', line)
+            value = int(str_value)
+            d[address] = (value & and_mask) | or_mask
+    return sum(d.values())
+
+
+def apply_mask(mask, address):
+    result = ''
+    for i in range(len(mask)):
+        if mask[i] == 'X':
+            result += 'X'
+        else:
+            if address[i] == ' ':
+                x = 0
+            else:
+                x = int(address[i])
+            result += str(int(mask[i]) | x)
+    return result
+
+
+def floating_masks(rest, acc=''):
+    if not rest:
+        yield acc
+    else:
+        head = rest[0]
+        if head == 'X':
+            for x in floating_masks(rest[1:], acc + '0'):
+                yield x
+            for x in floating_masks(rest[1:], acc + '1'):
+                yield x
+        else:
+            for x in floating_masks(rest[1:], acc + head):
+                yield x
+
+
+def part2(filename):
+    """
+    >>> part2(Path(__file__).parent / 'reference_b')
+    208
+    """
+    f = open(filename, 'r')
+    mask = None
+    d = {}
+    for line in f.readlines():
+        if line.startswith('mask'):
+            mask = line.split(' ')[2].strip()
+        else:
+            _, address, _, _, str_value = re.split(r'[\[\] ]', line)
+            value = int(str_value)
+            floating_mask = apply_mask(mask, format_bin(int(address)))
+            for fm in floating_masks(floating_mask):
+                d[fm] = value
+
+    return sum(d.values())
+
+
+if __name__ == '__main__':
+    print(part1('input'))
+    print(part2('input'))
