60f9cc588f05f4d9d15c853993aa8c1e72fef893
Enrico Pilz <kontakt@enricopilz.de>
2021-02-14 21:29:48 +0100

    Add most_common() trick

--- python/2017/07/day07.py
+++ python/2017/07/day07.py
@@ -44,19 +44,18 @@ def part1(filename):
     return get_root(tree).name
 
 
-def calc_total_weight(nodes, node: Node):
+def calc_correction_weight(nodes, node: Node):
     if node.children is None:
         node.total_weight = node.weight
         return
     for child in node.children:
-        result = calc_total_weight(nodes, nodes[child])
+        result = calc_correction_weight(nodes, nodes[child])
         if result is not None:
             return result
     children_total_weights = [nodes[child].total_weight for child in node.children]
-    counts = Counter(children_total_weights)
+    counts = Counter(children_total_weights).most_common()
     if len(counts) > 1:
-        good_weight = next(name for name, count in counts.items() if count > 1)
-        bad_weight = next(name for name, count in counts.items() if count == 1)
+        (good_weight, _), (bad_weight, _) = counts
         bad_child = next(child for child in node.children if nodes[child].total_weight == bad_weight)
         return nodes[bad_child].weight - (bad_weight - good_weight)
     node.total_weight = node.weight + sum(children_total_weights)
@@ -69,7 +68,7 @@ def part2(filename):
     """
     tree = read_tree(filename)
     root = get_root(tree)
-    return calc_total_weight(tree, root)
+    return calc_correction_weight(tree, root)
 
 
 if __name__ == '__main__':
