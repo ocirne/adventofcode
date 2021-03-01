f63eef501c97fa31e1a5c08470323d27b8a246ca
Enrico Pilz <kontakt@enricopilz.de>
2021-03-01 19:27:58 +0100

    py 2017/16

--- python/src/aoc/year2017/day16.py
+++ python/src/aoc/year2017/day16.py
@@ -17,13 +17,9 @@ def partner(s: str, x, y):
     return s
 
 
-def part1(lines, initial_standing="abcdefghijklmnop"):
-    """
-    >>> part1(load_example(__file__, "16"), "abcde")
-    'baedc'
-    """
-    standing = list(initial_standing)
-    for cmd in lines[0].strip().split(","):
+def dance(commands, initial_standing):
+    standing = initial_standing
+    for cmd in commands:
         #  print(line, standing)
         if cmd[0] == "s":
             #     print('s')
@@ -36,10 +32,44 @@ def part1(lines, initial_standing="abcdefghijklmnop"):
             standing = partner(standing, *cmd[1:].split("/"))
         else:
             raise
+    return standing
+
+
+def part1(lines, initial_standing="abcdefghijklmnop"):
+    """
+    >>> part1(load_example(__file__, "16"), "abcde")
+    'baedc'
+    """
+    commands = lines[0].strip().split(",")
+    standing = dance(commands, list(initial_standing))
     return "".join(standing)
+
+
+def find_loop(commands, initial_standing):
+    standing = list(initial_standing)
+    cache = {}
+    i = 0
+    while True:
+        s = "".join(standing)
+        if s in cache:
+            # works because cache[s] is 0
+            return cache, i
+        cache[s] = i
+        standing = dance(commands, standing)
+        i += 1
+
+
+def part2(lines, initial_standing="abcdefghijklmnop", count_dances=10 ** 9):
+    """
+    >>> part2(load_example(__file__, "16"), "abcde", 10)
+    'ceadb'
+    """
+    commands = lines[0].strip().split(",")
+    cache, s = find_loop(commands, initial_standing)
+    return list(cache.keys())[count_dances % s]
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2017, "16")
     print(part1(data))
-#    print(part1(load_example(__file__, "16"), "abcde"))
+    print(part2(data))
