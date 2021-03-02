7bf736780ba7d99056c071d8d080f0000b2e2822
Enrico Pilz <kontakt@enricopilz.de>
2021-03-02 21:47:39 +0100

    py 2017/18 pretty

--- python/src/aoc/year2017/day18.py
+++ python/src/aoc/year2017/day18.py
@@ -1,78 +1,15 @@
+from abc import ABC, abstractmethod
 from collections import defaultdict
 from queue import Queue
 
 from aoc.util import load_input, load_example
 
 
-class Solo:
+class Song(ABC):
     def __init__(self, program):
         self.program = program
         self.register = defaultdict(lambda: 0)
-
-    def value(self, t):
-        if t.isalpha():
-            return self.register[t]
-        else:
-            return int(t)
-
-    def instruction(self, pc):
-        return self.program[pc][0]
-
-    def values(self, pc):
-        return (t for t in self.program[pc][1:])
-
-    def execute(self):
-        pc = 0
-        sound = None
-        while True:
-            op = self.instruction(pc)
-            if op == "snd":
-                (x,) = self.values(pc)
-                sound = self.value(x)
-            elif op == "set":
-                x, y = self.values(pc)
-                self.register[x] = self.value(y)
-            elif op == "add":
-                x, y = self.values(pc)
-                self.register[x] += self.value(y)
-            elif op == "mul":
-                x, y = self.values(pc)
-                self.register[x] *= self.value(y)
-            elif op == "mod":
-                x, y = self.values(pc)
-                self.register[x] %= self.value(y)
-            elif op == "rcv":
-                (x,) = self.values(pc)
-                if self.value(x) != 0:
-                    return sound
-            elif op == "jgz":
-                x, y = self.values(pc)
-                if self.value(x) > 0:
-                    pc += self.value(y) - 1
-            else:
-                raise
-            pc += 1
-
-
-def part1(lines):
-    """
-    >>> part1(load_example(__file__, "18"))
-    4
-    """
-    program = [line.strip().split() for line in lines]
-    return Solo(program).execute()
-
-
-class Duet:
-    def __init__(self, program, pid):
-        self.program = program
-        self.register = defaultdict(lambda: 0)
-        self.pid = pid
-        self.register["p"] = pid
         self.pc = 0
-        self.msg_queue: Queue = Queue()
-        self.other = None
-        self.counter = 0
 
     def value(self, t):
         if t.isalpha():
@@ -86,13 +23,18 @@ class Duet:
     def values(self):
         return (t for t in self.program[self.pc][1:])
 
-    def execute_instruction(self):
-        waiting = None
+    @abstractmethod
+    def snd(self):
+        pass
+
+    @abstractmethod
+    def rcv(self):
+        pass
+
+    def step(self):
         op = self.instruction()
         if op == "snd":
-            (x,) = self.values()
-            self.counter += 1
-            self.other.msg_queue.put(self.value(x))
+            self.snd()
         elif op == "set":
             x, y = self.values()
             self.register[x] = self.value(y)
@@ -106,12 +48,9 @@ class Duet:
             x, y = self.values()
             self.register[x] %= self.value(y)
         elif op == "rcv":
-            (x,) = self.values()
-            if self.msg_queue.empty():
-                self.pc -= 1
-                waiting = True
-            else:
-                self.register[x] = self.msg_queue.get()
+            r = self.rcv()
+            if r:
+                return r
         elif op == "jgz":
             x, y = self.values()
             if self.value(x) > 0:
@@ -119,7 +58,55 @@ class Duet:
         else:
             raise
         self.pc += 1
-        return waiting
+
+
+class Solo(Song):
+    def __init__(self, program):
+        super(Solo, self).__init__(program)
+        self.sound = None
+
+    def snd(self):
+        (x,) = self.values()
+        self.sound = self.value(x)
+
+    def rcv(self):
+        (x,) = self.values()
+        if self.value(x) != 0:
+            return self.sound
+
+
+class Duet(Song):
+    def __init__(self, program, pid):
+        super(Duet, self).__init__(program)
+        self.register["p"] = pid
+        self.msg_queue: Queue = Queue()
+        self.other = None
+        self.counter = 0
+
+    def snd(self):
+        (x,) = self.values()
+        self.counter += 1
+        self.other.msg_queue.put(self.value(x))
+
+    def rcv(self):
+        (x,) = self.values()
+        if self.msg_queue.empty():
+            return True
+        else:
+            self.register[x] = self.msg_queue.get()
+
+
+def part1(lines):
+    """
+    >>> part1(load_example(__file__, "18"))
+    4
+    """
+    program = [line.strip().split() for line in lines]
+    solo = Solo(program)
+    while True:
+        sound = solo.step()
+        if sound:
+            return sound
 
 
 def part2(lines):
@@ -129,9 +116,9 @@ def part2(lines):
     duet0.other = duet1
     duet1.other = duet0
     while True:
-        wait0 = duet0.execute_instruction()
-        wait1 = duet1.execute_instruction()
-        if wait0 and wait1:
+        waiting0 = duet0.step()
+        waiting1 = duet1.step()
+        if waiting0 and waiting1:
             break
     return duet1.counter
 
