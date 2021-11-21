023ecd852c43cd958c1f6f9409f31f146c9a0bd8
Enrico Pilz <kontakt@enricopilz.de>
2021-11-21 16:28:24 +0100

    py 2016/10 putzen

--- python/src/aoc/year2016/day10.py
+++ python/src/aoc/year2016/day10.py
@@ -1,4 +1,3 @@
-import copy
 from collections import defaultdict
 
 from aoc.util import load_input, load_example
@@ -21,137 +20,81 @@ class Bot:
     def add_value(self, value):
         self.values.append(value)
 
-    def puts(self, bots, outputs, is_part1=False):
-        #        print(self.put_lower, self.put_higher, self.output_lower, self.output_higher)
-        if self.put_lower and self.output_lower:
-            raise Exception("lower put and output")
-        if self.put_higher and self.output_higher:
-            raise Exception("higher put and output")
-        if self.put_lower is None and self.output_lower is None:
-            raise Exception("lower put and output")
-        if self.put_higher is None and self.output_higher is None:
-            raise Exception("higher put and output")
-        #       print("index %s self.values %s" % (self.index, self.values))
+    def gives(self, bots, outputs, is_part1, mi_target, ma_target):
         mi = min(self.values)
         ma = max(self.values)
-        if is_part1 and mi == 17 and ma == 61:
+        if is_part1 and mi == mi_target and ma == ma_target:
             return True
         if self.put_lower is not None:
-            print("put value %s to bot %s" % (mi, self.put_lower))
             bots[self.put_lower].add_value(mi)
         if self.put_higher is not None:
-            print("put value %s to bot %s" % (ma, self.put_higher))
             bots[self.put_higher].add_value(ma)
         if self.output_lower is not None:
-            print("put value %s to output %s" % (mi, self.output_lower))
             outputs[self.output_lower].append(mi)
         if self.output_higher is not None:
-            print("put value %s to output %s" % (ma, self.output_higher))
             outputs[self.output_higher].append(ma)
         self.values.clear()
 
 
-#        return False
-
-
-def part1(rules):
-    bots = {}
-    outputs = defaultdict(list)
-    #    print('digraph x {')
-    for rule in rules:
-        m1 = input_pattern.match(rule)
-        if m1:
-            value1, bot1 = map(int, m1.groups())
-            if bot1 not in bots:
-                bots[bot1] = Bot()
+def load_bots(lines):
+    bots = defaultdict(Bot)
+    for rule in lines:
+        m = input_pattern.match(rule)
+        if m:
+            value1, bot1 = map(int, m.groups())
             bots[bot1].add_value(value1)
-        #            print("i%s -> %s" % (value1, bot1))
-        m2 = bot_bot_pattern.match(rule)
-        if m2:
-            bot2, low_bot2, high_bot2 = map(int, m2.groups())
-            if bot2 not in bots:
-                bots[bot2] = Bot()
+        m = bot_bot_pattern.match(rule)
+        if m:
+            bot2, low_bot2, high_bot2 = map(int, m.groups())
             bots[bot2].put_lower = low_bot2
             bots[bot2].put_higher = high_bot2
-        #            print("%s -> %s" % (bot2, low_bot2))
-        #            print("%s -> %s [style=dashed]" % (bot2, high_bot2))
-        m3 = output_output_pattern.match(rule)
-        if m3:
-            bot3, low_output3, high_output3 = map(int, m3.groups())
-            if bot3 not in bots:
-                bots[bot3] = Bot()
+        m = output_output_pattern.match(rule)
+        if m:
+            bot3, low_output3, high_output3 = map(int, m.groups())
             bots[bot3].output_lower = low_output3
             bots[bot3].output_higher = high_output3
-        #            print("%s -> { o%s o%s }" % (bot3, low_output3, high_output3))
-        m4 = output_bot_pattern.match(rule)
-        if m4:
-            bot4, low_output4, high_bot4 = map(int, m4.groups())
-            if bot4 not in bots:
-                bots[bot4] = Bot()
+        m = output_bot_pattern.match(rule)
+        if m:
+            bot4, low_output4, high_bot4 = map(int, m.groups())
             bots[bot4].output_lower = low_output4
             bots[bot4].put_higher = high_bot4
-    #            print("%s -> { o%s %s }" % (bot4, low_output4, high_bot4))
-    #    print('}')
+    return bots
 
+
+def simulate(bots, outputs, is_part1=False, mi_target=None, ma_target=None):
     changes = True
     while changes:
         changes = False
-        print("run")
         for i, b in bots.items():
-            #            if len(b.values) > 0:
-            #                print('Bot: %s has %s' % (i, b.values))
             if len(b.values) == 2:
-                print("Bot: %s" % i)
-                if b.puts(bots, outputs, is_part1=True):
-                    print("done")
+                if b.gives(bots, outputs, is_part1, mi_target, ma_target):
                     return i
                 changes = True
-    print("outputs", sorted(outputs.items()))
+    return outputs[0][0] * outputs[1][0] * outputs[2][0]
 
 
-#    print('sollte 2 herauskommen')
-
-
-def part2(rules):
-    bots = defaultdict(Bot)
+def part1(lines, mi_target=17, ma_target=61):
+    """
+    >>> part1(load_example(__file__, "10"), 2, 5)
+    2
+    """
+    bots = load_bots(lines)
     outputs = defaultdict(list)
-    for rule in rules:
-        m1 = input_pattern.match(rule)
-        if m1:
-            value1, bot1 = map(int, m1.groups())
-            bots[bot1].add_value(value1)
-        m2 = bot_bot_pattern.match(rule)
-        if m2:
-            bot2, low_bot2, high_bot2 = map(int, m2.groups())
-            bots[bot2].put_lower = low_bot2
-            bots[bot2].put_higher = high_bot2
-        m3 = output_output_pattern.match(rule)
-        if m3:
-            bot3, low_output3, high_output3 = map(int, m3.groups())
-            bots[bot3].output_lower = low_output3
-            bots[bot3].output_higher = high_output3
-        m4 = output_bot_pattern.match(rule)
-        if m4:
-            bot4, low_output4, high_bot4 = map(int, m4.groups())
-            bots[bot4].output_lower = low_output4
-            bots[bot4].put_higher = high_bot4
+    return simulate(bots, outputs, is_part1=True, mi_target=mi_target, ma_target=ma_target)
 
-    changes = True
-    while changes:
-        changes = False
-        print("run")
-        for i, b in bots.items():
-            if len(b.values) > 0:
-                print("Bot: %s has %s" % (i, b.values))
-            if len(b.values) == 2:
-                print("Action for Bot: %s" % i)
-                b.puts(bots, outputs)
-                changes = True
-    print("outputs", sorted(outputs.items()))
+
+def part2(lines):
+    """
+    >>> part2(load_example(__file__, "10"))
+    30
+    """
+    bots = load_bots(lines)
+    outputs = defaultdict(list)
+    simulate(bots, outputs)
+    return outputs[0][0] * outputs[1][0] * outputs[2][0]
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2016, "10")
-    #    data = load_example(__file__, '10')
-    #    print(part1(data))
+    print(part1(data))
     print(part2(data))
