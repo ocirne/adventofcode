10d7d7af8f9953f6f4c9d6dc7cdc8e216596f8d2
Enrico Pilz <kontakt@enricopilz.de>
2021-11-21 15:29:18 +0100

    py 2016/10b

--- python/src/aoc/year2016/day10.py
+++ python/src/aoc/year2016/day10.py
@@ -11,8 +11,7 @@ output_output_pattern = re.compile(r"bot (\d+) gives low to output (\d+) and hig
 
 
 class Bot:
-    def __init__(self, index):
-        self.index = index
+    def __init__(self):
         self.values = []
         self.put_lower = None
         self.put_higher = None
@@ -23,21 +22,32 @@ class Bot:
         self.values.append(value)
 
     def puts(self, bots, outputs, is_part1=False):
+        #        print(self.put_lower, self.put_higher, self.output_lower, self.output_higher)
         if self.put_lower and self.output_lower:
             raise Exception("lower put and output")
         if self.put_higher and self.output_higher:
             raise Exception("higher put and output")
-        print("index %s self.values %s" % (self.index, self.values))
-        if is_part1 and min(self.values) == 17 and max(self.values) == 61:
+        if self.put_lower is None and self.output_lower is None:
+            raise Exception("lower put and output")
+        if self.put_higher is None and self.output_higher is None:
+            raise Exception("higher put and output")
+        #       print("index %s self.values %s" % (self.index, self.values))
+        mi = min(self.values)
+        ma = max(self.values)
+        if is_part1 and mi == 17 and ma == 61:
             return True
-        if self.put_lower:
-            bots[self.put_lower].add_value(min(self.values))
-        if self.put_higher:
-            bots[self.put_higher].add_value(max(self.values))
-        if self.output_lower:
-            outputs[self.output_lower].append(min(self.values))
-        if self.output_higher:
-            outputs[self.output_higher].append(max(self.values))
+        if self.put_lower is not None:
+            print("put value %s to bot %s" % (mi, self.put_lower))
+            bots[self.put_lower].add_value(mi)
+        if self.put_higher is not None:
+            print("put value %s to bot %s" % (ma, self.put_higher))
+            bots[self.put_higher].add_value(ma)
+        if self.output_lower is not None:
+            print("put value %s to output %s" % (mi, self.output_lower))
+            outputs[self.output_lower].append(mi)
+        if self.output_higher is not None:
+            print("put value %s to output %s" % (ma, self.output_higher))
+            outputs[self.output_higher].append(ma)
         self.values.clear()
 
 
@@ -53,14 +63,14 @@ def part1(rules):
         if m1:
             value1, bot1 = map(int, m1.groups())
             if bot1 not in bots:
-                bots[bot1] = Bot(bot1)
+                bots[bot1] = Bot()
             bots[bot1].add_value(value1)
         #            print("i%s -> %s" % (value1, bot1))
         m2 = bot_bot_pattern.match(rule)
         if m2:
             bot2, low_bot2, high_bot2 = map(int, m2.groups())
             if bot2 not in bots:
-                bots[bot2] = Bot(bot2)
+                bots[bot2] = Bot()
             bots[bot2].put_lower = low_bot2
             bots[bot2].put_higher = high_bot2
         #            print("%s -> %s" % (bot2, low_bot2))
@@ -69,7 +79,7 @@ def part1(rules):
         if m3:
             bot3, low_output3, high_output3 = map(int, m3.groups())
             if bot3 not in bots:
-                bots[bot3] = Bot(bot3)
+                bots[bot3] = Bot()
             bots[bot3].output_lower = low_output3
             bots[bot3].output_higher = high_output3
         #            print("%s -> { o%s o%s }" % (bot3, low_output3, high_output3))
@@ -77,7 +87,7 @@ def part1(rules):
         if m4:
             bot4, low_output4, high_bot4 = map(int, m4.groups())
             if bot4 not in bots:
-                bots[bot4] = Bot(bot4)
+                bots[bot4] = Bot()
             bots[bot4].output_lower = low_output4
             bots[bot4].put_higher = high_bot4
     #            print("%s -> { o%s %s }" % (bot4, low_output4, high_bot4))
@@ -103,53 +113,38 @@ def part1(rules):
 
 
 def part2(rules):
-    bots = {}
+    bots = defaultdict(Bot)
     outputs = defaultdict(list)
-    #    print('digraph x {')
     for rule in rules:
         m1 = input_pattern.match(rule)
         if m1:
             value1, bot1 = map(int, m1.groups())
-            if bot1 not in bots:
-                bots[bot1] = Bot(bot1)
             bots[bot1].add_value(value1)
-        #            print("i%s -> %s" % (value1, bot1))
         m2 = bot_bot_pattern.match(rule)
         if m2:
             bot2, low_bot2, high_bot2 = map(int, m2.groups())
-            if bot2 not in bots:
-                bots[bot2] = Bot(bot2)
             bots[bot2].put_lower = low_bot2
             bots[bot2].put_higher = high_bot2
-        #            print("%s -> %s" % (bot2, low_bot2))
-        #            print("%s -> %s [style=dashed]" % (bot2, high_bot2))
         m3 = output_output_pattern.match(rule)
         if m3:
             bot3, low_output3, high_output3 = map(int, m3.groups())
-            if bot3 not in bots:
-                bots[bot3] = Bot(bot3)
             bots[bot3].output_lower = low_output3
             bots[bot3].output_higher = high_output3
-        #            print("%s -> { o%s o%s }" % (bot3, low_output3, high_output3))
         m4 = output_bot_pattern.match(rule)
         if m4:
             bot4, low_output4, high_bot4 = map(int, m4.groups())
-            if bot4 not in bots:
-                bots[bot4] = Bot(bot4)
             bots[bot4].output_lower = low_output4
             bots[bot4].put_higher = high_bot4
-    #            print("%s -> { o%s %s }" % (bot4, low_output4, high_bot4))
-    #    print('}')
 
     changes = True
     while changes:
         changes = False
         print("run")
         for i, b in bots.items():
-            #            if len(b.values) > 0:
-            #                print('Bot: %s has %s' % (i, b.values))
+            if len(b.values) > 0:
+                print("Bot: %s has %s" % (i, b.values))
             if len(b.values) == 2:
-                print("Bot: %s" % i)
+                print("Action for Bot: %s" % i)
                 b.puts(bots, outputs)
                 changes = True
     print("outputs", sorted(outputs.items()))
@@ -157,6 +152,6 @@ def part2(rules):
 
 if __name__ == "__main__":
     data = load_input(__file__, 2016, "10")
-    #   data = load_example(__file__, '10')
-    print(part1(data))
-#    print(part2(data))
+    #    data = load_example(__file__, '10')
+    #    print(part1(data))
+    print(part2(data))
