d29a3fb5fd6edfa4c3d20a4367b28fcfd3de7b7a
Enrico Pilz <kontakt@enricopilz.de>
2021-12-03 20:52:53 +0100

    py 2016/14a

---
+++ python/src/aoc/year2016/day14.py
@@ -0,0 +1,50 @@
+import hashlib
+from collections import defaultdict
+
+from aoc.util import load_input
+
+REP = {5 * hex(i)[2:]: 3 * hex(i)[2:] for i in range(16)}
+
+
+def find_three(md5_hash):
+    lowest_index = 100
+    for three in REP.values():
+        f = md5_hash.find(three)
+        if f >= 0:
+            if lowest_index > f:
+                result = three
+                lowest_index = f
+    if lowest_index == 100:
+        return None
+    return result
+
+
+def part1(lines):
+    """
+    >>> part1(['abc'])
+    22728
+    """
+    salt = lines[0].strip()
+    three_findings = defaultdict(list)
+    otp_keys = []
+    i = 0
+    while len(otp_keys) < 64:
+        md5_hash = hashlib.md5((salt + str(i)).encode()).hexdigest()
+        three = find_three(md5_hash)
+        if three is not None:
+            three_findings[three].append(i)
+        for f, t in REP.items():
+            if f in md5_hash:
+                for c in three_findings[t]:
+                    if 0 < i - c < 1001:
+                        # not clean, but works
+                        otp_keys.append(c)
+        i += 1
+    return otp_keys[63]
+
+
+if __name__ == "__main__":
+    data = load_input(__file__, 2016, "14")
+    assert part1(["abc"]) == 22728
+    print(part1(data))
+#    print(part2(data))
