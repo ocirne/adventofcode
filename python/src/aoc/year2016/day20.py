1b8db2db541dd974c327a51e46d3dab0a7c52930
Enrico Pilz <kontakt@enricopilz.de>
2021-12-09 21:44:43 +0100

    py 2016/20

--- python/src/aoc/year2016/day20.py
+++ python/src/aoc/year2016/day20.py
@@ -1,13 +1,12 @@
 from aoc.util import load_input, load_example
 
 
-def part1(lines):
-    """
-    >>> part1(load_example(__file__, "20"))
-    3
-    """
-    ranges = [tuple(map(int, line.split("-"))) for line in lines]
-    candidate = 0
+def to_ranges(lines):
+    return [tuple(map(int, line.split("-"))) for line in lines]
+
+
+def find_next_free(ranges, start_value=0):
+    candidate = start_value
     while True:
         next_candidates = [y for x, y in ranges if x <= candidate <= y]
         if not next_candidates:
@@ -15,7 +14,38 @@ def part1(lines):
         candidate = max(next_candidates) + 1
 
 
+def find_next_blocked(ranges, start_value):
+    next_candidates = [x for x, _ in ranges if start_value <= x]
+    if not next_candidates:
+        return None
+    return min(next_candidates)
+
+
+def part1(lines):
+    """
+    >>> part1(load_example(__file__, "20"))
+    3
+    """
+    return find_next_free(to_ranges(lines))
+
+
+def part2(lines, maximum=4294967295):
+    """
+    >>> part2(load_example(__file__, "20"), 9)
+    2
+    """
+    ranges = to_ranges(lines)
+    total = 0
+    next_blocked = 0
+    while True:
+        next_free = find_next_free(ranges, next_blocked)
+        next_blocked = find_next_blocked(ranges, next_free)
+        if next_blocked is None:
+            return total + (maximum - next_free + 1)
+        total += next_blocked - next_free
+
+
 if __name__ == "__main__":
     data = load_input(__file__, 2016, "20")
     print(part1(data))
-#    print(part2(data))
+    print(part2(data))
