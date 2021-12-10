7ff2c08d7da13041321bdef3552bc5fb57eaf453
Enrico Pilz <kontakt@enricopilz.de>
2021-12-10 12:24:29 +0100

    py 2021/10

--- python/src/aoc/year2021/day10.py
+++ python/src/aoc/year2021/day10.py
@@ -1,24 +1,40 @@
+from functools import reduce
+from statistics import median
+
 from aoc.util import load_input, load_example
 
-
-POINTS = {")": 3, "]": 57, "}": 1197, ">": 25137}
-
 BRACKETS = {"(": ")", "[": "]", "{": "}", "<": ">"}
+
+syntax_checker_score = {")": 3, "]": 57, "}": 1197, ">": 25137}
+autocomplete_single_score = {")": 1, "]": 2, "}": 3, ">": 4}
+
+
+def autocomplete_score(missing):
+    return reduce(lambda score, c: 5 * score + autocomplete_single_score[c], missing, 0)
 
 
 def match_brackets(expression):
     """
     >>> match_brackets('[({(<(())[]>[[{[]{<()<>>')
+    ('incomplete', '}}]])})]', 288957)
+    >>> match_brackets('[(()[<>])]({[<{<<[]>>(')
+    ('incomplete', ')}>]})', 5566)
     >>> match_brackets('{([(<{}[<>[]}>{[]{[(<()>')
-    '}'
+    ('invalid', '}', 1197)
+    >>> match_brackets('(((({<>}<{<{<>}{[]{[]{}')
+    ('incomplete', '}}>}>))))', 1480781)
     >>> match_brackets('[[<[([]))<([[{}[[()]]]')
-    ')'
+    ('invalid', ')', 3)
     >>> match_brackets('[{[{({}]{}}([{[{{{}}([]')
-    ']'
+    ('invalid', ']', 57)
+    >>> match_brackets('{<[[]]>}<{[{[{[]{()[[[]')
+    ('incomplete', ']]}}]}]}>', 995444)
     >>> match_brackets('[<(<(<(<{}))><([]([]()')
-    ')'
+    ('invalid', ')', 3)
     >>> match_brackets('<{([([[(<>()){}]>(<<{{')
-    '>'
+    ('invalid', '>', 25137)
+    >>> match_brackets('<{([{{}}[<[[[<>{}]]]>[]]')
+    ('incomplete', '])}>', 294)
     """
     stack = []
     for c in expression:
@@ -27,8 +43,15 @@ def match_brackets(expression):
         else:
             for opening, closing in BRACKETS.items():
                 if c == closing and stack[-1] != opening:
-                    return closing
+                    return "invalid", closing, syntax_checker_score[closing]
             stack.pop()
+    missing = "".join(BRACKETS[c] for c in reversed(stack))
+    return "incomplete", missing, autocomplete_score(missing)
+
+
+def handle_lines(lines):
+    for line in lines:
+        yield match_brackets(line.strip())
 
 
 def part1(lines):
@@ -36,17 +59,18 @@ def part1(lines):
     >>> part1(load_example(__file__, "10"))
     26397
     """
-    return sum(POINTS[c] for c in (match_brackets(line.strip()) for line in lines) if c is not None)
+    return sum(score for code, _, score in handle_lines(lines) if code == "invalid")
 
 
 def part2(lines):
     """
     >>> part2(load_example(__file__, "10"))
+    288957
     """
+    return median(score for code, _, score in handle_lines(lines) if code == "incomplete")
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2021, "10")
-    assert part1(load_example(__file__, "10")) == 26397
     print(part1(data))
-#    print(part2(data))
+    print(part2(data))
