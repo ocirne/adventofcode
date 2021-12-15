9dc538a425a75830d272a8d20d5185fec67e73e1
Enrico Pilz <kontakt@enricopilz.de>
2021-12-15 21:19:42 +0100

    py 2021/15

--- python/src/aoc/year2021/day15.py
+++ python/src/aoc/year2021/day15.py
@@ -11,23 +11,23 @@ def load_cave(lines):
     return cave
 
 
-def find_neighbors(cave, current_node):
-    x, y = current_node
+def find_neighbors(current_node, end_node):
+    ex, ey = end_node
+    cx, cy = current_node
     neighbors = []
-    if (x - 1, y) in cave:
-        neighbors.append((x - 1, y))
-    if (x + 1, y) in cave:
-        neighbors.append((x + 1, y))
-    if (x, y - 1) in cave:
-        neighbors.append((x, y - 1))
-    if (x, y + 1) in cave:
-        neighbors.append((x, y + 1))
+    if cx > 0:
+        neighbors.append((cx - 1, cy))
+    if cx < ex:
+        neighbors.append((cx + 1, cy))
+    if cy > 0:
+        neighbors.append((cx, cy - 1))
+    if cy < ex:
+        neighbors.append((cx, cy + 1))
     return neighbors
 
 
-def a_star(score):
+def a_star(score, end_node):
     start_node = (0, 0)
-    end_node = max(score)
     open_heap = []
     closed_set = set()
     parent = {}
@@ -35,45 +35,56 @@ def a_star(score):
 
     heappush(open_heap, (0, start_node))
     while open_heap:
-        print("open:", len(open_heap), "closed:", len(closed_set))
         current_node = heappop(open_heap)[1]
-
         if current_node == end_node:
             length = 0
             while current_node in parent:
-                print(score[current_node], current_node)
-                length += score[current_node]
+                length += score(*current_node)
                 current_node = parent[current_node]
-            print("return", length)
             return length
-
         closed_set.add(current_node)
         # expand node
-        for neighbor in find_neighbors(score, current_node):
+        for neighbor in find_neighbors(current_node, end_node):
             if neighbor in closed_set:
                 continue
-            tentative_g = g[current_node] + score[neighbor]
+            tentative_g = g[current_node] + score(*neighbor)
             if neighbor in open_heap and tentative_g >= g[neighbor]:
                 continue
-
             if tentative_g < g.get(neighbor, 0) or neighbor not in [i[1] for i in open_heap]:
                 parent[neighbor] = current_node
                 g[neighbor] = tentative_g
                 heappush(open_heap, (tentative_g, neighbor))
 
 
+def expand_score(cave, ex, ey):
+    return lambda x, y: ((cave[x % ex, y % ey] + x // ex + y // ey) - 1) % 9 + 1
+
+
 def part1(lines):
     """
-    >>> part1(load_example(__file__, "14"))
+    >>> part1(load_example(__file__, "15"))
     40
     """
     cave = load_cave(lines)
-    result = a_star(cave)
+    ex, ey = max(cave)
+    end_node = ex, ey
+    result = a_star(expand_score(cave, ex + 1, ey + 1), end_node)
+    return result
+
+
+def part2(lines):
+    """
+    >>> part2(load_example(__file__, "15"))
+    315
+    """
+    cave = load_cave(lines)
+    ex, ey = max(cave)
+    end_node = 5 * (ex + 1) - 1, 5 * (ey + 1) - 1
+    result = a_star(expand_score(cave, ex + 1, ey + 1), end_node)
     return result
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2021, "15")
-    assert part1(load_example(__file__, "15")) == 40
     print(part1(data))
-#    print(part2(data))
+    print(part2(data))
