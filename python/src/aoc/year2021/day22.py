d47db48e5cb744f3c7207a3fbd30fbaaf0a0c6c0
Enrico Pilz <kontakt@enricopilz.de>
2021-12-21 21:20:56 +0100

    py 2021/2x templates

---
+++ python/src/aoc/year2021/day22.py
@@ -0,0 +1,25 @@
+from aoc.util import load_input, load_example
+
+
+def part1(lines):
+    """
+    >>> part1(load_example(__file__, "22"))
+
+    """
+    ...
+
+
+def part2(lines):
+    """
+    >>> part2(load_example(__file__, "22"))
+
+    """
+    ...
+
+
+if __name__ == "__main__":
+    assert part1(load_example(__file__, "22")) == 0
+    assert part2(load_example(__file__, "22")) == 0
+    data = load_input(__file__, 2021, "22")
+    print(part1(data))
+    print(part2(data))
