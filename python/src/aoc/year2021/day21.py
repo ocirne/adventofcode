fd7da40e04e48ce4eca39b6ed46bfe115525a089
Enrico Pilz <kontakt@enricopilz.de>
2021-12-21 18:53:48 +0100

    py 2021/21 clean up

--- python/src/aoc/year2021/day21.py
+++ python/src/aoc/year2021/day21.py
@@ -4,6 +4,20 @@ from itertools import product
 from aoc.util import load_input, load_example
 
 
+def mod1(x, m):
+    """
+    >>> mod1(1, 10)
+    1
+    >>> mod1(10, 10)
+    10
+    >>> mod1(11, 10)
+    1
+    >>> mod1(15, 10)
+    5
+    """
+    return (x - 1) % m + 1
+
+
 def part1(lines):
     """
     >>> part1(load_example(__file__, "21"))
@@ -11,43 +25,28 @@ def part1(lines):
     """
     p1, p2 = (int(line.split()[4]) for line in lines)
     score1, score2 = 0, 0
-    print(p1, p2)
     dice = 1
     total_roll = 0
     while True:
-        print(p1, p2)
         for _ in range(3):
-            print("dice", dice)
-            p1 += dice
-            while p1 > 10:
-                p1 -= 10
-            dice += 1
+            p1 = mod1(p1 + dice, 10)
+            dice = mod1(dice + 1, 100)
             total_roll += 1
-            if dice > 100:
-                dice = 1
         score1 += p1
         if score1 >= 1000:
-            print("player 1 wins with score", score1)
             return total_roll * score2
 
         for _ in range(3):
-            print("dice", dice)
-            p2 += dice
-            while p2 > 10:
-                p2 -= 10
-            dice += 1
+            p2 = mod1(p2 + dice, 10)
+            dice = mod1(dice + 1, 100)
             total_roll += 1
-            if dice > 100:
-                dice = 1
         score2 += p2
         if score2 >= 1000:
-            print("player 2 wins")
             return total_roll * score1
 
 
-def sums():
-    a = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
-    return Counter(sum(p) for p in product(*a))
+def counts_per_round():
+    return Counter(sum(p) for p in product([1, 2, 3], repeat=3))
 
 
 def part2(lines):
@@ -56,52 +55,39 @@ def part2(lines):
     444356092776315
     """
     p1, p2 = (int(line.split()[4]) for line in lines)
-    # Wie oft kann welche Position erreicht werden, und welchen Score hat man dort?
-    p11 = defaultdict(lambda: 0)
-    p11[p1, 0, p2, 0] = 1
-    c = sums()
-    print(c)
+    state = defaultdict(lambda: 0)
+    state[p1, 0, p2, 0] = 1
+    counts = counts_per_round()
     total1 = 0
     total2 = 0
-    while p11:
-        print(len(p11), sum(p11.values()))
-
+    while state:
         tmp = defaultdict(lambda: 0)
-        for (p1, s1, p2, s2), count1 in p11.items():
-            for hop, count2 in c.items():
-                next_position = p1 + hop
-                while next_position > 10:
-                    next_position -= 10
+        for (p1, s1, p2, s2), count1 in state.items():
+            for hop, count2 in counts.items():
+                next_position = mod1(p1 + hop, 10)
                 next_score = s1 + next_position
                 cases = count1 * count2
                 if next_score >= 21:
                     total1 += cases
                 else:
                     tmp[next_position, next_score, p2, s2] += cases
-        p11 = tmp
-        print(len(p11), sum(p11.values()))
+        state = tmp
 
         tmp = defaultdict(lambda: 0)
-        for (p1, s1, p2, s2), count1 in p11.items():
-            for hop, count2 in c.items():
-                next_position = p2 + hop
-                while next_position > 10:
-                    next_position -= 10
+        for (p1, s1, p2, s2), count1 in state.items():
+            for hop, count2 in counts.items():
+                next_position = mod1(p2 + hop, 10)
                 next_score = s2 + next_position
                 cases = count1 * count2
                 if next_score >= 21:
                     total2 += cases
                 else:
                     tmp[p1, s1, next_position, next_score] += cases
-        p11 = tmp
-    print(total1, total2)
-    print(444356092776315, 341960390180808, "ref")
+        state = tmp
     return max(total1, total2)
 
 
 if __name__ == "__main__":
-    #    assert part1(load_example(__file__, "21")) == 739785
-    assert part2(load_example(__file__, "21")) == 444356092776315
     data = load_input(__file__, 2021, "21")
-    #    print(part1(data))
+    print(part1(data))
     print(part2(data))
