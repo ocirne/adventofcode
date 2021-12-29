f8f553c5d5ca9e42880a9e04461825602d4607fc
Enrico Pilz <kontakt@enricopilz.de>
2021-12-29 19:55:04 +0100

    py 2021/25

--- python/src/aoc/year2021/day25.py
+++ python/src/aoc/year2021/day25.py
@@ -1,25 +1,63 @@
+from collections import defaultdict
+
 from aoc.util import load_input, load_example
+
+
+def step(old_cucumbers, width, height):
+    next_cucumbers = defaultdict(lambda: ".")
+    changes = 0
+    for y in range(height):
+        for x in range(width):
+            value = old_cucumbers[x, y]
+            if value == ">":
+                if old_cucumbers[(x + 1) % width, y] == ".":
+                    next_cucumbers[(x + 1) % width, y] = ">"
+                    changes += 1
+                else:
+                    next_cucumbers[x, y] = ">"
+            elif value == "v":
+                next_cucumbers[x, y] = value
+
+    next_cucumbers2 = defaultdict(lambda: ".")
+    for y in range(height):
+        for x in range(width):
+            value = next_cucumbers[x, y]
+            if value == "v":
+                if next_cucumbers[x, (y + 1) % height] == ".":
+                    next_cucumbers2[x, (y + 1) % height] = "v"
+                    changes += 1
+                else:
+                    next_cucumbers2[x, y] = "v"
+            elif value == ">":
+                next_cucumbers2[x, y] = value
+
+    return changes, next_cucumbers2
 
 
 def part1(lines):
     """
-    >>> part1(load_example(__file__, "25"))
-
+    >>> part1(load_example(__file__, "25b"))
+    58
     """
-    ...
+    cucumbers = defaultdict(lambda: ".")
+    width = len(lines[0].strip())
+    height = len(lines)
+    for y, line in enumerate(lines):
+        for x, value in enumerate(line.strip()):
+            cucumbers[x, y] = value
+    steps = 1
+    while True:
+        changes, cucumbers = step(cucumbers, width, height)
+        if changes == 0:
+            return steps
+        steps += 1
 
 
 def part2(lines):
-    """
-    >>> part2(load_example(__file__, "25"))
-
-    """
     ...
 
 
 if __name__ == "__main__":
-    assert part1(load_example(__file__, "25")) == 0
-    assert part2(load_example(__file__, "25")) == 0
+    assert part1(load_example(__file__, "25b")) == 58
     data = load_input(__file__, 2021, "25")
     print(part1(data))
-    print(part2(data))
