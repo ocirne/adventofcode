3ca4fee922ebe6068d447ed44baa1a376bcb3fd6
Enrico Pilz <kontakt@enricopilz.de>
2022-01-02 21:58:50 +0100

    py 2021/19b

--- python/src/aoc/year2021/day19.py
+++ python/src/aoc/year2021/day19.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 from aoc.util import load_input, load_example
 
 
@@ -61,26 +63,30 @@ def find_overlap(cube1, cube2):
             transformed_cube2 = transform(cube2, dx, dy, dz)
             beacons = set(cube1).intersection(set(transformed_cube2))
             if len(beacons) >= 12:
-                print(dx, dy, dz)
-                print(beacons)
-                return transformed_cube2
-    return None
+                return transformed_cube2, (dx, dy, dz)
+    return None, None
 
 
 def solve(fixed_cube, rotated_cubes):
     known_cubes = [fixed_cube]
     open_list = rotated_cubes
+    scanners = []
     while open_list:
         for known in known_cubes:
             for rotations in open_list:
                 for r in rotations:
-                    transformed_cube = find_overlap(known, r)
+                    transformed_cube, scanner = find_overlap(known, r)
                     if transformed_cube is not None:
                         known_cubes.append(transformed_cube)
+                        scanners.append(scanner)
                         open_list.remove(rotations)
-    c = set().union(*known_cubes)
-    print(c)
-    return len(c)
+    return set().union(*known_cubes), scanners
+
+
+def find_all_beacons(lines):
+    base_cubes = read_data(lines)
+    fixed_cube, rotated_cubes = rotate_base_cubes(base_cubes)
+    return solve(fixed_cube, rotated_cubes)
 
 
 def part1(lines):
@@ -88,21 +94,25 @@ def part1(lines):
     >>> part1(load_example(__file__, "19"))
     79
     """
-    base_cubes = read_data(lines)
-    fixed_cube, rotated_cubes = rotate_base_cubes(base_cubes)
-    return solve(fixed_cube, rotated_cubes)
+    return len(find_all_beacons(lines)[0])
+
+
+def manhattan_distance(u, v):
+    return abs(u[0] - v[0]) + abs(u[1] - v[1]) + abs(u[2] - v[2])
 
 
 def part2(lines):
     """
     >>> part2(load_example(__file__, "19"))
-
+    3621
     """
-    ...
+    scanners = find_all_beacons(lines)[1]
+    return max(manhattan_distance(s1, s2) for s1, s2 in combinations(scanners, 2))
 
 
 if __name__ == "__main__":
     assert part1(load_example(__file__, "19")) == 79
+    assert part2(load_example(__file__, "19")) == 3621
     data = load_input(__file__, 2021, "19")
-    print(part1(data))
-#    print(part2(data))
+    #    print(part1(data))
+    print(part2(data))
