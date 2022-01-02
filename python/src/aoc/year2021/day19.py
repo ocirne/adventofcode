448d261fe3ad7bbbfef23da03468b572a6ca9187
Enrico Pilz <kontakt@enricopilz.de>
2022-01-02 21:46:44 +0100

    py 2021/19a

---
+++ python/src/aoc/year2021/day19.py
@@ -0,0 +1,108 @@
+from aoc.util import load_input, load_example
+
+
+def read_data(lines):
+    base_cubes = []
+    cube = []
+    for line in lines[1:]:
+        if not line.strip():
+            continue
+        elif "scanner" in line:
+            base_cubes.append(cube)
+            cube = []
+        else:
+            cube.append(tuple(map(int, line.split(","))))
+    base_cubes.append(cube)
+    return base_cubes
+
+
+# https://stackoverflow.com/questions/16452383/how-to-get-all-24-rotations-of-a-3-dimensional-array
+
+
+def roll(v):
+    return v[0], v[2], -v[1]
+
+
+def turn(v):
+    return -v[1], v[0], v[2]
+
+
+def rotations(v):
+    for _ in range(2):
+        for _ in range(3):
+            v = roll(v)
+            yield v
+            for _ in range(3):
+                v = turn(v)
+                yield v
+        v = roll(turn(roll(v)))
+
+
+def rotate_base_cubes(base_cubes):
+    rotated_cubes = []
+    for base_cube in base_cubes[1:]:
+        rotated_cube = [[] for _ in range(24)]
+        for xyz in base_cube:
+            for j, r in enumerate(rotations(xyz)):
+                rotated_cube[j].append(r)
+        rotated_cubes.append(rotated_cube)
+    # first cube is not rotated
+    return base_cubes[0], rotated_cubes
+
+
+def transform(cube, dx, dy, dz):
+    return [(x + dx, y + dy, z + dz) for x, y, z in cube]
+
+
+def find_overlap(cube1, cube2):
+    for x1, y1, z1 in cube1:
+        for x2, y2, z2 in cube2:
+            dx, dy, dz = x1 - x2, y1 - y2, z1 - z2
+            transformed_cube2 = transform(cube2, dx, dy, dz)
+            beacons = set(cube1).intersection(set(transformed_cube2))
+            if len(beacons) >= 12:
+                print(dx, dy, dz)
+                print(beacons)
+                return transformed_cube2
+    return None
+
+
+def solve(fixed_cube, rotated_cubes):
+    known_cubes = [fixed_cube]
+    open_list = rotated_cubes
+    while open_list:
+        for known in known_cubes:
+            for rotations in open_list:
+                for r in rotations:
+                    transformed_cube = find_overlap(known, r)
+                    if transformed_cube is not None:
+                        known_cubes.append(transformed_cube)
+                        open_list.remove(rotations)
+    c = set().union(*known_cubes)
+    print(c)
+    return len(c)
+
+
+def part1(lines):
+    """
+    >>> part1(load_example(__file__, "19"))
+    79
+    """
+    base_cubes = read_data(lines)
+    fixed_cube, rotated_cubes = rotate_base_cubes(base_cubes)
+    return solve(fixed_cube, rotated_cubes)
+
+
+def part2(lines):
+    """
+    >>> part2(load_example(__file__, "19"))
+
+    """
+    ...
+
+
+if __name__ == "__main__":
+    assert part1(load_example(__file__, "19")) == 79
+    data = load_input(__file__, 2021, "19")
+    print(part1(data))
+#    print(part2(data))
