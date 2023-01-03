2027ae1561a3272e482d745efa4bb378e142880f
Enrico Pilz <kontakt@enricopilz.de>
2023-01-03 22:09:09 +0100

    py 2022/6

--- python/src/aoc/year2022/day6.py
+++ python/src/aoc/year2022/day6.py
@@ -1,34 +1,47 @@
-from aoc.util import load_input, load_example
+from aoc.util import load_input
 
 
-def calorieSums(lines):
-    s = 0
-    for line in lines:
-        try:
-            s += int(line)
-        except ValueError:
-            yield s
-            s = 0
-    yield s
+def find_sop_marker(message, marker):
+    i = marker
+    while True:
+        if len(set(message[i - marker : i])) == marker:
+            return i
+        i += 1
 
 
 def part1(lines):
     """
-    >>> part1(load_example(__file__, "1"))
-    24000
+    >>> part1(["mjqjpqmgbljsphdztnvjfqwrcgsmlb"])
+    7
+    >>> part1(["bvwbjplbgvbhsrlpgdmjqwftvncz"])
+    5
+    >>> part1(["nppdvjthqldpwncqszvftbrmjlhg"])
+    6
+    >>> part1(["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"])
+    10
+    >>> part1(["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"])
+    11
     """
-    return max(calorieSums(lines))
+    return find_sop_marker(lines[0], 4)
 
 
 def part2(lines):
     """
-    >>> part2(load_example(__file__, "1"))
-    45000
+    >>> part2(["mjqjpqmgbljsphdztnvjfqwrcgsmlb"])
+    19
+    >>> part2(["bvwbjplbgvbhsrlpgdmjqwftvncz"])
+    23
+    >>> part2(["nppdvjthqldpwncqszvftbrmjlhg"])
+    23
+    >>> part2(["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"])
+    29
+    >>> part2(["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"])
+    26
     """
-    return sum(sorted(calorieSums(lines))[-3:])
+    return find_sop_marker(lines[0], 14)
 
 
 if __name__ == "__main__":
-    data = load_input(__file__, 2022, "1")
+    data = load_input(__file__, 2022, "6")
     print(part1(data))
     print(part2(data))
