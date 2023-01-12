a889bd6e6e9b53107a235a58cbc1aaf900ae898c
Enrico Pilz <kontakt@enricopilz.de>
2023-01-12 20:22:02 +0100

    Putzen

--- python/src/aoc/year2022/day15.py
+++ python/src/aoc/year2022/day15.py
@@ -36,37 +36,27 @@ def merge(segments):
         return [(segments[0][0], max(segments[0][1], rec[0][1]))] + rec[1:]
 
 
-# TODO https://en.wikipedia.org/wiki/Segment_tree
 def part1(lines, y=2_000_000):
     """
     >>> part1(load_example(__file__, "15"), y=10)
     26
     """
     segments = sorted(foo(lines, y))
-    #    print(segments)
     segments = merge(merge(segments))
-    #    print("----")
-    #    print(segments)
     total_length = sum(t - f for f, t in segments)
     # -1 for counting beacons by hand
     return total_length - 1
 
 
-# definitiv segment tree in 2d
 def part2(lines, m=4_000_000):
     """
     >>> part2(load_example(__file__, "15"), m=20)
     56000011
     """
     for y in range(m + 1):
-        print(y)
         segments = sorted(foo(lines, y))
-        #        print(segments)
         segments = merge(merge(merge(segments)))
-        #        print("----")
         if len(segments) > 1:
-            total_length = sum(t - f for f, t in segments)
-            print("y", y, segments, total_length)
             x = segments[0][1]
             return x * 4000000 + y
 
