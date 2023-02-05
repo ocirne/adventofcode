5897111412b99d2cb28bce92bc4f6f8903afa122
Enrico Pilz <kontakt@enricopilz.de>
2023-02-05 20:43:51 +0100

    py 2022/16 part2

--- python/src/aoc/year2022/day16.py
+++ python/src/aoc/year2022/day16.py
@@ -106,70 +106,152 @@ class Cave:
         return result
 
     def search3(self):
+        result = 0
         for limit in range(LIMIT2):
             best_flow = self.search4("AA", limit=limit)
             print(limit, best_flow)
             if self.all_are_open(best_flow[1]):
+                print("all open!")
                 return best_flow[2]
             self.expect[limit] = best_flow[0]
-        return self.expect[LIMIT2 - 1]
+            result = max(result, best_flow[2])
+        return result
 
     def all_are_open(self, opened):
         return self.interesting_valves == set(opened)
 
-    def search4(self, turn="me", me="AA", elephant="AA", opened=[], duration=0, limit=30, flow_rate=0, flow=0):
+    def search4(
+        self,
+        turn="me",
+        me="AA",
+        me_wait=0,
+        elephant="AA",
+        elephant_wait=0,
+        opened=[],
+        duration=0,
+        limit=30,
+        flow_rate=0,
+        flow=0,
+    ):
+        result = flow, opened, flow + (LIMIT2 - duration) * flow_rate
         if turn == "me":
-            result = (0, [])
+            if me_wait > 0:
+                return self.search4(
+                    turn="elephant",
+                    me=me,
+                    me_wait=me_wait - 1,
+                    elephant=elephant,
+                    elephant_wait=elephant_wait,
+                    opened=opened,
+                    duration=duration,
+                    limit=limit,
+                    flow_rate=flow_rate,
+                    flow=flow,
+                )
             if me not in opened and self.valves[me].flow_rate > 0:
+                # open valve
+                new_flow_rate = flow_rate + self.valves[me].flow_rate
                 result = max(
                     result,
                     self.search4(
-                        "elephant",
-                        me,
-                        elephant,
-                        opened + [me],
-                        duration,
-                        limit,
-                        flow_rate + self.valves[me].flow_rate,
-                        flow,
+                        turn="elephant",
+                        me=me,
+                        me_wait=0,
+                        elephant=elephant,
+                        elephant_wait=elephant_wait,
+                        opened=opened + [me],
+                        duration=duration,
+                        limit=limit,
+                        flow_rate=new_flow_rate,
+                        flow=flow,
                     ),
                 )
+            # visit neighbors
             for n_me in self.valves[me].neighbors:
-                result = max(result, self.search4("elephant", n_me, elephant, opened, duration, limit, flow_rate, flow))
+                result = max(
+                    result,
+                    self.search4(
+                        turn="elephant",
+                        me=n_me,
+                        me_wait=self.valves[me].neighbors[n_me] - 1,
+                        elephant=elephant,
+                        elephant_wait=elephant_wait,
+                        opened=opened,
+                        duration=duration,
+                        limit=limit,
+                        flow_rate=flow_rate,
+                        flow=flow,
+                    ),
+                )
             return result
         elif turn == "elephant":
-            result = (0, [])
+            if elephant_wait > 0:
+                return self.search4(
+                    turn="wrap",
+                    me=me,
+                    me_wait=me_wait,
+                    elephant=elephant,
+                    elephant_wait=elephant_wait - 1,
+                    opened=opened,
+                    duration=duration,
+                    limit=limit,
+                    flow_rate=flow_rate,
+                    flow=flow,
+                )
             if elephant not in opened and self.valves[elephant].flow_rate > 0:
+                # open valve
+                new_flow_rate = flow_rate + self.valves[elephant].flow_rate
                 result = max(
                     result,
                     self.search4(
-                        "wrap",
-                        me,
-                        elephant,
-                        opened + [elephant],
-                        duration,
-                        limit,
-                        flow_rate + self.valves[elephant].flow_rate,
-                        flow,
+                        turn="wrap",
+                        me=me,
+                        me_wait=me_wait,
+                        elephant=elephant,
+                        elephant_wait=0,
+                        opened=opened + [elephant],
+                        duration=duration,
+                        limit=limit,
+                        flow_rate=new_flow_rate,
+                        flow=flow,
                     ),
                 )
+            # visit neighbors
             for n_elephant in self.valves[elephant].neighbors:
-                result = max(result, self.search4("wrap", me, n_elephant, opened, duration, limit, flow_rate, flow))
+                result = max(
+                    result,
+                    self.search4(
+                        turn="wrap",
+                        me=me,
+                        me_wait=me_wait,
+                        elephant=n_elephant,
+                        elephant_wait=self.valves[elephant].neighbors[n_elephant] - 1,
+                        opened=opened,
+                        duration=duration,
+                        limit=limit,
+                        flow_rate=flow_rate,
+                        flow=flow,
+                    ),
+                )
             return result
         else:
+            if duration > limit:
+                return 0, [], 0
             if duration == limit or self.all_are_open(opened):
-                return flow, opened, flow + (LIMIT2 - duration) * flow_rate
-            if duration in self.expect and flow + 30 < self.expect[duration]:
-                return 0, []
+                return result
+            if duration in self.expect and flow + 50 < self.expect[duration]:
+                return result
             return self.search4(
-                "me",
-                me,
-                elephant,
-                opened,
-                duration + 1,
-                limit,
-                flow_rate,
-                flow + flow_rate,
+                turn="me",
+                me=me,
+                me_wait=me_wait,
+                elephant=elephant,
+                elephant_wait=elephant_wait,
+                opened=opened,
+                duration=duration + 1,
+                limit=limit,
+                flow_rate=flow_rate,
+                flow=flow + flow_rate,
             )
 
 
@@ -204,7 +286,7 @@ def part2(lines):
 
 
 if __name__ == "__main__":
-    #    data = load_input(__file__, 2022, "16")
-    data = load_example(__file__, "16")
-    print(part1(data))
-#    print(part2(data))
+    data = load_input(__file__, 2022, "16")
+    # data = load_example(__file__, "16")
+    #    print(part1(data))
+    print(part2(data))
