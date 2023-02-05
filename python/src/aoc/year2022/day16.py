a4d6262ce71d2eef8006c32b76934e326b8c28c2
Enrico Pilz <kontakt@enricopilz.de>
2023-02-05 22:29:07 +0100

    py 2022/16 refactor

--- python/src/aoc/year2022/day16.py
+++ python/src/aoc/year2022/day16.py
@@ -3,29 +3,25 @@ import re
 from aoc.util import load_input, load_example
 
 SCANNER_REGEX = re.compile(r"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)")
-LIMIT = 30
-LIMIT2 = 26
 
 
 class Valve:
-    def __init__(self, name: str, flow_rate: int, neighbors: list):
+    def __init__(self, name: str, flow_rate: int, neighbors: dict):
         self.name = name
         self.flow_rate = flow_rate
-        self.neighbors: dict = {n: 1 for n in neighbors}
+        self.neighbors = neighbors
 
     def __str__(self):
         return "%s (%s): %s" % (self.name, self.flow_rate, self.neighbors)
 
 
 def identify_candidate(valves):
-    for name, valve in valves.items():
-        if valve.flow_rate == 0 and len(valve.neighbors) == 2:
-            return name
+    return next((name for name, valve in valves.items() if valve.flow_rate == 0 and len(valve.neighbors) == 2), None)
 
 
 def simplify(valves: dict):
     """
-    Valve FF has flow rate=0; tunnels lead to valves EE, GG
+    "Valve FF has flow rate=0; tunnels lead to valves EE, GG"
     EE->(1)->FF->(1)->GG
     is the same as
     EE->(2)->GG
@@ -44,38 +40,52 @@ def simplify(valves: dict):
 
 
 class Cave:
-    def __init__(self, valves):
-        self.valves = valves
-        self.interesting_valves = set(name for name, valve in valves.items() if valve.flow_rate > 0)
+    def __init__(self, lines, minutes):
+        self.valves = self.initialize_valves(lines)
+        self.non_empty_valves = set(name for name, valve in self.valves.items() if valve.flow_rate > 0)
         self.expect = {}
+        self.minutes = minutes
 
-    def search2(self):
+    @staticmethod
+    def initialize_valves(lines):
+        valves = {}
+        for line in lines:
+            m = SCANNER_REGEX.match(line)
+            name, flow_rate, neighbors = m.groups()
+            valves[name] = Valve(name, int(flow_rate), {n: 1 for n in neighbors.split(", ")})
+        return simplify(valves)
+
+    def all_are_open(self, opened):
+        return self.non_empty_valves == set(opened)
+
+    def search_part1(self):
         result = 0
-        for limit in range(LIMIT):
-            best_flow = self.search("AA", limit=limit)
+        for limit in range(self.minutes):
+            best_flow = self.search(limit=limit)
             if self.all_are_open(best_flow[1]):
                 return best_flow[2]
-            print(limit, best_flow, "**")
             self.expect[limit] = best_flow[0]
             result = max(result, best_flow[2])
-        for key, value in self.expect.items():
-            print(key, value)
-        print("result", result)
+        return result
+
+    def search_part2(self):
+        result = 0
+        for limit in range(self.minutes):
+            best_flow = self.search4(limit=limit)
+            if self.all_are_open(best_flow[1]):
+                return best_flow[2]
+            self.expect[limit] = best_flow[0]
+            result = max(result, best_flow[2])
         return result
 
     def search(self, current="AA", opened=[], duration=0, limit=30, flow_rate=0, flow=0):
-        #        print(total_flow_rate, last_total_flow)
-        #        total_flow = last_total_flow + total_flow_rate
-        #        print("end, current, %s, opened: %s, total_flow: %s" % (current, opened, total_flow))
         if duration > limit:
             return 0, [], 0
         if duration == limit or self.all_are_open(opened):
-            #            print("end, current, %s, opened: %s, total_flow: %s" % (current, opened, total_flow))
-            #            print("end")
-            return flow, opened, flow + (LIMIT - duration - 1) * flow_rate
+            return flow, opened, flow + (self.minutes - duration - 1) * flow_rate
         if duration in self.expect and flow + 100 < self.expect[duration]:
-            return flow, opened, flow + (LIMIT - duration - 1) * flow_rate
-        result = flow, opened, flow + (LIMIT - duration - 1) * flow_rate
+            return flow, opened, flow + (self.minutes - duration - 1) * flow_rate
+        result = flow, opened, flow + (self.minutes - duration - 1) * flow_rate
         # open valve
         if current not in opened and self.valves[current].flow_rate > 0:
             new_flow_rate = flow_rate + self.valves[current].flow_rate
@@ -105,24 +115,9 @@ class Cave:
             )
         return result
 
-    def search3(self):
-        result = 0
-        for limit in range(LIMIT2):
-            best_flow = self.search4("AA", limit=limit)
-            print(limit, best_flow)
-            if self.all_are_open(best_flow[1]):
-                print("all open!")
-                return best_flow[2]
-            self.expect[limit] = best_flow[0]
-            result = max(result, best_flow[2])
-        return result
-
-    def all_are_open(self, opened):
-        return self.interesting_valves == set(opened)
-
     def search4(
         self,
-        turn="me",
+        turn="me",  # ??
         me="AA",
         me_wait=0,
         elephant="AA",
@@ -133,7 +128,7 @@ class Cave:
         flow_rate=0,
         flow=0,
     ):
-        result = flow, opened, flow + (LIMIT2 - duration) * flow_rate
+        result = flow, opened, flow + (self.minutes - duration - 1) * flow_rate
         if turn == "me":
             if me_wait > 0:
                 return self.search4(
@@ -260,15 +255,7 @@ def part1(lines):
     >>> part1(load_example(__file__, "16"))
     1651
     """
-    valves = {}
-    for line in lines:
-        m = SCANNER_REGEX.match(line)
-        name, flow_rate, neighbors = m.groups()
-        valves[name] = Valve(name, int(flow_rate), neighbors.split(", "))
-    cave = Cave(simplify(valves))
-    #    for valve in cave.valves.values():
-    #        print(valve)
-    return cave.search2()
+    return Cave(lines, 30).search_part1()
 
 
 def part2(lines):
@@ -276,17 +263,11 @@ def part2(lines):
     >>> part2(load_example(__file__, "16"))
     1707
     """
-    valves = {}
-    for line in lines:
-        m = SCANNER_REGEX.match(line)
-        name, flow_rate, neighbors = m.groups()
-        valves[name] = Valve(name, int(flow_rate), neighbors.split(", "))
-    cave = Cave(simplify(valves))
-    return cave.search3()
+    return Cave(lines, 26).search_part2()
 
 
 if __name__ == "__main__":
-    data = load_input(__file__, 2022, "16")
-    # data = load_example(__file__, "16")
-    #    print(part1(data))
+    #    data = load_input(__file__, 2022, "16")
+    data = load_example(__file__, "16")
+    print(part1(data))
     print(part2(data))
