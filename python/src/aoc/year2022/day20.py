453f5664aa47c762855a0e2813f885b94a57edad
Enrico Pilz <kontakt@enricopilz.de>
2023-02-21 15:12:17 +0100

    py 2022/20 part2

--- python/src/aoc/year2022/day20.py
+++ python/src/aoc/year2022/day20.py
@@ -34,12 +34,31 @@ def part1(lines):
 def part2(lines):
     """
     >>> part2(load_example(__file__, "20"))
-    .
+    1623178306
     """
+    values = list(enumerate(int(n) * 811589153 for n in lines))
+    m = len(values)
+    print([k for _, k in values])
+    for _ in range(10):
+        for i in range(m):
+            values = rotate(values, i)
+            print()
+            #        print(i, values)
+            item = values.pop(0)
+            t = (item[1]) % (m - 1)
+            values.insert(t, item)
+            print("Move value %s from %s to %s" % (item[1], 0, t))
+        #        print([k for _, k in values])
+    print("result", [k for _, k in values])
+    values = rotate(values, 0, 1)
+    v1 = values[(+1000) % m][1]
+    v2 = values[(+2000) % m][1]
+    v3 = values[(+3000) % m][1]
+    return v1 + v2 + v3
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2022, "20")
     #    data = load_example(__file__, "20")
-    print(part1(data))
-#    print(part2(data))
+    #    print(part1(data))
+    print(part2(data))
