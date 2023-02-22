4a26aa086662f2296504d97e43ed7521a9a40407
Enrico Pilz <kontakt@enricopilz.de>
2023-02-22 19:55:04 +0100

    py 2022/9 putzen

--- python/src/aoc/year2022/day9.py
+++ python/src/aoc/year2022/day9.py
@@ -4,27 +4,30 @@ from aoc.util import load_input, load_example
 directions = {"R": 1, "U": 1j, "L": -1, "D": -1j}
 
 
-def part1(lines):
-    """
-    >>> part1(load_example(__file__, "9a"))
-    .
-    """
-    h = 0j
-    t = 0j
-    visited = set()
-    visited.add(t)
+def simulate_rope(lines, knots):
+    rope = [0j for _ in range(knots)]
+    visited = {rope[knots - 1]}
     for line in lines:
         direction, count = line.split()
         for _ in range(int(count)):
-            h += directions[direction]
-            if abs(h - t) < 2:
-                continue
-            u = h - ((h - t) / abs(t - h))
-            r = complex(round(u.real), round(u.imag))
-            print("h", h, "t", t, "h-t", h - t, "u", u, "r", r, "-->", h - r)
-            t = r
-            visited.add(t)
+            rope[0] += directions[direction]
+            for i in range(1, knots):
+                h = rope[i - 1]
+                t = rope[i]
+                if abs(h - t) < 2:
+                    continue
+                u = h - ((h - t) / abs(h - t))
+                rope[i] = complex(round(u.real), round(u.imag))
+            visited.add(rope[knots - 1])
     return len(visited)
+
+
+def part1(lines):
+    """
+    >>> part1(load_example(__file__, "9a"))
+    13
+    """
+    return simulate_rope(lines, 2)
 
 
 def part2(lines):
@@ -32,26 +35,10 @@ def part2(lines):
     >>> part2(load_example(__file__, "9b"))
     36
     """
-    rope = [0j for _ in range(10)]
-    visited = set()
-    visited.add(rope[9])
-    for line in lines:
-        direction, count = line.split()
-        for _ in range(int(count)):
-            rope[0] += directions[direction]
-            for i in range(1, 10):
-                h = rope[i - 1]
-                t = rope[i]
-                if abs(h - t) < 2:
-                    continue
-                u = h - ((h - t) / abs(t - h))
-                rope[i] = complex(round(u.real), round(u.imag))
-            visited.add(rope[9])
-    return len(visited)
+    return simulate_rope(lines, 10)
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2022, "9")
-    #    data = load_example(__file__, "9b")
-    #    print(part1(data))
+    print(part1(data))
     print(part2(data))
