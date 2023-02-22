cde25c0ad23976fc9b17a427d2d0c905fb4a0b63
Enrico Pilz <kontakt@enricopilz.de>
2023-02-22 19:48:02 +0100

    py 2022/9 part2

--- python/src/aoc/year2022/day9.py
+++ python/src/aoc/year2022/day9.py
@@ -6,7 +6,7 @@ directions = {"R": 1, "U": 1j, "L": -1, "D": -1j}
 
 def part1(lines):
     """
-    >>> part1(load_example(__file__, "9"))
+    >>> part1(load_example(__file__, "9a"))
     .
     """
     h = 0j
@@ -29,14 +29,29 @@ def part1(lines):
 
 def part2(lines):
     """
-    >>> part2(load_example(__file__, "9"))
-    .
+    >>> part2(load_example(__file__, "9b"))
+    36
     """
-    ...
+    rope = [0j for _ in range(10)]
+    visited = set()
+    visited.add(rope[9])
+    for line in lines:
+        direction, count = line.split()
+        for _ in range(int(count)):
+            rope[0] += directions[direction]
+            for i in range(1, 10):
+                h = rope[i - 1]
+                t = rope[i]
+                if abs(h - t) < 2:
+                    continue
+                u = h - ((h - t) / abs(t - h))
+                rope[i] = complex(round(u.real), round(u.imag))
+            visited.add(rope[9])
+    return len(visited)
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2022, "9")
-    #    data = load_example(__file__, "9")
-    print(part1(data))
-#    print(part2(data))
+    #    data = load_example(__file__, "9b")
+    #    print(part1(data))
+    print(part2(data))
