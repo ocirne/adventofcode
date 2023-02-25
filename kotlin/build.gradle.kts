dd636b6d0b3105bc7f2417970be7fa219b40dc2d
Enrico Pilz <kontakt@enricopilz.de>
2023-02-25 14:34:02 +0100

    Update gradle, java, kotlin, lib versions

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.31"
+    kotlin("jvm") version "1.8.10"
     id("org.cyclonedx.bom") version "1.7.3"
 }
 
@@ -11,7 +11,7 @@ repositories {
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
@@ -22,13 +22,13 @@ tasks.withType<Test> {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    testImplementation(platform("org.junit:junit-bom:5.7.1"))
+    testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("io.kotest:kotest-runner-junit5:4.4.1")
-    testImplementation("io.kotest:kotest-assertions-core:4.4.1")
-    testImplementation("com.opencsv:opencsv:5.4")
-    testImplementation("org.jetbrains.kotlin:kotlin-reflect:1.4.31")
-    testImplementation("org.reflections:reflections:0.9.12")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+    testImplementation("com.opencsv:opencsv:5.7.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+    testImplementation("org.reflections:reflections:0.10.2")
 }
 
 configurations {
@@ -37,9 +37,9 @@ configurations {
     }
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks {
