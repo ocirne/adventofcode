179ab0ce676fb09d86b8b038f77da99e993a0fbd
Enrico Pilz <kontakt@enricopilz.de>
2023-03-04 21:07:47 +0100

    kt 2022/4 part2

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2022/Day4.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2022/Day4.kt
@@ -14,6 +14,11 @@ class Day4(val lines: List<String>) : AocChallenge(2022, 4) {
     }
 
     override fun part2(): Int {
-        return -1
+        return lines.count { line ->
+            val (first, second) = line.split(',')
+            val (f0, f1) = first.split('-').map { it.toInt() }
+            val (s0, s1) = second.split('-').map { it.toInt() }
+            (f1 >= s0 && f0 <= s1) || (f1 <= s0 && f0 >= s1)
+        }
     }
 }
