29fdeff0be21cebfcaafb51ce43a449834cfc5b9
Enrico Pilz <kontakt@enricopilz.de>
2023-03-04 23:46:57 +0100

    kt 2022/4 refactor

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2022/Day4.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2022/Day4.kt
@@ -4,20 +4,22 @@ import io.github.ocirne.aoc.AocChallenge
 
 class Day4(val lines: List<String>) : AocChallenge(2022, 4) {
 
+    private val regex = """(\d+)-(\d+),(\d+)-(\d+)""".toRegex()
+
+    private fun foo(line: String): List<Int> {
+        return regex.find(line)!!.destructured.toList().map { it.toInt() }
+    }
+
     override fun part1(): Int {
-        return lines.count { line ->
-            val (first, second) = line.split(',')
-            val (f0, f1) = first.split('-').map { it.toInt() }
-            val (s0, s1) = second.split('-').map { it.toInt() }
+        return lines.count {
+            val (f0, f1, s0, s1) = foo(it)
             (s0 <= f0 && f1 <= s1) || (f0 <= s0 && s1 <= f1)
         }
     }
 
     override fun part2(): Int {
-        return lines.count { line ->
-            val (first, second) = line.split(',')
-            val (f0, f1) = first.split('-').map { it.toInt() }
-            val (s0, s1) = second.split('-').map { it.toInt() }
+        return lines.count {
+            val (f0, f1, s0, s1) = foo(it)
             (f1 >= s0 && f0 <= s1) || (f1 <= s0 && f0 >= s1)
         }
     }
