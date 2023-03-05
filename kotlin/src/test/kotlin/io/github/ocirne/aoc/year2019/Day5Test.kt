c122a860865937f86fbeb9c7d6d249fddb9e4824
Enrico Pilz <kontakt@enricopilz.de>
2023-03-05 21:57:50 +0100

    kt 2019/5 tests

--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day5Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day5Test.kt
@@ -13,20 +13,34 @@ internal class Day5Test : AocTest(Day5::class, loadExample = false) {
         IntCodeEmulatorDay5("1101,100,-1,4,0").run(listOf()).toString() shouldBe "1101,100,-1,4,99"
     }
 
+    @Test
+    fun `examples year 2019 day 5 part 2`() {
+        // Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+        IntCodeEmulatorDay5("3,9,8,9,10,9,4,9,99,-1,8").run(listOf(8)).output shouldBe 1
+        IntCodeEmulatorDay5("3,9,8,9,10,9,4,9,99,-1,8").run(listOf(4)).output shouldBe 0
+        // Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+        IntCodeEmulatorDay5("3,9,7,9,10,9,4,9,99,-1,8").run(listOf(8)).output shouldBe 0
+        IntCodeEmulatorDay5("3,9,7,9,10,9,4,9,99,-1,8").run(listOf(4)).output shouldBe 1
+        // Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+        IntCodeEmulatorDay5("3,3,1108,-1,8,3,4,3,99").run(listOf(8)).output shouldBe 1
+        IntCodeEmulatorDay5("3,3,1108,-1,8,3,4,3,99").run(listOf(4)).output shouldBe 0
+        // Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+        IntCodeEmulatorDay5("3,3,1107,-1,8,3,4,3,99").run(listOf(8)).output shouldBe 0
+        IntCodeEmulatorDay5("3,3,1107,-1,8,3,4,3,99").run(listOf(4)).output shouldBe 1
 
-    3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
-    3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
-    3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
-    3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+        // position mode
+        IntCodeEmulatorDay5("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9").run(listOf(0)).output shouldBe 0
+        IntCodeEmulatorDay5("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9").run(listOf(42)).output shouldBe 1
+        // immediate mode
+        IntCodeEmulatorDay5("3,3,1105,-1,9,1101,0,0,12,4,12,99,1").run(listOf(0)).output shouldBe 0
+        IntCodeEmulatorDay5("3,3,1105,-1,9,1101,0,0,12,4,12,99,1").run(listOf(42)).output shouldBe 1
 
-    3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 (using position mode)
-    3,3,1105,-1,9,1101,0,0,12,4,12,99,1 (using immediate mode)
-
-    3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-    1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-    999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
-
-    The above example program uses an input instruction to ask for a single number. The program will then output 999 if the input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is greater than 8.
-
-
+        val largerExample = "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99"
+        // The program will then output 999 if the input value is below 8
+        IntCodeEmulatorDay5(largerExample).run(listOf(7)).output shouldBe 999
+        // output 1000 if the input value is equal to 8
+        IntCodeEmulatorDay5(largerExample).run(listOf(8)).output shouldBe 1000
+        // or output 1001 if the input value is greater than 8
+        IntCodeEmulatorDay5(largerExample).run(listOf(9)).output shouldBe 1001
+    }
 }
