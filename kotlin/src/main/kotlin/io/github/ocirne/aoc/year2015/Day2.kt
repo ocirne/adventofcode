09901adf8a26bbe9a318f8f691863650aab465c4
Enrico Pilz <kontakt@enricopilz.de>
2023-03-07 22:51:43 +0100

    kt 2015/2 fix

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2015/Day2.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2015/Day2.kt
@@ -4,7 +4,7 @@ import io.github.ocirne.aoc.AocChallenge
 
 class Day2(lines: List<String>) : AocChallenge(2015, 2) {
 
-    private val presents = lines.map { line -> line.split("x").map(String::toInt) }
+    private val presents = lines.map { line -> line.split("x").map(String::toInt).sorted() }
 
     override fun part1(): Int {
         return presents.sumOf { (h, l, w) -> 3 * h * l + 2 * h * w + 2 * l * w }
