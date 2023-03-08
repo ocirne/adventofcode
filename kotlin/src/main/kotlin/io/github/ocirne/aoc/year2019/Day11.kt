e7034df1d70797d4c5355a38413e5bb07e0d1570
Enrico Pilz <kontakt@enricopilz.de>
2023-03-08 20:28:33 +0100

    kt 2019/11 part2

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day11.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day11.kt
@@ -41,16 +41,58 @@ class Day11(val lines: List<String>) : AocChallenge(2019, 11) {
             } else {
                 throw IllegalStateException(direction.toString())
             }
-            println("robot output $color $direction :: d $d x $x y $y")
+//            println("robot output $color $direction :: d $d x $x y $y")
             robot.addInput(panels.getOrDefault(Pair(x, y), 0))
         }
         return panels.size
     }
 
     override fun part2(): Long {
-        TODO()
+        val robot = IntCodeEmulator2019(lines.first())
+        val panels = mutableMapOf<Pair<Int, Int>, Long>()
+        robot.addInput(1)
+        var d = 0
+        var x = 0
+        var y = 0
+        while (!robot.step()) {
+            val color = robot.getLastOutput()
+            panels[Pair(x, y)] = color
+            robot.step()
+            val direction = robot.getLastOutput()
+            if (direction == 0L) {
+                // turn left
+                d = (d + 3) % 4
+                val (dx, dy) = goForward(d, x, y)
+                x += dx
+                y += dy
+            } else if (direction == 1L) {
+                // turn right
+                d = (d + 1) % 4
+                val (dx, dy) = goForward(d, x, y)
+                x += dx
+                y += dy
+            } else {
+                throw IllegalStateException(direction.toString())
+            }
+//            println("robot output $color $direction :: d $d x $x y $y")
+            robot.addInput(panels.getOrDefault(Pair(x, y), 0))
+        }
+        val whiteKeys = panels.filter { (_, value) -> value == 1L }.keys
+        val x0 = whiteKeys.minOf { (y, x) -> x }
+        val y0 = whiteKeys.minOf { (y, x) -> y }
+        val x1 = whiteKeys.maxOf { (y, x) -> x }
+        val y1 = whiteKeys.maxOf { (y, x) -> y }
+        println("$x0 $y0 - $x1 $y1")
+        for (x in x0 .. x1) {
+            for (y in y0 .. y1) {
+                if (panels.getOrDefault(Pair(y, x), 0) == 1L) {
+                    print ('#')
+                } else {
+                    print(' ')
+                }
+            }
+            println()
+        }
+        return -1L
     }
 }
-
-
-// too high 9746
