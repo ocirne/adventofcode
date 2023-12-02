3083129e4384e030168b37a17a3fffd9e46475e5
Enrico Pilz <kontakt@enricopilz.de>
2023-12-02 23:01:37 +0100

    py 2023/2 putzen

--- python/src/aoc/year2023/day2.py
+++ python/src/aoc/year2023/day2.py
@@ -1,18 +1,24 @@
+from collections import defaultdict
+
 from aoc.util import load_input, load_example
 
+MAXIMA_PART1 = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
 
-def foo(game):
+
+def iter_colors(game):
     game_rounds = game.split(": ")[1].split(";")
     for game_round in game_rounds:
-        for count_name in game_round.split(","):
-            count, name = count_name.split()
-            if name == "red" and int(count) > 12:
-                return False
-            if name == "green" and int(count) > 13:
-                return False
-            if name == "blue" and int(count) > 14:
-                return False
-    return True
+        for count_color in game_round.split(","):
+            count, color = count_color.split()
+            yield int(count), color
+
+
+def find_valid_games(game):
+    return all(count <= MAXIMA_PART1[color] for count, color in iter_colors(game))
 
 
 def part1(lines):
@@ -20,26 +26,14 @@ def part1(lines):
     >>> part1(load_example(__file__, "2"))
     8
     """
-    total = 0
-    for id0, game in enumerate(lines):
-        if foo(game):
-            total += id0 + 1
-    return total
+    return sum(id0 + 1 for id0, game in enumerate(lines) if find_valid_games(game))
 
 
-def bar(game):
-    red = green = blue = 0
-    game_rounds = game.split(": ")[1].split(";")
-    for game_round in game_rounds:
-        for count_name in game_round.split(","):
-            count, name = count_name.split()
-            if name == "red":
-                red = max(red, int(count))
-            if name == "green":
-                green = max(green, int(count))
-            if name == "blue":
-                blue = max(blue, int(count))
-    return red * green * blue
+def find_minimum_count(game):
+    d = defaultdict(int)
+    for count, color in iter_colors(game):
+        d[color] = max(d[color], count)
+    return d["red"] * d["green"] * d["blue"]
 
 
 def part2(lines):
@@ -47,10 +41,7 @@ def part2(lines):
     >>> part2(load_example(__file__, "2"))
     2286
     """
-    total = 0
-    for id0, game in enumerate(lines):
-        total += bar(game)
-    return total
+    return sum(find_minimum_count(game) for id0, game in enumerate(lines))
 
 
 if __name__ == "__main__":
