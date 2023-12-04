6aa423a8aa1b370969f1000ff269845ab2c3dcc6
Enrico Pilz <kontakt@enricopilz.de>
2023-12-04 12:07:15 +0100

    py 2023/3 putzen

--- python/src/aoc/year2023/day3.py
+++ python/src/aoc/year2023/day3.py
@@ -12,38 +12,37 @@ def is_symbol(d, y, x):
     return not (c.isdigit() or c == ".")
 
 
-def detect_part_numbers(d):
+def detect_adjacent_symbol(d):
     part_number = ""
-    part_flag = False
+    symbol_positions = []
     for y in range(len(d)):
         for x in range(len(d[y])):
             c = d[y][x]
             if not part_number and c.isdigit():
                 # start of a number
-                if is_symbol(d, y - 1, x - 1) or is_symbol(d, y, x - 1) or is_symbol(d, y + 1, x - 1):
-                    part_flag = True
-                if is_symbol(d, y - 1, x) or is_symbol(d, y + 1, x):
-                    part_flag = True
+                symbol_positions.extend(
+                    (sy, sx)
+                    for sy, sx in ((y - 1, x - 1), (y, x - 1), (y + 1, x - 1), (y - 1, x), (y + 1, x))
+                    if is_symbol(d, sy, sx)
+                )
                 part_number += c
             elif part_number and c.isdigit():
                 # in a number
-                if is_symbol(d, y - 1, x) or is_symbol(d, y + 1, x):
-                    part_flag = True
+                symbol_positions.extend((sy, sx) for sy, sx in ((y - 1, x), (y + 1, x)) if is_symbol(d, sy, sx))
                 part_number += c
             elif part_number and not c.isdigit():
                 # end of a number
-                if is_symbol(d, y - 1, x) or is_symbol(d, y, x) or is_symbol(d, y + 1, x):
-                    part_flag = True
-                if part_flag:
-                    yield int(part_number)
+                symbol_positions.extend((sy, sx) for sy, sx in ((y - 1, x), (y, x), (y + 1, x)) if is_symbol(d, sy, sx))
+                if symbol_positions:
+                    yield int(part_number), symbol_positions
                 part_number = ""
-                part_flag = False
+                symbol_positions = []
         if part_number:
             # end of a number on a line
-            if part_flag:
-                yield int(part_number)
+            if symbol_positions:
+                yield int(part_number), symbol_positions
             part_number = ""
-            part_flag = False
+            symbol_positions = []
 
 
 def part1(lines):
@@ -51,50 +50,7 @@ def part1(lines):
     >>> part1(load_example(__file__, "3"))
     4361
     """
-    return sum(detect_part_numbers(lines))
-
-
-def is_gear(d, y, x):
-    if y < 0 or len(d) <= y:
-        return False
-    if x < 0 or len(d[y]) <= x:
-        return False
-    return d[y][x] == "*"
-
-
-def detect_gear_numbers(d):
-    part_number = ""
-    gear_pos = None
-    for y in range(len(d)):
-        for x in range(len(d[y])):
-            c = d[y][x]
-            if not part_number and c.isdigit():
-                # start of a number
-                for gy, gx in ((y - 1, x - 1), (y, x - 1), (y + 1, x - 1), (y - 1, x), (y + 1, x)):
-                    if is_gear(d, gy, gx):
-                        gear_pos = gy, gx
-                part_number += c
-            elif part_number and c.isdigit():
-                # in a number
-                for gy, gx in ((y - 1, x), (y + 1, x)):
-                    if is_gear(d, gy, gx):
-                        gear_pos = gy, gx
-                part_number += c
-            elif part_number and not c.isdigit():
-                # end of a number
-                for gy, gx in ((y - 1, x), (y, x), (y + 1, x)):
-                    if is_gear(d, gy, gx):
-                        gear_pos = gy, gx
-                if gear_pos is not None:
-                    yield int(part_number), gear_pos
-                part_number = ""
-                gear_pos = None
-        if part_number:
-            # end of a number on a line
-            if gear_pos is not None:
-                yield int(part_number), gear_pos
-            part_number = ""
-            gear_pos = None
+    return sum(n for n, _ in detect_adjacent_symbol(lines))
 
 
 def part2(lines):
@@ -103,8 +59,10 @@ def part2(lines):
     467835
     """
     m = defaultdict(list)
-    for n, gear_pos in detect_gear_numbers(lines):
-        m[gear_pos].append(n)
+    for n, symbol_positions in detect_adjacent_symbol(lines):
+        for gy, gx in symbol_positions:
+            if lines[gy][gx] == "*":
+                m[gy, gx].append(n)
     return sum(n[0] * n[1] for n in m.values() if len(n) == 2)
 
 
