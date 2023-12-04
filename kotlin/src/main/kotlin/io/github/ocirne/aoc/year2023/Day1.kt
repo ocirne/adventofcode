372018f58d510a4148d3da5b769c85339d8e3a1d
Enrico Pilz <kontakt@enricopilz.de>
2023-12-04 22:17:22 +0100

    kt 2023/1 part1

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2023/Day1.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2023/Day1.kt
@@ -5,7 +5,11 @@ import io.github.ocirne.aoc.AocChallenge
 class Day1(val lines: List<String>) : AocChallenge(2023, 1) {
 
     override fun part1(): Int {
-        return -1
+        return lines.sumOf { line ->
+            val first = line.first { it.isDigit() }.digitToInt()
+            val last = line.last { it.isDigit() }.digitToInt()
+            10 * first + last
+        }
     }
 
     override fun part2(): Int {
