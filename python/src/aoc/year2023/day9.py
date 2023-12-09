e00fca065d0c33c933595bbd555eb65f16362cb0
Enrico Pilz <kontakt@enricopilz.de>
2023-12-09 08:43:27 +0100

    py 2023/9 part2

--- python/src/aoc/year2023/day9.py
+++ python/src/aoc/year2023/day9.py
@@ -5,7 +5,6 @@ def foo(a):
     if all(n == 0 for n in a):
         return 0
     d = [s - p for p, s in zip(a[:-1], a[1:])]
-    print(d)
     return a[-1] + foo(d)
 
 
@@ -25,18 +24,40 @@ def analyze_pattern(line):
 def part1(lines):
     """
     >>> part1(load_example(__file__, "9"))
-    144
+    114
     """
     return sum(analyze_pattern(line) for line in lines)
+
+
+def bar(a):
+    if all(n == 0 for n in a):
+        return 0
+    d = [s - p for p, s in zip(a[:-1], a[1:])]
+    return a[0] - bar(d)
+
+
+def analyze_pattern2(line):
+    """
+    >>> analyze_pattern2('0 3 6 9 12 15')
+    -3
+    >>> analyze_pattern2('1 3 6 10 15 21')
+    0
+    >>> analyze_pattern2('10 13 16 21 30 45')
+    5
+    """
+    a = [int(s) for s in line.split()]
+    return bar(a)
 
 
 def part2(lines):
     """
     >>> part2(load_example(__file__, "9"))
+    2
     """
+    return sum(analyze_pattern2(line) for line in lines)
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2023, "9")
     print(part1(data))
-    # print(part2(data))
+    print(part2(data))
