bc2a97330995522c1a0e69f391fada547c16b9a5
Enrico Pilz <kontakt@enricopilz.de>
2023-12-09 09:14:49 +0100

    py 2023/8 putzen

--- python/src/aoc/year2023/day8.py
+++ python/src/aoc/year2023/day8.py
@@ -1,9 +1,27 @@
+from functools import reduce
+from math import lcm
+
 from aoc.util import load_input, load_example
 import re
 
-from sage.arith.functions import lcm
-
 NETWORK_PATTERN = r"(\w{3}) = \((\w{3}), (\w{3})\)"
+
+
+class GhostsMap:
+    def __init__(self, lines):
+        self.instructions = [0 if d == "L" else 1 for d in lines[0].strip()]
+        self.network = {}
+        for line in lines[2:]:
+            node, right, left = re.match(NETWORK_PATTERN, line).groups()
+            self.network[node] = (right, left)
+
+    def measure_path(self, start, end):
+        p = start
+        i = 0
+        while not p.endswith(end):
+            p = self.network[p][self.instructions[i % len(self.instructions)]]
+            i += 1
+        return i
 
 
 def part1(lines):
@@ -13,17 +31,18 @@ def part1(lines):
     >>> part1(load_example(__file__, "8b"))
     6
     """
-    instructions = [0 if d == "L" else 1 for d in lines[0].strip()]
-    network = {}
-    for line in lines[2:]:
-        node, right, left = re.match(NETWORK_PATTERN, line).groups()
-        network[node] = (right, left)
-    p = "AAA"
-    i = 0
-    while p != "ZZZ":
-        p = network[p][instructions[i % len(instructions)]]
-        i += 1
-    return i
+    m = GhostsMap(lines)
+    return m.measure_path("AAA", "ZZZ")
+
+
+def list_lcm(a):
+    """
+    >>> list_lcm([2, 3, 5])
+    30
+    >>> list_lcm([2, 3, 4])
+    12
+    """
+    return reduce(lcm, a)
 
 
 def part2(lines):
@@ -31,30 +50,12 @@ def part2(lines):
     >>> part2(load_example(__file__, "8c"))
     6
     """
-    instructions = [0 if d == "L" else 1 for d in lines[0].strip()]
-    network = {}
-    for line in lines[2:]:
-        node, right, left = re.match(NETWORK_PATTERN, line).groups()
-        network[node] = (right, left)
-
-    # identify pathes:
-    paths = []
-    for p in network.keys():
-        if p.endswith("A"):
-            paths.append(p)
-
-    result = []
-    for p in paths:
-        i = 0
-        while not p.endswith("Z"):
-            p = network[p][instructions[i % len(instructions)]]
-            i += 1
-        print(i)
-        result.append(i)
-    return lcm(result)
+    m = GhostsMap(lines)
+    starts = [p for p in m.network.keys() if p.endswith("A")]
+    return list_lcm(m.measure_path(s, "Z") for s in starts)
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2023, "8")
-    #    print(part1(data))
+    print(part1(data))
     print(part2(data))
