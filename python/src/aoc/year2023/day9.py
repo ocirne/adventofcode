caa7b4db6f6de627f19db9e31b4584f67147d57a
Enrico Pilz <kontakt@enricopilz.de>
2023-12-09 08:59:43 +0100

    py 2023/9 putzen

--- python/src/aoc/year2023/day9.py
+++ python/src/aoc/year2023/day9.py
@@ -1,24 +1,37 @@
 from aoc.util import load_input, load_example
 
 
-def foo(a):
+def next_value(a, d):
+    return a[-1] + rec(d, next_value)
+
+
+def previous_value(a, d):
+    return a[0] - rec(d, previous_value)
+
+
+def rec(a, f):
     if all(n == 0 for n in a):
         return 0
     d = [s - p for p, s in zip(a[:-1], a[1:])]
-    return a[-1] + foo(d)
+    return f(a, d)
 
 
-def analyze_pattern(line):
+def sum_adjacent_values(lines, f):
     """
-    >>> analyze_pattern('0 3 6 9 12 15')
+    >>> sum_adjacent_values(['0 3 6 9 12 15'], next_value)
     18
-    >>> analyze_pattern('1 3 6 10 15 21')
+    >>> sum_adjacent_values(['1 3 6 10 15 21'], next_value)
     28
-    >>> analyze_pattern('10 13 16 21 30 45')
+    >>> sum_adjacent_values(['10 13 16 21 30 45'], next_value)
     68
+    >>> sum_adjacent_values(['0 3 6 9 12 15'], previous_value)
+    -3
+    >>> sum_adjacent_values(['1 3 6 10 15 21'], previous_value)
+    0
+    >>> sum_adjacent_values(['10 13 16 21 30 45'], previous_value)
+    5
     """
-    a = [int(s) for s in line.split()]
-    return foo(a)
+    return sum(rec(list(map(int, line.split())), f) for line in lines)
 
 
 def part1(lines):
@@ -26,27 +39,7 @@ def part1(lines):
     >>> part1(load_example(__file__, "9"))
     114
     """
-    return sum(analyze_pattern(line) for line in lines)
-
-
-def bar(a):
-    if all(n == 0 for n in a):
-        return 0
-    d = [s - p for p, s in zip(a[:-1], a[1:])]
-    return a[0] - bar(d)
-
-
-def analyze_pattern2(line):
-    """
-    >>> analyze_pattern2('0 3 6 9 12 15')
-    -3
-    >>> analyze_pattern2('1 3 6 10 15 21')
-    0
-    >>> analyze_pattern2('10 13 16 21 30 45')
-    5
-    """
-    a = [int(s) for s in line.split()]
-    return bar(a)
+    return sum_adjacent_values(lines, next_value)
 
 
 def part2(lines):
@@ -54,7 +47,7 @@ def part2(lines):
     >>> part2(load_example(__file__, "9"))
     2
     """
-    return sum(analyze_pattern2(line) for line in lines)
+    return sum_adjacent_values(lines, previous_value)
 
 
 if __name__ == "__main__":
