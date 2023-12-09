d4d5dc78d25ee5a54e4b3d17975ab5b604cc8dfa
Enrico Pilz <kontakt@enricopilz.de>
2023-12-09 10:50:34 +0100

    Disable test

--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2023/Day1Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2023/Day1Test.kt
@@ -3,6 +3,7 @@ package io.github.ocirne.aoc.year2023
 import io.github.ocirne.aoc.AocTest
 import io.github.ocirne.aoc.loadExample
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class Day1Test: AocTest(Day1::class, loadExample = false) {
@@ -13,6 +14,8 @@ internal class Day1Test: AocTest(Day1::class, loadExample = false) {
         day1.part1() shouldBe 142
     }
 
+
+    @Disabled
     @Test
     fun `examples year 2023 day 1 part 2`() {
         val day1 = Day1(loadExample(2023, "1b"))
