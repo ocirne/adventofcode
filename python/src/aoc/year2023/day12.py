047b441de7282348b8986a6891c1b7191a30b1d5
Enrico Pilz <kontakt@enricopilz.de>
2023-12-12 22:29:50 +0100

    py 2023/12 part1

---
+++ python/src/aoc/year2023/day12.py
@@ -0,0 +1,67 @@
+from aoc.util import load_input, load_example
+
+
+def dot(springs, pattern, result):
+    return rec(springs[1:], pattern, result + ".")
+
+
+def spring(springs, pattern, result):
+    p = pattern[0]
+    if p > len(springs):
+        return 0
+    for i in range(p):
+        if springs[i] == ".":
+            return 0
+    if len(springs) > p and springs[p] == "#":
+        return 0
+    return rec(springs[p + 1 :], pattern[1:], result + p * "#" + ".")
+
+
+def rec(springs, pattern, result=""):
+    #    print('s %10s p %10s r %-10s' % (springs, pattern, result))
+    if all(s != "#" for s in springs) and not pattern:
+        print("**********", result, "************")
+        return 1
+    if springs == "" or not pattern:
+        return 0
+    total = 0
+    c = springs[0]
+    if c == ".":
+        total += dot(springs, pattern, result)
+    elif c == "#":
+        total += spring(springs, pattern, result)
+    elif c == "?":
+        total += dot(springs, pattern, result) + spring(springs, pattern, result)
+    return total
+
+
+def count_arrangements(springs, pattern):
+    p = [int(n) for n in pattern.split(",")]
+    #    print(springs)
+    #    print(p)
+    return rec(springs, p)
+
+
+def part1(lines):
+    """
+    >>> part1(load_example(__file__, "12"))
+    21
+    """
+    total = 0
+    for i, line in enumerate(lines):
+        springs, pattern = line.split()
+        tmp = count_arrangements(springs, pattern)
+        print(i, tmp)
+        total += tmp
+    return total
+
+
+def part2(lines):
+    ...
+
+
+if __name__ == "__main__":
+    data = load_input(__file__, 2023, "12")
+    #    data = load_example(__file__, "12")
+    print(part1(data))
+#   print(part2(data))
