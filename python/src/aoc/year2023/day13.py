2087d86f6963283bad85cd085a740a826c2b3195
Enrico Pilz <kontakt@enricopilz.de>
2023-12-13 19:04:18 +0100

    py 2023/13 part2

--- python/src/aoc/year2023/day13.py
+++ python/src/aoc/year2023/day13.py
@@ -35,9 +35,9 @@ def is_reflection_row(pattern, r):
     return True
 
 
-def count_reflected_rows(pattern):
+def count_reflected_rows(pattern, forbidden=None):
     for r in range(len(pattern)):
-        if is_reflection_row(pattern, r):
+        if is_reflection_row(pattern, r) and r + 1 != forbidden:
             return r + 1
     return 0
 
@@ -54,14 +54,45 @@ def part1(lines):
     return total
 
 
+def replacements(pattern):
+    w, h = len(pattern[0]), len(pattern)
+    d = {}
+    for y, row in enumerate(pattern):
+        for x, c in enumerate(row):
+            d[x, y] = c
+    for x in range(w):
+        for y in range(h):
+            o = d[x, y]
+            d[x, y] = "." if d[x, y] == "#" else "#"
+            result = ["".join(d[ix, iy] for ix in range(w)) for iy in range(h)]
+            d[x, y] = o
+            yield x, y, result
+
+
+def foo(pattern):
+    # print(pattern)
+    o_rows = count_reflected_rows(pattern)
+    o_cols = count_reflected_rows(transform_pattern(pattern))
+    for x, y, smudge_pattern in replacements(pattern):
+        #        print(x, y, smudge_pattern)
+        s_rows = count_reflected_rows(smudge_pattern, o_rows)
+        s_cols = count_reflected_rows(transform_pattern(smudge_pattern), o_cols)
+        #            print('x', x, 'y', y, 'r', s_rows, 'c', s_cols)
+        if s_rows is not None and s_rows > 0 and s_rows != o_rows:
+            return 100 * s_rows
+        if s_cols is not None and s_cols > 0 and s_cols != o_cols:
+            return s_cols
+
+
 def part2(lines):
     """
     >>> part2(load_example(__file__, "13"))
+    400
     """
+    return sum(foo(pattern) for pattern in read_patterns(lines))
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2023, "13")
-    # data = load_example(__file__, "13")
     print(part1(data))
-    # print(part2(data))
+    print(part2(data))
