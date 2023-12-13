45d34ac9bee76b617a5621650b1be6390a76168a
Enrico Pilz <kontakt@enricopilz.de>
2023-12-13 19:49:51 +0100

    py 2023/13 refactor

--- python/src/aoc/year2023/day13.py
+++ python/src/aoc/year2023/day13.py
@@ -1,45 +1,75 @@
+from typing import Generator
+
 from aoc.util import load_input, load_example
 
 # https://github.com/PyCQA/flake8/issues/316
 # flake8: noqa
 
 
-def read_patterns(lines):
+class MirrorPattern:
+    def __init__(self, lines):
+        self.w, self.h = len(lines[0]), len(lines)
+        self.d = {}
+        for y, line in enumerate(lines):
+            for x, c in enumerate(line):
+                self.d[x, y] = c
+
+    @staticmethod
+    def is_reflection_line(lines, r):
+        for i in range(len(lines)):
+            if r - i - 1 < 0 or r + i >= len(lines):
+                return i > 0
+            if lines[r - i - 1] != lines[r + i]:
+                return False
+        return True
+
+    def count_reflected_rows(self, forbidden=None):
+        rows = ["".join(self.d[x, y] for x in range(self.w)) for y in range(self.h)]
+        for i in range(1, self.h + 1):
+            if self.is_reflection_line(rows, i) and i != forbidden:
+                return i
+        return 0
+
+    def count_reflected_columns(self, forbidden=None):
+        columns = ["".join(self.d[x, y] for y in range(self.h)) for x in range(self.w)]
+        for i in range(1, self.w + 1):
+            if self.is_reflection_line(columns, i) and i != forbidden:
+                return i
+        return 0
+
+    def cleaned_patterns(self):
+        for x in range(self.w):
+            for y in range(self.h):
+                o, self.d[x, y] = self.d[x, y], "." if self.d[x, y] == "#" else "#"
+                yield
+                self.d[x, y] = o
+
+    def part1(self):
+        r = self.count_reflected_rows()
+        c = self.count_reflected_columns()
+        return 100 * r + c
+
+    def part2(self):
+        orig_r = self.count_reflected_rows()
+        orig_c = self.count_reflected_columns()
+        for _ in self.cleaned_patterns():
+            r = self.count_reflected_rows(orig_r)
+            c = self.count_reflected_columns(orig_c)
+            if r != 0 or c != 0:
+                return 100 * r + c
+
+
+def read_patterns(lines: str) -> Generator[MirrorPattern, None, None]:
     it = iter(lines)
+    pattern = None
     try:
         while it:
             pattern = []
             while (row := next(it)) != "":
                 pattern.append(row)
-            yield pattern
+            yield MirrorPattern(pattern)
     except StopIteration:
-        yield pattern
-
-
-def transform_pattern(pattern):
-    result = ["" for _ in range(len(pattern[0]))]
-    for row in pattern:
-        for x, c in enumerate(row):
-            result[x] += c
-    return result
-
-
-def is_reflection_row(pattern, r):
-    for i in range(len(pattern)):
-        if r - i < 0:
-            return i > 0
-        if r + i + 1 >= len(pattern):
-            return i > 0
-        if pattern[r - i] != pattern[r + i + 1]:
-            return False
-    return True
-
-
-def count_reflected_rows(pattern, forbidden=None):
-    for r in range(len(pattern)):
-        if is_reflection_row(pattern, r) and r + 1 != forbidden:
-            return r + 1
-    return 0
+        yield MirrorPattern(pattern)
 
 
 def part1(lines):
@@ -47,41 +77,7 @@ def part1(lines):
     >>> part1(load_example(__file__, "13"))
     405
     """
-    total = 0
-    for pattern in read_patterns(lines):
-        total += 100 * count_reflected_rows(pattern)
-        total += count_reflected_rows(transform_pattern(pattern))
-    return total
-
-
-def replacements(pattern):
-    w, h = len(pattern[0]), len(pattern)
-    d = {}
-    for y, row in enumerate(pattern):
-        for x, c in enumerate(row):
-            d[x, y] = c
-    for x in range(w):
-        for y in range(h):
-            o = d[x, y]
-            d[x, y] = "." if d[x, y] == "#" else "#"
-            result = ["".join(d[ix, iy] for ix in range(w)) for iy in range(h)]
-            d[x, y] = o
-            yield x, y, result
-
-
-def foo(pattern):
-    # print(pattern)
-    o_rows = count_reflected_rows(pattern)
-    o_cols = count_reflected_rows(transform_pattern(pattern))
-    for x, y, smudge_pattern in replacements(pattern):
-        #        print(x, y, smudge_pattern)
-        s_rows = count_reflected_rows(smudge_pattern, o_rows)
-        s_cols = count_reflected_rows(transform_pattern(smudge_pattern), o_cols)
-        #            print('x', x, 'y', y, 'r', s_rows, 'c', s_cols)
-        if s_rows is not None and s_rows > 0 and s_rows != o_rows:
-            return 100 * s_rows
-        if s_cols is not None and s_cols > 0 and s_cols != o_cols:
-            return s_cols
+    return sum(pattern.part1() for pattern in read_patterns(lines))
 
 
 def part2(lines):
@@ -89,7 +85,7 @@ def part2(lines):
     >>> part2(load_example(__file__, "13"))
     400
     """
-    return sum(foo(pattern) for pattern in read_patterns(lines))
+    return sum(pattern.part2() for pattern in read_patterns(lines))
 
 
 if __name__ == "__main__":
