f72a47f77cfa14a48944d660472a02ee1871d3d5
Enrico Pilz <kontakt@enricopilz.de>
2023-12-14 20:36:25 +0100

    py 2023/10 part2

--- python/src/aoc/year2023/day10.py
+++ python/src/aoc/year2023/day10.py
@@ -9,20 +9,20 @@ def position_of_s(lines):
 
 
 M = {
-    ("N", "S"): "N",
-    ("S", "S"): "S",
-    ("N", "|"): "N",
-    ("S", "|"): "S",
-    ("W", "-"): "W",
-    ("E", "-"): "E",
-    ("S", "L"): "E",
-    ("W", "L"): "N",
-    ("S", "J"): "W",
-    ("E", "J"): "N",
-    ("N", "7"): "W",
-    ("E", "7"): "S",
-    ("N", "F"): "E",
-    ("W", "F"): "S",
+    ("N", "S"): ("N", "E"),
+    ("S", "S"): ("S", "W"),
+    ("N", "|"): ("N", "E"),
+    ("S", "|"): ("S", "W"),
+    ("W", "-"): ("W", "N"),
+    ("E", "-"): ("E", "S"),
+    ("S", "L"): ("E", "WS"),
+    ("W", "L"): ("N", ""),
+    ("S", "J"): ("W", ""),
+    ("E", "J"): ("N", "SE"),
+    ("N", "7"): ("W", "EN"),
+    ("E", "7"): ("S", ""),
+    ("N", "F"): ("E", ""),
+    ("W", "F"): ("S", "NW"),
 }
 
 H = {
@@ -39,7 +39,7 @@ def wander(tiles, start_position, direction="S"):
     tile = tiles[py][px]
     while True:
         i += 1
-        direction = M[direction, tile]
+        direction, _ = M[direction, tile]
         dy, dx = H[direction]
         py += dy
         px += dx
@@ -59,14 +59,64 @@ def part1(lines):
     return wander(lines, s) // 2
 
 
+def wander2(tiles, start_position, direction="S"):
+    i = 0
+    py, px = start_position
+    tile = tiles[py][px]
+    border = set()
+    inner_area = set()
+    while True:
+        i += 1
+        direction, ia_pos = M[direction, tile]
+        dy, dx = H[direction]
+        for ia in ia_pos:
+            iy, ix = H[ia]
+            inner_area.add((py + iy, px + ix))
+        py += dy
+        px += dx
+        tile = tiles[py][px]
+        border.add((py, px))
+        if tile == "S":
+            return border, inner_area
+
+
+def neighbors(pos):
+    py, px = pos
+    return (py - 1, px), (py + 1, px), (py, px - 1), (py, px + 1)
+
+
+def flooding(border, inner_area):
+    open_set = inner_area.difference(border)
+    real_inner_area = set()
+    while open_set:
+        if len(open_set) > 1000:
+            return None
+        pos = open_set.pop()
+        real_inner_area.add(pos)
+        for neighbor in neighbors(pos):
+            if neighbor in border:
+                continue
+            if neighbor in real_inner_area:
+                continue
+            open_set.add(neighbor)
+    return real_inner_area
+
+
 def part2(lines):
     """
-    >>> part2(load_example(__file__, "10a"))
+    >>> part2(load_example(__file__, "10d"))
+    8
+    >>> part2(load_example(__file__, "10e"))
+    10
     """
-    ...
+    s = position_of_s(lines)
+    b, ia = wander2(lines, s)
+    f = flooding(b, ia)
+    if f is not None:
+        return len(f)
 
 
 if __name__ == "__main__":
     data = load_input(__file__, 2023, "10")
     print(part1(data))
-    # print(part2(data))
+    print(part2(data))
