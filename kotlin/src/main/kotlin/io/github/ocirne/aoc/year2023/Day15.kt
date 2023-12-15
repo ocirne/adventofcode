4d4ee000c19069606e6b8c29f2060b00dfa6d0e5
Enrico Pilz <kontakt@enricopilz.de>
2023-12-15 11:20:11 +0100

    kt 2023/15 putzen

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2023/Day15.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2023/Day15.kt
@@ -12,11 +12,10 @@ class Day15(val lines: List<String>) : AocChallenge(2023, 15) {
         return lines.first().split(',').sumOf { step -> aocHash(step) }
     }
 
-    fun focusingPower(boxes: List<MutableList<Pair<String, Int>>>): Int {
+    fun focusingPower(boxes: List<LinkedHashMap<String, Int>>): Int {
         var result = 0
         boxes.mapIndexed { boxNumber, box ->
             box.onEachIndexed { slotNumber, (_, focalLength) ->
-                println("$boxNumber $slotNumber $focalLength")
                 result += (1 + boxNumber) * (slotNumber + 1) * focalLength
             }
         }
@@ -24,28 +23,15 @@ class Day15(val lines: List<String>) : AocChallenge(2023, 15) {
     }
 
     override fun part2(): Int {
-        val boxes = (0..255).map { mutableListOf<Pair<String, Int>>() }
+        val boxes = (0..255).map { LinkedHashMap<String, Int>() }
         for (step in lines.first().split(',')) {
+            val label = step.split("[=-]".toRegex()).first()
+            val box = aocHash(label)
             if (step.contains("=")) {
-                val label = step.split("=")[0]
-                val box = aocHash(label)
-                val focalLength = step.split("=")[1].toInt()
-                var foo = true
-                boxes[box].forEachIndexed { index, (iLabel, _) ->
-                    if (iLabel == label) {
-                        boxes[box][index] = label to focalLength
-                        foo = false
-                    }
-                }
-                if (foo) {
-                    boxes[box].add(label to focalLength)
-                }
+                val focalLength = step.split("=").last().toInt()
+                boxes[box][label] = focalLength
             } else if (step.endsWith('-')) {
-                val label = step.split("-")[0]
-                val box = aocHash(label)
-                boxes[box].removeIf { (iLabel, _) -> iLabel == label }
-            } else {
-                throw IllegalStateException()
+                boxes[box].remove(label)
             }
         }
         return focusingPower(boxes)
