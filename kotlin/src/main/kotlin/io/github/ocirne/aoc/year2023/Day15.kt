71c26d918d1cb3fb971dfdb86d896de2cdd8183d
Enrico Pilz <kontakt@enricopilz.de>
2023-12-15 10:02:09 +0100

    kt 2023/15 part2

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2023/Day15.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2023/Day15.kt
@@ -12,7 +12,42 @@ class Day15(val lines: List<String>) : AocChallenge(2023, 15) {
         return lines.first().split(',').sumOf { step -> aocHash(step) }
     }
 
+    fun focusingPower(boxes: List<MutableList<Pair<String, Int>>>): Int {
+        var result = 0
+        boxes.mapIndexed { boxNumber, box ->
+            box.onEachIndexed { slotNumber, (_, focalLength) ->
+                println("$boxNumber $slotNumber $focalLength")
+                result += (1 + boxNumber) * (slotNumber + 1) * focalLength
+            }
+        }
+        return result
+    }
+
     override fun part2(): Int {
-        return -1
+        val boxes = (0..255).map { mutableListOf<Pair<String, Int>>() }
+        for (step in lines.first().split(',')) {
+            if (step.contains("=")) {
+                val label = step.split("=")[0]
+                val box = aocHash(label)
+                val focalLength = step.split("=")[1].toInt()
+                var foo = true
+                boxes[box].forEachIndexed { index, (iLabel, _) ->
+                    if (iLabel == label) {
+                        boxes[box][index] = label to focalLength
+                        foo = false
+                    }
+                }
+                if (foo) {
+                    boxes[box].add(label to focalLength)
+                }
+            } else if (step.endsWith('-')) {
+                val label = step.split("-")[0]
+                val box = aocHash(label)
+                boxes[box].removeIf { (iLabel, _) -> iLabel == label }
+            } else {
+                throw IllegalStateException()
+            }
+        }
+        return focusingPower(boxes)
     }
 }
