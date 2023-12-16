7625336f7c15b243529b3e98daa55ad35515a5f9
Enrico Pilz <kontakt@enricopilz.de>
2023-12-16 10:08:02 +0100

    py 2023/16 refactor

--- python/src/aoc/year2023/day16.py
+++ python/src/aoc/year2023/day16.py
@@ -1,9 +1,5 @@
 from aoc.util import load_input, load_example
 
-import sys
-
-sys.setrecursionlimit(2_00000)
-
 
 M = {
     ">": (+1, 0),
@@ -12,86 +8,72 @@ M = {
     "v": (0, +1),
 }
 
+CD = {
+    "/>": "^",
+    "/<": "v",
+    "/^": ">",
+    "/v": "<",
+    "\\>": "v",
+    "\\<": "^",
+    "\\^": "<",
+    "\\v": ">",
+    "|>": "^v",
+    "|<": "^v",
+    "-^": "<>",
+    "-v": "<>",
+}
 
-class CaveWalker:
-    def __init__(self, lines):
-        self.w, self.h, self.cave = self.read_cave(lines)
-        self.visited = {}
+
+class CaveBeam:
+    def __init__(self, w, h, lines):
+        self.w, self.h = w, h
+        self.cave = self.read_cave(lines)
 
     @staticmethod
     def read_cave(lines):
-        w, h = len(lines[0]), len(lines)
         cave = {}
         for y, line in enumerate(lines):
             for x, c in enumerate(line):
                 cave[x, y] = c
-        return w, h, cave
+        return cave
 
-    def rec(self, px, py, d):
-        dx, dy = M[d]
-        px += dx
-        py += dy
-        if not (0 <= px < self.w and 0 <= py < self.h):
-            return
-        if (px, py) in self.visited and self.visited[px, py] == d:
-            return
-        self.visited[px, py] = d
-        if self.cave[px, py] == ".":
-            self.rec(px, py, d)
-        elif self.cave[px, py] == "/":
-            if d == ">":
-                self.rec(px, py, "^")
-            elif d == "<":
-                self.rec(px, py, "v")
-            elif d == "^":
-                self.rec(px, py, ">")
-            elif d == "v":
-                self.rec(px, py, "<")
-        elif self.cave[px, py] == "\\":
-            if d == ">":
-                self.rec(px, py, "v")
-            elif d == "<":
-                self.rec(px, py, "^")
-            elif d == "^":
-                self.rec(px, py, "<")
-            elif d == "v":
-                self.rec(px, py, ">")
-        elif self.cave[px, py] == "|":
-            if d in "<>":
-                self.rec(px, py, "^")
-                self.rec(px, py, "v")
-            else:
-                self.rec(px, py, d)
-        elif self.cave[px, py] == "-":
-            if d in "^v":
-                self.rec(px, py, "<")
-                self.rec(px, py, ">")
-            else:
-                self.rec(px, py, d)
+    def neighbors(self, px, py, d):
+        for nd in CD.get(self.cave[px, py] + d, d):
+            dx, dy = M[nd]
+            yield px + dx, py + dy, nd
+
+    def dijkstra(self, px, py, d):
+        open_set = {(px, py, d)}
+        visited = {}
+        while open_set:
+            px, py, d = open_set.pop()
+            visited[px, py] = d
+            for nx, ny, nd in self.neighbors(px, py, d):
+                if not (0 <= nx < self.w and 0 <= ny < self.h):
+                    continue
+                if (nx, ny) in visited and visited[nx, ny] == nd:
+                    continue
+                open_set.add((nx, ny, nd))
+        return len(visited)
 
 
 def part1(lines):
-    walker = CaveWalker(lines)
-    walker.rec(-1, 0, ">")
-    return len(walker.visited)
+    """
+    >>> part1(load_example(__file__, "16"))
+    46
+    """
+    w, h = len(lines[0]), len(lines)
+    return CaveBeam(w, h, lines).dijkstra(0, 0, ">")
 
 
-def try_out(lines):
+def try_all_beams(lines):
     w, h = len(lines[0]), len(lines)
     for sx in range(w):
-        walker = CaveWalker(lines)
-        walker.rec(sx, -1, "v")
-        yield len(walker.visited)
-        walker = CaveWalker(lines)
-        walker.rec(sx, h, "^")
-        yield len(walker.visited)
+        yield CaveBeam(w, h, lines).dijkstra(sx, 0, "v")
+        yield CaveBeam(w, h, lines).dijkstra(sx, h - 1, "^")
     for sy in range(h):
-        walker = CaveWalker(lines)
-        walker.rec(-1, sy, ">")
-        yield len(walker.visited)
-        walker = CaveWalker(lines)
-        walker.rec(w, sy, "<")
-        yield len(walker.visited)
+        yield CaveBeam(w, h, lines).dijkstra(0, sy, ">")
+        yield CaveBeam(w, h, lines).dijkstra(w - 1, sy, "<")
 
 
 def part2(lines):
@@ -99,7 +81,7 @@ def part2(lines):
     >>> part2(load_example(__file__, "16"))
     51
     """
-    return max(try_out(lines))
+    return max(try_all_beams(lines))
 
 
 if __name__ == "__main__":
