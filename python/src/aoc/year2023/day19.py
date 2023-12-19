b131523c41b6b43ed0c49daf13dc366846f53763
Enrico Pilz <kontakt@enricopilz.de>
2023-12-19 21:59:44 +0100

    py 2023/19 bisserle putzen

--- python/src/aoc/year2023/day19.py
+++ python/src/aoc/year2023/day19.py
@@ -6,7 +6,6 @@ from aoc.util import load_input, load_example
 WORKFLOW_PATTERN = re.compile(r"([a-z]+)\{(.*)}")
 LESS_THAN_PATTERN = re.compile(r"([xmas])<(\d+):([ARa-z]+)")
 GREATER_THAN_PATTERN = re.compile(r"([xmas])>(\d+):([ARa-z]+)")
-PART_PATTERN = re.compile(r"\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}")
 
 
 class LessThanRule:
@@ -27,18 +26,18 @@ class LessThanRule:
 
 class GreaterThanRule:
     def __init__(self, variable, value, next_workflow):
-        self.variable = variable
+        self.category = variable
         self.value = int(value)
         self.next_workflow = next_workflow
 
     def match(self, part):
-        return part.foo[self.variable] > self.value
+        return part.foo[self.category] > self.value
 
     def __str__(self) -> str:
-        return self.variable + " greater than: " + str(self.value) + " -> " + self.next_workflow
+        return self.category + " greater than: " + str(self.value) + " -> " + self.next_workflow
 
     def anti(self):
-        return LessThanRule(self.variable, self.value + 1, "anti")
+        return LessThanRule(self.category, self.value + 1, "anti")
 
 
 class DirectRule:
@@ -65,7 +64,6 @@ class Workflow:
             elif re.match(r"[a-z]+", rule):
                 self.rules.append(DirectRule(rule))
             else:
-                print("Problem", rule)
                 raise
 
     def __str__(self) -> str:
@@ -79,11 +77,14 @@ class Workflow:
 
 
 class Part:
+
+    PART_PATTERN = re.compile(r"\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}")
+
     def __init__(self, line):
-        x, m, a, s = map(int, PART_PATTERN.match(line).groups())
+        x, m, a, s = map(int, self.PART_PATTERN.match(line).groups())
         self.foo = {"x": x, "m": m, "a": a, "s": s}
 
-    def sum(self):
+    def rating(self):
         return sum(self.foo.values())
 
 
@@ -96,11 +97,7 @@ def handle_part(workflows, part):
         current_workflow = workflows[result]
 
 
-def part1(lines):
-    """
-    >>> part1(load_example(__file__, "19"))
-    19114
-    """
+def read_workflows_parts(lines):
     it = iter(lines)
 
     workflows = {}
@@ -110,24 +107,28 @@ def part1(lines):
         g = WORKFLOW_PATTERN.match(line)
         name, rules = g.groups()
         workflows[name] = Workflow(rules)
-        print("workflow", workflows[name])
-    print()
-    total = 0
-    for line in it:
-        print("part", line)
-        part = Part(line)
-        if handle_part(workflows, part) == "A":
-            total += part.sum()
-    return total
+
+    parts = [Part(line) for line in it]
+
+    return workflows, parts
 
 
-def upper_bound(collected_rules, variable):
-    values = [rule.value for rule in collected_rules if isinstance(rule, LessThanRule) and rule.variable == variable]
+def part1(lines):
+    """
+    >>> part1(load_example(__file__, "19"))
+    19114
+    """
+    workflows, parts = read_workflows_parts(lines)
+    return sum(part.rating() for part in parts if handle_part(workflows, part) == "A")
+
+
+def upper_bound(constraints, category):
+    values = [rule.value for rule in constraints if isinstance(rule, LessThanRule) and rule.variable == category]
     return min(values) if values else 4001
 
 
-def lower_bound(collected_rules, variable):
-    values = [rule.value for rule in collected_rules if isinstance(rule, GreaterThanRule) and rule.variable == variable]
+def lower_bound(constraints, category):
+    values = [rule.value for rule in constraints if isinstance(rule, GreaterThanRule) and rule.category == category]
     return max(values) if values else 0
 
 
@@ -137,15 +138,13 @@ def bar(collected_rules):
         lb, ub = lower_bound(collected_rules, v), upper_bound(collected_rules, v)
         assert lb < ub
         p = ub - lb - 1
-        print(v, lb, ub, p)
         total *= p
     return total
 
 
-def rec(workflows, current_node, i=0, collected_rules=[]):
+def dfs(workflows, current_node, i=0, constraints=[]):
     if current_node == "A":
-        print("; ".join(str(s) for s in collected_rules))
-        return bar(collected_rules)
+        return bar(constraints)
     if current_node == "R":
         return 0
     current_workflow = workflows[current_node]
@@ -153,10 +152,10 @@ def rec(workflows, current_node, i=0, collected_rules=[]):
         raise
     current_rule = current_workflow.rules[i]
     # match
-    total = rec(workflows, current_rule.next_workflow, 0, collected_rules + [current_rule])
+    total = dfs(workflows, current_rule.next_workflow, 0, constraints + [current_rule])
     # no match
     if i + 1 < len(current_workflow.rules):
-        total += rec(workflows, current_node, i + 1, collected_rules + [current_rule.anti()])
+        total += dfs(workflows, current_node, i + 1, constraints + [current_rule.anti()])
     return total
 
 
@@ -165,37 +164,8 @@ def part2(lines):
     >>> part2(load_example(__file__, "19"))
     167409079868000
     """
-    it = iter(lines)
-
-    workflows = {}
-    for line in it:
-        if not line:
-            break
-        g = WORKFLOW_PATTERN.match(line)
-        name, rules = g.groups()
-        workflows[name] = Workflow(rules)
-    #        print("workflow", workflows[name])
-
-    while True:
-        direct = {}
-        for name, workflow in workflows.items():
-            targets = [rule.next_workflow for rule in workflow.rules]
-            if len(set(targets)) == 1:
-                direct[name] = targets[0]
-        if not direct:
-            break
-        for name, workflow in workflows.items():
-            for rule in workflow.rules:
-                if rule.next_workflow in direct:
-                    rule.next_workflow = direct[rule.next_workflow]
-        for name in direct:
-            workflows.pop(name)
-    for name, wf in workflows.items():
-        print(name, "; ".join(str(s) for s in wf.rules))
-
-    print()
-    # analyze workflow tree
-    return rec(workflows, "in")
+    workflows, _ = read_workflows_parts(lines)
+    return dfs(workflows, "in")
 
 
 if __name__ == "__main__":
