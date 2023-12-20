977d2b6f046a1bfe933165791ab27caf411bff00
Enrico Pilz <kontakt@enricopilz.de>
2023-12-20 20:04:39 +0100

    py 2023/20 part2 wip

--- python/src/aoc/year2023/day20.py
+++ python/src/aoc/year2023/day20.py
@@ -4,6 +4,8 @@ from aoc.util import load_input, load_example
 class Module:
     def __init__(self, destinations):
         self.destinations = destinations.split(", ")
+        self.state = None
+        self.count = 0
 
 
 class FlipFlop(Module):
@@ -12,6 +14,7 @@ class FlipFlop(Module):
         self.state = 0
 
     def pulse(self, _, value):
+        self.count += 1
         if value == 0:
             self.state ^= 1
             for d in self.destinations:
@@ -25,14 +28,15 @@ class Conjunction(Module):
         self.most_recent = {}
 
     def pulse(self, src, value):
+        self.count += 1
         assert src in self.most_recent
         self.most_recent[src] = value
         if all(v == 1 for v in self.most_recent.values()):
-            for d in self.destinations:
-                yield d, 0
+            self.state = 0
         else:
-            for d in self.destinations:
-                yield d, 1
+            self.state = 1
+        for d in self.destinations:
+            yield d, self.state
 
 
 class Broadcaster(Module):
@@ -40,6 +44,7 @@ class Broadcaster(Module):
         super().__init__(destinations)
 
     def pulse(self, _, value):
+        self.count += 1
         for d in self.destinations:
             yield d, value
 
@@ -47,9 +52,12 @@ class Broadcaster(Module):
 class Output(Module):
     def __init__(self):
         super().__init__("")
+        self.state = 0
 
-    def pulse(self, _, value):
-        print("Output", value)
+    def pulse(self, src, value):
+        self.count += 1
+        #   print(src, " -> rx", value)
+        self.state = value
         return []
 
 
@@ -89,12 +97,12 @@ def part1(lines):
         total_pulses[0] += 1
         while pulses:
             src, dest, value = pulses.pop(0)
-            print(src, "-", value, "->", dest)
+            #      print(src, "-", value, "->", dest)
             if dest in modules:
                 for d, v in modules[dest].pulse(src, value):
                     total_pulses[v] += 1
                     pulses.append((dest, d, v))
-    #    print(total_pulses)
+    print(total_pulses)
     return total_pulses[0] * total_pulses[1]
 
 
@@ -102,10 +110,48 @@ def part2(lines):
     """
     >>> part2(load_example(__file__, "20a"))
     """
+    modules = {"output": Output(), "rx": Output()}
+    for line in lines:
+        src, dest = line.split(" -> ")
+        if src == "broadcaster":
+            modules["broadcaster"] = Broadcaster(dest)
+        elif src.startswith("%"):
+            modules[src[1:]] = FlipFlop(dest)
+        elif src.startswith("&"):
+            modules[src[1:]] = Conjunction(dest)
+        else:
+            raise
+
+    for source, module in modules.items():
+        for destination in module.destinations:
+            if not destination:
+                continue
+            if destination not in modules:
+                continue
+            if not isinstance(modules[destination], Conjunction):
+                continue
+            modules[destination].most_recent[source] = 0
+
+    total_pulses = {0: 0, 1: 0}
+    for i in range(1000):
+        pulses = [("button", "broadcaster", 0)]
+        modules["broadcaster"].pulse(None, 0)
+        total_pulses[0] += 1
+        while pulses:
+            src, dest, value = pulses.pop(0)
+            #            print(src, "-", value, "->", dest)
+            for d, v in modules[dest].pulse(src, value):
+                total_pulses[v] += 1
+                pulses.append((dest, d, v))
+        if modules["rx"].state == 0 and modules["rx"].count == 1:
+            return i
+        modules["rx"].count = 0
+        print(" ".join("%s:%s" % (key, module.state) for key, module in modules.items()))
+    return total_pulses
 
 
 if __name__ == "__main__":
     # print(part1(load_example(__file__, "20b")))
     data = load_input(__file__, 2023, "20")
     print(part1(data))
-    # print(part2(data))
+    print(part2(data))
