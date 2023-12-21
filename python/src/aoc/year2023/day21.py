0f65888584c6f548d25ae5dd404219a57a84a1b2
Enrico Pilz <kontakt@enricopilz.de>
2023-12-21 19:42:10 +0100

    py 2023/21 part2 wip

--- python/src/aoc/year2023/day21.py
+++ python/src/aoc/year2023/day21.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from aoc.util import load_input, load_example
 
 
@@ -38,6 +40,47 @@ def part1(lines, steps=64):
     return foo(garden, start, steps)
 
 
+def read_infinite_garden(lines):
+    w, h = len(lines[0]), len(lines)
+    garden, s = defaultdict(lambda: None), None
+    for y, line in enumerate(lines):
+        for x, v in enumerate(line):
+            garden[x, y] = v
+            if v == "S":
+                s = x, y
+    return w, h, garden, s
+
+
+def infinite_neighbors(w, h, garden, x, y):
+    for nx, ny in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+        if garden[nx % w, ny % h] != "#":
+            yield nx, ny
+
+
+def infinite_foo(w, h, garden, start, max_steps):
+    steps = {start}
+    s = []
+    for i in range(max_steps):
+        #        print('i', i, 'plots', len(steps))
+        s.append(len(steps))
+        new_steps = set()
+        for cx, cy in steps:
+            for nx, ny in infinite_neighbors(w, h, garden, cx, cy):
+                new_steps.add((nx, ny))
+        steps = new_steps
+    #    print('i', max_steps, 'plots', len(steps))
+    s.append(len(steps))
+    return len(steps), s
+
+
+def rec(a):
+    if all(n == 0 for n in a):
+        print("***")
+        return 0
+    d = [s - p for p, s in zip(a[:-1], a[1:])]
+    return a[-1] + rec(d)
+
+
 def part2(lines, steps=26501365):
     """
     >>> part2(load_example(__file__, "21"), steps=6)
@@ -55,10 +98,15 @@ def part2(lines, steps=26501365):
     >>> part2(load_example(__file__, "21"), steps=5000)
     16733044
     """
+    w, h, garden, start = read_infinite_garden(lines)
+    r, s = infinite_foo(w, h, garden, start, steps)
+    print("r", r)
+    print(s)
+    print(rec(s[200::2]))
 
 
 if __name__ == "__main__":
-    print(part2(load_example(__file__, "21"), steps=6))
+    print(part2(load_example(__file__, "21"), steps=64))
     # data = load_input(__file__, 2023, "21")
     # print(part1(data))
     # print(part2(data))
