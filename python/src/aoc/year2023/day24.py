cc7f1d45108f458639e5e229cd0be168ba840775
Enrico Pilz <kontakt@enricopilz.de>
2023-12-26 12:53:06 +0100

    py 2023/24 wip

--- python/src/aoc/year2023/day24.py
+++ python/src/aoc/year2023/day24.py
@@ -5,7 +5,7 @@ from aoc.util import load_input, load_example
 LB, UB = 200_000_000_000_000, 400_000_000_000_000
 
 
-def read_vectors(lines):
+def read_vectors1(lines):
     for line in lines:
         p, v = line.split("@")
         px, py, pz = map(int, p.split(","))
@@ -13,16 +13,15 @@ def read_vectors(lines):
         yield px, py, vx, vy
 
 
-def cross_point(v1, v2):
+def cross_point(v1, v2, lb, ub):
     """
-    # ax + a * vx == bx + b * wx
-    # ay + a * vy == by + b * wy
+    # ax + a * vx == bx + b * wx == cx
+    # ay + a * vy == by + b * wy == cy
     """
     ax, ay, vx, vy = v1
     bx, by, wx, wy = v2
     # parallel?
     if vx * wy == vy * wx:
-        print(v1, v2, "-> parallel")
         return False
     n = vx * wy - vy * wx
     a = (wy * (bx - ax) + wx * (ay - by)) / n
@@ -32,26 +31,38 @@ def cross_point(v1, v2):
     if b < 0:
         return False
     cx, cy = ax + a * vx, ay + a * vy
-    return LB <= cx <= UB and LB <= cy <= UB
+    return lb <= cx <= ub and lb <= cy <= ub
 
 
-def part1(lines):
+def part1(lines, lb=LB, ub=UB):
     """
-    >>> part1(load_example(__file__, "24"))
+    >>> part1(load_example(__file__, "24"), lb=7, ub=27)
     2
     """
-    vectors = list(read_vectors(lines))
-    return sum(cross_point(v1, v2) for v1, v2 in combinations(vectors, 2))
+    vectors = read_vectors1(lines)
+    return sum(cross_point(v1, v2, lb, ub) for v1, v2 in combinations(vectors, 2))
+
+
+def read_vectors2(lines):
+    for line in lines:
+        p, v = line.split("@")
+        px, py, pz = map(int, p.split(","))
+        vx, vy, vz = map(int, v.split(","))
+        yield px, py, pz, vx, vy, vz
 
 
 def part2(lines):
     """
     >>> part2(load_example(__file__, "24"))
+    47
     """
+    vectors = list(read_vectors1(lines))
+    for v in vectors:
+        print(v)
 
 
 if __name__ == "__main__":
-    # print(part1(load_example(__file__, "24")))
-    data = load_input(__file__, 2023, "24")
-    print(part1(data))
+    print(part2(load_example(__file__, "24")))
+    # data = load_input(__file__, 2023, "24")
+    # print(part1(data))
     # print(part2(data))
