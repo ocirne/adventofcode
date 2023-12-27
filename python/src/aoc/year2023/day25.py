5d8983d37c2738f6bfd6506423168afc810ad6e0
Enrico Pilz <kontakt@enricopilz.de>
2023-12-27 12:06:07 +0100

    py 2023/25 refactor

--- python/src/aoc/year2023/day25.py
+++ python/src/aoc/year2023/day25.py
@@ -4,49 +4,71 @@ import random
 from aoc.util import load_input, load_example
 
 
-def read_graph(lines):
-    graph = defaultdict(list)
-    for line in lines:
-        node, neighbors = line.split(": ")
-        for neighbor in neighbors.split():
-            graph[node].append(neighbor)
-            graph[neighbor].append(node)
-    return graph
+class Snowverload:
+    def __init__(self, lines):
+        self.graph = self.read_graph(lines)
+        self.result = None
 
+    @staticmethod
+    def read_graph(lines):
+        graph = defaultdict(list)
+        for line in lines:
+            node, neighbors = line.split(": ")
+            for neighbor in neighbors.split():
+                graph[node].append(neighbor)
+                graph[neighbor].append(node)
+        return graph
 
-def find_random_path(graph, candidates):
-    start = random.choice(list(graph.keys()))
-    end = random.choice(list(graph.keys()))
-    open_set = [start]
-    visited = set()
-    parent = {}
-    while open_set:
-        current_node = open_set.pop(0)
-        if current_node == end:
-            path_node = end
-            path = []
-            while path_node in parent:
-                next_node = parent[path_node]
-                path.append((min(path_node, next_node), max(path_node, next_node)))
-                path_node = next_node
-            return None, path
-        if current_node in visited:
-            continue
-        visited.add(current_node)
-        for neighbor_node in random.sample(graph[current_node], len(graph[current_node])):
-            if neighbor_node in visited:
+    def random_node(self):
+        return random.choice(list(self.graph.keys()))
+
+    @staticmethod
+    def collect_path(parent, end):
+        path_node = end
+        path = []
+        while path_node in parent:
+            next_node = parent[path_node]
+            path.append((min(path_node, next_node), max(path_node, next_node)))
+            path_node = next_node
+        return path
+
+    def find_random_path(self, candidates):
+        start = self.random_node()
+        end = self.random_node()
+        open_set = [start]
+        visited = set()
+        parent = {}
+        while open_set:
+            current_node = open_set.pop(0)
+            if current_node == end:
+                return self.collect_path(parent, end)
+            if current_node in visited:
                 continue
-            if (current_node, neighbor_node) in candidates:
-                continue
-            if (neighbor_node, current_node) in candidates:
-                continue
-            parent[neighbor_node] = current_node
-            open_set.append(neighbor_node)
-    # no path from start to end possible
-    total = len(graph)
-    half = len(visited)
-    if half < total:
-        return (total - half) * half, None
+            visited.add(current_node)
+            for neighbor_node in self.graph[current_node]:
+                if neighbor_node in visited:
+                    continue
+                if (current_node, neighbor_node) in candidates:
+                    continue
+                if (neighbor_node, current_node) in candidates:
+                    continue
+                parent[neighbor_node] = current_node
+                open_set.append(neighbor_node)
+        # no path from start to end possible
+        half = len(visited)
+        if half < len(self.graph):
+            self.result = (len(self.graph) - half) * half
+        return None
+
+    def divide_graph(self):
+        counter = Counter()
+        candidates = []
+        while True:
+            path_edges = self.find_random_path(candidates)
+            if path_edges is None:
+                return
+            counter.update(path_edges)
+            candidates = [p for p, _ in counter.most_common()[:3]]
 
 
 def part1(lines):
@@ -54,15 +76,9 @@ def part1(lines):
     >>> part1(load_example(__file__, "25"))
     54
     """
-    graph = read_graph(lines)
-    counter = Counter()
-    candidates = []
-    while True:
-        result, path_edges = find_random_path(graph, candidates)
-        if result is not None:
-            return result
-        counter.update(path_edges)
-        candidates = [p for p, _ in counter.most_common()[:3]]
+    snowverload = Snowverload(lines)
+    snowverload.divide_graph()
+    return snowverload.result
 
 
 def part2(lines):
