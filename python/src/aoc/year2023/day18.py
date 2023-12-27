f782e196259e4a71d72cf4bc54859d1f81dd0503
Enrico Pilz <kontakt@enricopilz.de>
2023-12-27 23:04:54 +0100

    py 2023/18 refactor

--- python/src/aoc/year2023/day18.py
+++ python/src/aoc/year2023/day18.py
@@ -1,227 +1,168 @@
 import re
+from typing import Tuple, Set, List, Generator
 
 from aoc.util import load_input, load_example
 
 
-MOVES = {
-    "0": (0, +1),
-    "R": (0, +1),
-    "1": (+1, 0),
-    "D": (+1, 0),
-    "2": (0, -1),
-    "L": (0, -1),
-    "3": (-1, 0),
-    "U": (-1, 0),
-}
+class Digger:
+    def __init__(self, edge):
+        self.edge = edge
+
+    @staticmethod
+    def _neighbors(position):
+        x, y = position
+        if x < -200 or y < -200:
+            print("outer area")
+            raise
+        return (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)
+
+    def dig_inner_area(self, start):
+        assert start not in self.edge
+        open_set = {start}
+        real_inner_area = set()
+        while open_set:
+            current = open_set.pop()
+            real_inner_area.add(current)
+            for neighbor in self._neighbors(current):
+                if neighbor in self.edge:
+                    continue
+                if neighbor in real_inner_area:
+                    continue
+                open_set.add(neighbor)
+        assert not self.edge.intersection(real_inner_area)
+        return self.edge.union(real_inner_area)
 
 
-def wander(lines):
-    py, px = 0, 0
+class Indizes:
+    def __init__(self, lines, extract_dig_plan):
+        x, y = 0, 0
+        xs, ys = set(), set()
+        for dx, dy, count in extract_dig_plan(lines):
+            x += count * dx
+            y += count * dy
+            xs.add(x)
+            xs.add(x + 1)
+            ys.add(y)
+            ys.add(y + 1)
+        self.x, self.y = sorted(xs), sorted(ys)
+
+    @staticmethod
+    def _reverse(values: List[int], value: int) -> int:
+        for index, w in enumerate(values):
+            if value == w:
+                return index
+        raise
+
+    @staticmethod
+    def _reverse_range(values: List[int], left: int, right: int) -> Generator[int, None, None]:
+        if left > right:
+            left, right = right, left
+        for index, w in enumerate(values):
+            if left <= w <= right:
+                yield index
+
+    def reverse_x(self, x: int) -> int:
+        return self._reverse(self.x, x)
+
+    def reverse_x_range(self, left: int, right: int) -> Generator[int, None, None]:
+        return self._reverse_range(self.x, left, right)
+
+    def reverse_y(self, y: int) -> int:
+        return self._reverse(self.y, y)
+
+    def reverse_y_range(self, left: int, right: int) -> Generator[int, None, None]:
+        return self._reverse_range(self.y, left, right)
+
+
+def trace_edge(indizes: Indizes, lines: List[str], extract_dig_plan) -> Set[Tuple[int, int]]:
+    x, y = 0, 0
     edge = set()
-    for line in lines:
-        direction, count, _ = line.split()
-        dy, dx = MOVES[direction]
-        for i in range(int(count)):
-            py += dy
-            px += dx
-            edge.add((py, px))
+    for dx, dy, count in extract_dig_plan(lines):
+        if dx != 0:
+            iy = indizes.reverse_y(y)
+            next_x = x + count * dx
+            edge.update((ix, iy) for ix in indizes.reverse_x_range(x, next_x))
+            x = next_x
+        if dy != 0:
+            ix = indizes.reverse_x(x)
+            next_y = y + count * dy
+            edge.update((ix, iy) for iy in indizes.reverse_y_range(y, next_y))
+            y = next_y
     return edge
 
 
-def neighbors(pos):
-    py, px = pos
-    if py < 0 or px < 0:
-        print("outer area")
-        raise
-    return (py - 1, px), (py + 1, px), (py, px - 1), (py, px + 1)
+def sum_total_area(indizes: Indizes, area: Set[Tuple[int, int]]) -> int:
+    total_area = 0
+    for ix, iy in area:
+        w = indizes.x[ix + 1] - indizes.x[ix]
+        h = indizes.y[iy + 1] - indizes.y[iy]
+        total_area += w * h
+    return total_area
 
 
-def flooding(edge, start):
-    assert start not in edge
-    open_set = {start}
-    real_inner_area = set()
-    while open_set:
-        pos = open_set.pop()
-        real_inner_area.add(pos)
-        for neighbor in neighbors(pos):
-            if neighbor in edge:
-                continue
-            if neighbor in real_inner_area:
-                continue
-            open_set.add(neighbor)
-    assert not edge.intersection(real_inner_area)
-    return edge.union(real_inner_area)
+MOVES = {
+    "0": (+1, 0),
+    "R": (+1, 0),
+    "1": (0, +1),
+    "D": (0, +1),
+    "2": (-1, 0),
+    "L": (-1, 0),
+    "3": (0, -1),
+    "U": (0, -1),
+}
 
 
-def part1(lines):
+def extract_dig_plan_1(lines: List[str]) -> Generator[Tuple[int, int, int], None, None]:
     """
-    good enough
-
-    >>> part1(load_example(__file__, "18"))
-    62
-    """
-    edge = wander(lines)
-    #    foo = flooding(edge, (20, 152))
-
-    min_x = min(x for _, x in edge)
-    max_x = max(x for _, x in edge)
-    min_y = min(y for y, _ in edge)
-    max_y = max(y for y, _ in edge)
-
-    print(min_x, max_x, min_y, max_y)
-
-    min_x, max_x = -60, 120
-    min_y, max_y = -200, -160
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            if (y, x) == (20, 152):
-                print("%", end="")
-            elif (y, x) in edge:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-
-    print(min_x, max_x, min_y, max_y)
-
-    return len(edge)
-
-
-def extract_1(lines):
-    """
-    >>> next(extract_1(["R 6 (#70c710)"]))
-    (0, 1, 6)
-    >>> next(extract_1(["D 5 (#0dc571)"]))
-    (1, 0, 5)
+    >>> next(extract_dig_plan_1(["R 6 (#70c710)"]))
+    (1, 0, 6)
+    >>> next(extract_dig_plan_1(["D 5 (#0dc571)"]))
+    (0, 1, 5)
     """
     for line in lines:
         direction, count, _ = line.split()
-        dy, dx = MOVES[direction]
-        yield dy, dx, int(count)
+        dx, dy = MOVES[direction]
+        yield dx, dy, int(count)
 
 
-def extract_2(lines):
+def extract_dig_plan_2(lines: List[str]) -> Generator[Tuple[int, int, int], None, None]:
     """
-    >>> next(extract_2(["R 6 (#70c710)"]))
-    (0, 1, 461937)
-    >>> next(extract_2(["D 5 (#0dc571)"]))
-    (1, 0, 56407)
+    >>> next(extract_dig_plan_2(["R 6 (#70c710)"]))
+    (1, 0, 461937)
+    >>> next(extract_dig_plan_2(["D 5 (#0dc571)"]))
+    (0, 1, 56407)
     """
     pattern = re.compile(r".*\(#([a-f0-9]{5})(\d)\)")
     for line in lines:
         count, direction = pattern.match(line).groups()
-        dy, dx = MOVES[direction]
-        yield dy, dx, int(count, 16)
+        dx, dy = MOVES[direction]
+        yield dx, dy, int(count, 16)
 
 
-def reverse_index(values, left, right):
-    if left > right:
-        left, right = right, left
-    for index, w in enumerate(values):
-        if left <= w <= right:
-            yield index
+def find_inner_area(lines: List[str], extract_dig_plan, start) -> int:
+    indizes = Indizes(lines, extract_dig_plan)
+    edge = trace_edge(indizes, lines, extract_dig_plan)
+    area = Digger(edge).dig_inner_area(start)
+    return sum_total_area(indizes, area)
 
 
-def reverse_index2(values, value):
-    for index, w in enumerate(values):
-        if value == w:
-            return index
-    #    print(values, value)
-    raise
-
-
-def solution2(iys, ixs, flooded_edge):
-    total_area = 0
-    for iy, ix in flooded_edge:
-        dy = iys[iy + 1] - iys[iy]
-        dx = ixs[ix + 1] - ixs[ix]
-        total_area += dy * dx
-    return total_area
-
-
-def wander2(lines):
+def part1(lines: List[str], start: Tuple[int, int] = (152, 20)):
     """
-    Kante 0,0 -> 0,5 (vertikal)
-    x: Breite: 1
-    y: Höhe: 6
-
-    x: [-oo..0][0:1][1..+oo]
-    y: [-oo..0][0:6][6..+oo]
-
-    :param lines:
-    :return:
+    >>> part1(load_example(__file__, "18"), start=(1, 1))
+    62
     """
-    py, px = 0, 0
-    ys, xs = set(), set()
-    for dy, dx, count in extract_2(lines):
-        py += count * dy
-        px += count * dx
-        ys.add(py)
-        ys.add(py + 1)
-        xs.add(px)
-        xs.add(px + 1)
-    ys = sorted(ys)
-    xs = sorted(xs)
-
-    py, px = 0, 0
-    edge = set()
-    for dy, dx, count in extract_2(lines):
-        #        print('dy', dy, 'dx', dx, 'count', count, 'px', px, 'py', py)
-        if dy == 0:
-            iy = reverse_index2(ys, py)
-            npx = px + count * dx
-            for ix in reverse_index(xs, px, npx):
-                edge.add((iy, ix))
-            px = npx
-            ix = reverse_index2(xs, px)
-        if dx == 0:
-            ix = reverse_index2(xs, px)
-            npy = py + count * dy
-            for iy in reverse_index(ys, py, npy):
-                edge.add((iy, ix))
-            py = npy
-            iy = reverse_index2(ys, py)
-    return ys, xs, edge
+    return find_inner_area(lines, extract_dig_plan_1, start)
 
 
-def part2(lines):
+def part2(lines: List[str], start: Tuple[int, int] = (152, 20)):
     """
-    >>> part2(load_example(__file__, "18"))
+    >>> part2(load_example(__file__, "18"), start=(1, 1))
     952408144115
     """
-    ys, xs, edge = wander2(lines)
-    #    print('ys', ys)
-    #    print('xs', xs)
-    #    print('edge', edge)
-
-    edge = flooding(edge, (20, 152))
-
-    min_x = min(x for _, x in edge)
-    max_x = max(x for _, x in edge)
-    min_y = min(y for y, _ in edge)
-    max_y = max(y for y, _ in edge)
-
-    print(min_x, max_x, min_y, max_y)
-
-    min_x, max_x = 0, 180
-    min_y, max_y = 0, 40
-    for y in range(min_y, max_y + 1):
-        for x in range(min_x, max_x + 1):
-            if (y, x) == (20, 152):
-                print("%", end="")
-            elif (y, x) in edge:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-
-    print(min_x, max_x, min_y, max_y)
-
-    return solution2(ys, xs, edge)
+    return find_inner_area(lines, extract_dig_plan_2, start)
 
 
 if __name__ == "__main__":
-    # print(part2(load_example(__file__, "18")))
     data = load_input(__file__, 2023, "18")
     print(part1(data))
     print(part2(data))
