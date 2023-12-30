dcc7f7e2ad1a208ab04b42524449f263a272f42f
Enrico Pilz <kontakt@enricopilz.de>
2023-12-30 20:15:27 +0100

    py 2023/20 part2

--- python/src/aoc/year2023/day20.py
+++ python/src/aoc/year2023/day20.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from math import lcm
+
 from aoc.util import load_input, load_example
 
 
@@ -54,70 +57,13 @@ class Output(Module):
         super().__init__("")
         self.state = 0
 
-    def pulse(self, src, value):
+    def pulse(self, _, value):
         self.count += 1
-        #   print(src, " -> rx", value)
         self.state = value
         return []
 
 
-def part1(lines):
-    """
-    >>> part1(load_example(__file__, "20a"))
-    32000000
-    >>> part1(load_example(__file__, "20b"))
-    11687500
-    """
-    modules = {"output": Output()}
-    for line in lines:
-        src, dest = line.split(" -> ")
-        if src == "broadcaster":
-            modules["broadcaster"] = Broadcaster(dest)
-        elif src.startswith("%"):
-            modules[src[1:]] = FlipFlop(dest)
-        elif src.startswith("&"):
-            modules[src[1:]] = Conjunction(dest)
-        else:
-            raise
-
-    for source, module in modules.items():
-        for destination in module.destinations:
-            if not destination:
-                continue
-            if destination not in modules:
-                continue
-            if not isinstance(modules[destination], Conjunction):
-                continue
-            modules[destination].most_recent[source] = 0
-
-    total_pulses = {0: 0, 1: 0}
-    for _ in range(1000):
-        pulses = [("button", "broadcaster", 0)]
-        modules["broadcaster"].pulse(None, 0)
-        total_pulses[0] += 1
-        while pulses:
-            src, dest, value = pulses.pop(0)
-            #      print(src, "-", value, "->", dest)
-            if dest in modules:
-                for d, v in modules[dest].pulse(src, value):
-                    total_pulses[v] += 1
-                    pulses.append((dest, d, v))
-    print(total_pulses)
-    return total_pulses[0] * total_pulses[1]
-
-
-def part2(lines):
-    #    print('digraph D {')
-
-    #    for line in lines:
-    #        src, dest = line.split(" -> ")
-    #        if src == "broadcaster":
-    #            print(line)
-    #        else:
-    #            print(line[1:])
-    #    print('}')
-    #    return
-
+def read_modules(lines):
     modules = {"output": Output(), "rx": Output()}
     for line in lines:
         src, dest = line.split(" -> ")
@@ -139,37 +85,49 @@ def part2(lines):
             if not isinstance(modules[destination], Conjunction):
                 continue
             modules[destination].most_recent[source] = 0
+    return modules
 
-    # source, target = 'xr', 'sg'
-    # source, target = 'mn', 'dh'
-    # source, target = 'pl', 'lm'
-    # source, target = 'xc', 'db'
-    for i in range(40):
+
+def part1(lines):
+    """
+    >>> part1(load_example(__file__, "20a"))
+    32000000
+    >>> part1(load_example(__file__, "20b"))
+    11687500
+    """
+    modules = read_modules(lines)
+    total_pulses = {0: 0, 1: 0}
+    for _ in range(1000):
+        pulses = [("button", "broadcaster", 0)]
+        modules["broadcaster"].pulse(None, 0)
+        total_pulses[0] += 1
+        while pulses:
+            src, dest, value = pulses.pop(0)
+            if dest in modules:
+                for d, v in modules[dest].pulse(src, value):
+                    total_pulses[v] += 1
+                    pulses.append((dest, d, v))
+    return total_pulses[0] * total_pulses[1]
+
+
+def find_first_high_pulse(lines, t):
+    modules = read_modules(lines)
+    for i in range(1, 2**12):
         pulses = [("button", "broadcaster", 0)]
         modules["broadcaster"].pulse(None, 0)
         while pulses:
             src, dest, value = pulses.pop(0)
-            #            print(src, "-", value, "->", dest)
             for d, v in modules[dest].pulse(src, value):
                 pulses.append((dest, d, v))
-        if modules["sg"].state == 1:
-            return i, modules["sg"].count
-        for t in ("sg", "dh", "lm", "db"):  # , 'rx'):
-            # if modules[t].state == 1:
-            print("%s: state: %s, %s times" % (t, modules[t].state, modules[t].count), end=" | ")
-            modules[t].count = 0
-        print()
+            if modules[t].state == 1:
+                return i
 
 
-#        print(" ".join("%s:%s" % (key, module.state) for key, module in modules.items()))
-#    return target, total_pulses
+def part2(lines):
+    return reduce(lcm, (find_first_high_pulse(lines, t) for t in ("sg", "dh", "lm", "db")))
 
 
 if __name__ == "__main__":
-    # print(part1(load_example(__file__, "20b")))
     data = load_input(__file__, 2023, "20")
-    # print(part1(data))
+    print(part1(data))
     print(part2(data))
-
-#  python3 day20.py > 20.dot
-#  dot -Tpng 20.dot > output.png
