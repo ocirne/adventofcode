7abbc7c8c98032a39db04531313aa3d05ad122cd
Enrico Pilz <kontakt@enricopilz.de>
2024-01-05 20:51:51 +0100

    py 2023/24 part2

--- python/src/aoc/year2023/day24.py
+++ python/src/aoc/year2023/day24.py
@@ -1,16 +1,17 @@
 from itertools import combinations
 
 from aoc.util import load_input, load_example
+from sage.all import solve, var
 
 LB, UB = 200_000_000_000_000, 400_000_000_000_000
 
 
-def read_vectors1(lines):
+def read_vectors(lines):
     for line in lines:
         p, v = line.split("@")
         px, py, pz = map(int, p.split(","))
         vx, vy, vz = map(int, v.split(","))
-        yield px, py, vx, vy
+        yield px, py, pz, vx, vy, vz
 
 
 def cross_point(v1, v2, lb, ub):
@@ -18,8 +19,8 @@ def cross_point(v1, v2, lb, ub):
     # ax + a * vx == bx + b * wx == cx
     # ay + a * vy == by + b * wy == cy
     """
-    ax, ay, vx, vy = v1
-    bx, by, wx, wy = v2
+    ax, ay, _, vx, vy, _ = v1
+    bx, by, _, wx, wy, _ = v2
     # parallel?
     if vx * wy == vy * wx:
         return False
@@ -39,16 +40,12 @@ def part1(lines, lb=LB, ub=UB):
     >>> part1(load_example(__file__, "24"), lb=7, ub=27)
     2
     """
-    vectors = read_vectors1(lines)
+    vectors = read_vectors(lines)
     return sum(cross_point(v1, v2, lb, ub) for v1, v2 in combinations(vectors, 2))
 
 
-def read_vectors2(lines):
-    for line in lines:
-        p, v = line.split("@")
-        px, py, pz = map(int, p.split(","))
-        vx, vy, vz = map(int, v.split(","))
-        yield px, py, pz, vx, vy, vz
+# because F821 undefined name 'x'
+# flake8: noqa
 
 
 def part2(lines):
@@ -56,13 +53,20 @@ def part2(lines):
     >>> part2(load_example(__file__, "24"))
     47
     """
-    vectors = list(read_vectors1(lines))
-    for v in vectors:
-        print(v)
+    vectors = list(read_vectors(lines))[:3]
+    var("x y z wx wy wz")
+    eqn = []
+    for i, (px, py, pz, vx, vy, vz) in enumerate(vectors):
+        t = var("t%d" % i)
+        eqn.append(px + t * vx == x + t * wx)
+        eqn.append(py + t * vy == y + t * wy)
+        eqn.append(pz + t * vz == z + t * wz)
+
+    sols = solve(eqn, [x, y, z, wx, wy, wz, t0, t1, t2], solution_dict=True)[0]
+    return sols[x] + sols[y] + sols[z]
 
 
 if __name__ == "__main__":
-    print(part2(load_example(__file__, "24")))
-    # data = load_input(__file__, 2023, "24")
-    # print(part1(data))
-    # print(part2(data))
+    data = load_input(__file__, 2023, "24")
+    print(part1(data))
+    print(part2(data))
