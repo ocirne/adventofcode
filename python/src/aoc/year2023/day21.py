49e72303e3537e3adffff77b854e2a2717587d13
Enrico Pilz <kontakt@enricopilz.de>
2024-01-06 20:27:06 +0100

    py 2023/21 refactor

--- python/src/aoc/year2023/day21.py
+++ python/src/aoc/year2023/day21.py
@@ -4,31 +4,32 @@ from aoc.util import load_input, load_example
 from sage.all import PolynomialRing, QQ
 
 
-def read_garden(lines):
-    garden, s = {}, None
-    for y, line in enumerate(lines):
-        for x, v in enumerate(line):
-            garden[x, y] = v
-            if v == "S":
-                s = x, y
-    return garden, s
+class Garden:
+    def __init__(self, lines):
+        self.width, self.height = len(lines[0]), len(lines)
+        self.plots, self.start = defaultdict(lambda: None), None
+        for y, line in enumerate(lines):
+            for x, v in enumerate(line):
+                self.plots[x, y] = v
+                if v == "S":
+                    self.start = x, y
 
+    def _neighbors(self, x, y):
+        for nx, ny in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
+            if self.plots[nx % self.width, ny % self.height] != "#":
+                yield nx, ny
 
-def neighbors(garden, x, y):
-    for nx, ny in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-        if (nx, ny) in garden and garden[nx, ny] != "#":
-            yield nx, ny
-
-
-def foo(garden, start, max_steps):
-    steps = {start}
-    for _ in range(max_steps):
-        new_steps = set()
-        for cx, cy in steps:
-            for nx, ny in neighbors(garden, cx, cy):
-                new_steps.add((nx, ny))
-        steps = new_steps
-    return len(steps)
+    def step_around(self, max_steps):
+        steps = {self.start}
+        reachable = [0]
+        for _ in range(max_steps):
+            new_steps = set()
+            for cx, cy in steps:
+                for nx, ny in self._neighbors(cx, cy):
+                    new_steps.add((nx, ny))
+            steps = new_steps
+            reachable.append(len(steps))
+        return reachable
 
 
 def part1(lines, steps=64):
@@ -36,50 +37,20 @@ def part1(lines, steps=64):
     >>> part1(load_example(__file__, "21"), steps=6)
     16
     """
-    garden, start = read_garden(lines)
-    return foo(garden, start, steps)
-
-
-def read_infinite_garden(lines):
-    w, h = len(lines[0]), len(lines)
-    garden, s = defaultdict(lambda: None), None
-    for y, line in enumerate(lines):
-        for x, v in enumerate(line):
-            garden[x, y] = v
-            if v == "S":
-                s = x, y
-    return w, h, garden, s
-
-
-def infinite_neighbors(w, h, garden, x, y):
-    for nx, ny in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-        if garden[nx % w, ny % h] != "#":
-            yield nx, ny
-
-
-def infinite_foo(w, h, garden, start, max_steps):
-    steps = {start}
-    s = []
-    for i in range(max_steps):
-        s.append(len(steps))
-        new_steps = set()
-        for cx, cy in steps:
-            for nx, ny in infinite_neighbors(w, h, garden, cx, cy):
-                new_steps.add((nx, ny))
-        steps = new_steps
-    s.append(len(steps))
-    return len(steps), s
+    garden = Garden(lines)
+    reachable = garden.step_around(steps)
+    return reachable[-1]
 
 
 def part2(lines, steps=26501365):
-    width, height, garden, start = read_infinite_garden(lines)
-    r, s = infinite_foo(width, height, garden, start, 328)
-    assert height == width == 131
-    assert steps % width == 65
+    garden = Garden(lines)
+    reachable = garden.step_around(328)
+    assert garden.height == garden.width == 131
+    assert steps % garden.width == 65
     assert 65 % 131 == 196 % 131 == 327 % 131 == 65
 
     R = PolynomialRing(QQ, "x")
-    points = [(i, s[i]) for i in (65, 196, 327)]
+    points = [(i, reachable[i]) for i in (65, 196, 327)]
     p = R.lagrange_polynomial(points)
     return p(steps)
 
