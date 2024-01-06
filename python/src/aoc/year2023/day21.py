03717d1e188d4b5c1a9627e5f1c1119f45d7ab45
Enrico Pilz <kontakt@enricopilz.de>
2024-01-06 19:56:29 +0100

    py 2023/21 part2

--- python/src/aoc/year2023/day21.py
+++ python/src/aoc/year2023/day21.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 from aoc.util import load_input, load_example
+from sage.all import PolynomialRing, QQ
 
 
 def read_garden(lines):
@@ -22,7 +23,6 @@ def neighbors(garden, x, y):
 def foo(garden, start, max_steps):
     steps = {start}
     for _ in range(max_steps):
-        print(len(steps), max_steps)
         new_steps = set()
         for cx, cy in steps:
             for nx, ny in neighbors(garden, cx, cy):
@@ -61,52 +61,30 @@ def infinite_foo(w, h, garden, start, max_steps):
     steps = {start}
     s = []
     for i in range(max_steps):
-        #        print('i', i, 'plots', len(steps))
         s.append(len(steps))
         new_steps = set()
         for cx, cy in steps:
             for nx, ny in infinite_neighbors(w, h, garden, cx, cy):
                 new_steps.add((nx, ny))
         steps = new_steps
-    #    print('i', max_steps, 'plots', len(steps))
     s.append(len(steps))
     return len(steps), s
 
 
-def rec(a):
-    if all(n == 0 for n in a):
-        print("***")
-        return 0
-    d = [s - p for p, s in zip(a[:-1], a[1:])]
-    return a[-1] + rec(d)
-
-
 def part2(lines, steps=26501365):
-    """
-    >>> part2(load_example(__file__, "21"), steps=6)
-    16
-    >>> part2(load_example(__file__, "21"), steps=10)
-    50
-    >>> part2(load_example(__file__, "21"), steps=50)
-    1594
-    >>> part2(load_example(__file__, "21"), steps=100)
-    6536
-    >>> part2(load_example(__file__, "21"), steps=500)
-    167004
-    >>> part2(load_example(__file__, "21"), steps=1000)
-    668697
-    >>> part2(load_example(__file__, "21"), steps=5000)
-    16733044
-    """
-    w, h, garden, start = read_infinite_garden(lines)
-    r, s = infinite_foo(w, h, garden, start, steps)
-    print("r", r)
-    print(s)
-    print(rec(s[200::2]))
+    width, height, garden, start = read_infinite_garden(lines)
+    r, s = infinite_foo(width, height, garden, start, 328)
+    assert height == width == 131
+    assert steps % width == 65
+    assert 65 % 131 == 196 % 131 == 327 % 131 == 65
+
+    R = PolynomialRing(QQ, "x")
+    points = [(i, s[i]) for i in (65, 196, 327)]
+    p = R.lagrange_polynomial(points)
+    return p(steps)
 
 
 if __name__ == "__main__":
-    print(part2(load_example(__file__, "21"), steps=64))
-    # data = load_input(__file__, 2023, "21")
-    # print(part1(data))
-    # print(part2(data))
+    data = load_input(__file__, 2023, "21")
+    print(part1(data))
+    print(part2(data))
