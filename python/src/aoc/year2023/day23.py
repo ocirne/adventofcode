13e1a361583f74021e752d05bc43f3c78ec99da6
Enrico Pilz <kontakt@enricopilz.de>
2024-01-06 21:48:28 +0100

    py 2023/23 refactor

--- python/src/aoc/year2023/day23.py
+++ python/src/aoc/year2023/day23.py
@@ -2,58 +2,108 @@ from functools import lru_cache
 from aoc.util import load_input, load_example
 
 
-def read_trail(lines):
-    trail, start, end = {}, None, None
-    for y, line in enumerate(lines):
-        for x, v in enumerate(line):
-            if v in ".<>^v":
-                trail[x, y] = v
-                if y == 0:
-                    start = x, y
-                if y == len(lines) - 1:
-                    end = x, y
-    return trail, start, end
+class GridTrail:
 
+    ANTI = {
+        "<": ">",
+        ">": "<",
+        "^": "v",
+        "v": "^",
+    }
 
-A = {
-    "<": ">",
-    ">": "<",
-    "^": "v",
-    "v": "^",
-}
+    def __init__(self, lines):
+        self.trail, self.start, self.end = {}, None, None
+        for y, line in enumerate(lines):
+            for x, v in enumerate(line):
+                if v in ".<>^v":
+                    self.trail[x, y] = v
+                    if y == 0:
+                        self.start = x, y
+                    if y == len(lines) - 1:
+                        self.end = x, y
 
-
-def neighbors(trail, current_node):
-    (x, y), d, g, visited = current_node
-    for next_pos, nd in (((x - 1, y), "<"), ((x + 1, y), ">"), ((x, y - 1), "^"), ((x, y + 1), "v")):
-        if nd == A[d]:
-            continue
-        if next_pos not in trail:
-            continue
-        if trail[x, y] != "." and trail[x, y] != nd:
-            continue
-        if str(next_pos) in visited:
-            continue
-        yield next_pos, nd, g + 1, visited + str(next_pos)
-
-
-def foo(trail, start, end):
-    open_set = {(start, "v", 0, "")}
-    closed_set = set()
-    while open_set:
-        # print(len(open_set))
-        current_node = open_set.pop()
-        print(current_node[2])
-        if current_node[0] == end:
-            print(current_node[2], current_node[3])
-            yield current_node[2]
-        if current_node in closed_set:
-            continue
-        closed_set.add(current_node)
-        for next_node in neighbors(trail, current_node):
-            if next_node in closed_set:
+    def _neighbors(self, current_node):
+        (x, y), d, g, visited = current_node
+        for next_pos, nd in (((x - 1, y), "<"), ((x + 1, y), ">"), ((x, y - 1), "^"), ((x, y + 1), "v")):
+            if nd == self.ANTI[d]:
                 continue
-            open_set.add(next_node)
+            if next_pos not in self.trail:
+                continue
+            if self.trail[x, y] != "." and self.trail[x, y] != nd:
+                continue
+            if str(next_pos) in visited:
+                continue
+            yield next_pos, nd, g + 1, visited + str(next_pos)
+
+    def dijkstra_directed(self):
+        open_set = {(self.start, "v", 0, "")}
+        closed_set = set()
+        while open_set:
+            current_node = open_set.pop()
+            if current_node[0] == self.end:
+                yield current_node[2]
+            if current_node in closed_set:
+                continue
+            closed_set.add(current_node)
+            for next_node in self._neighbors(current_node):
+                if next_node in closed_set:
+                    continue
+                open_set.add(next_node)
+
+
+class GraphTrail:
+    def __init__(self, lines, grid: GridTrail):
+        self.grid = grid
+        self.trail = self._grid_to_graph(lines)
+        self.result = -1
+        self.end = grid.end
+
+    def _grid_neighbors(self, x, y):
+        return (pos for pos in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)) if pos in self.grid.trail)
+
+    def _find_graph_neighbors(self, vertices, start):
+        open_set = {start}
+        visited = set()
+        g = {start: 0}
+        while open_set:
+            current_pos = open_set.pop()
+            if current_pos in visited:
+                continue
+            visited.add(current_pos)
+            for next_pos in self._grid_neighbors(*current_pos):
+                if next_pos in visited:
+                    continue
+                if next_pos in vertices:
+                    yield next_pos, g[current_pos] + 1
+                else:
+                    g[next_pos] = g[current_pos] + 1
+                    open_set.add(next_pos)
+
+    def _grid_to_graph(self, lines):
+        w, h = len(lines[0]), len(lines)
+        vertices = {self.grid.start, self.grid.end}
+        for y in range(h):
+            for x in range(w):
+                if (x, y) in self.grid.trail:
+                    s = sum(1 for _ in self._grid_neighbors(x, y))
+                    if s > 2:
+                        vertices.add((x, y))
+        graph = {v: [] for v in vertices}
+        for vertex_start in vertices:
+            for vertex_neighbor in self._find_graph_neighbors(vertices, vertex_start):
+                graph[vertex_start].append(vertex_neighbor)
+        return graph
+
+    @lru_cache
+    def dfs(self, current, steps=0, visited=""):
+        if current == self.end:
+            if self.result < steps:
+                self.result = steps
+        else:
+            for neighbor, edge in self.trail[current]:
+                if str(current) in visited:
+                    continue
+                self.dfs(neighbor, steps + edge, visited + str(current))
 
 
 def part1(lines):
@@ -61,83 +111,8 @@ def part1(lines):
     >>> part1(load_example(__file__, "23"))
     94
     """
-    trail, start, end = read_trail(lines)
-    return max(foo(trail, start, end))
-
-
-def neighbors2(trail, current_node):
-    (x, y), d, g, visited = current_node
-    for next_pos, nd in (((x - 1, y), "<"), ((x + 1, y), ">"), ((x, y - 1), "^"), ((x, y + 1), "v")):
-        if nd == A[d]:
-            continue
-        if next_pos not in trail:
-            continue
-        # if trail[x, y] != "." and trail[x, y] != nd:
-        #    continue
-        if str(next_pos) in visited:
-            continue
-        yield next_pos, nd, g + 1, visited + str(next_pos)
-
-
-def simple_neighbors(trail, current_pos):
-    x, y = current_pos
-    return (pos for pos in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)) if pos in trail)
-
-
-def find_graph_neighbors(trail, vertices, start):
-    open_set = {start}
-    visited = set()
-    g = {start: 0}
-    while open_set:
-        current_pos = open_set.pop()
-        if current_pos in visited:
-            continue
-        visited.add(current_pos)
-        for next_pos in simple_neighbors(trail, current_pos):
-            if next_pos in visited:
-                continue
-            if next_pos in vertices:
-                yield next_pos, g[current_pos] + 1
-            else:
-                g[next_pos] = g[current_pos] + 1
-                open_set.add(next_pos)
-
-
-def simplify_trail(lines, trail, start, end):
-    w, h = len(lines[0]), len(lines)
-    vertices = {start, end}
-    for y in range(h):
-        for x in range(w):
-            if (x, y) in trail:
-                s = sum(1 for pos in simple_neighbors(trail, (x, y)))
-                if s > 2:
-                    vertices.add((x, y))
-    graph = {v: [] for v in vertices}
-    for vertex_start in vertices:
-        for vertex_neighbor in find_graph_neighbors(trail, vertices, vertex_start):
-            graph[vertex_start].append(vertex_neighbor)
-    for v, e in graph.items():
-        print(v, e)
-    return graph
-
-
-class Foo:
-    def __init__(self, graph, end):
-        self.graph = graph
-        self.end = end
-        self.result = -1
-
-    @lru_cache
-    def dfs(self, current, steps=0, visited=""):
-        if current == self.end:
-            if self.result < steps:
-                self.result = steps
-                print(steps)
-        else:
-            for neighbor, edge in self.graph[current]:
-                if str(current) in visited:
-                    continue
-                self.dfs(neighbor, steps + edge, visited + str(current))
+    trail = GridTrail(lines)
+    return max(trail.dijkstra_directed())
 
 
 def part2(lines):
@@ -145,19 +120,13 @@ def part2(lines):
     >>> part2(load_example(__file__, "23"))
     154
     """
-    trail, start, end = read_trail(lines)
-    # start_node = (start, "v", 0, "")
-    #    return max(foo2(trail, start_node, end))
-    graph = simplify_trail(lines, trail, start, end)
-    foo3 = Foo(graph, end)
-    foo3.dfs(start)
-    return foo3.result
+    grid = GridTrail(lines)
+    graph = GraphTrail(lines, grid)
+    graph.dfs(grid.start)
+    return graph.result
 
 
 if __name__ == "__main__":
-    # print(part2(load_example(__file__, "23")))
-    print(part2(load_input(__file__, 2023, "23")))
-
-    # data = load_input(__file__, 2023, "23")
-    # print(part1(data))
-    # print(part2(data))
+    data = load_input(__file__, 2023, "23")
+    print(part1(data))
+    print(part2(data))
