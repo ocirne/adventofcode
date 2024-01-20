e72e04080a17d72485fb1e9b4991e1114ed27f79
Enrico Pilz <kontakt@enricopilz.de>
2024-01-20 11:06:41 +0100

    kt 2019/15 refactor

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day15.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day15.kt
@@ -5,83 +5,68 @@ import java.util.PriorityQueue
 
 class Day15(val lines: List<String>) : AocChallenge(2019, 15) {
 
-    private class Droid(val program: String) {
+    private data class Position(val x: Int, val y: Int)
 
-        data class Position(val x: Int, val y: Int)
+    private class Node(val steps: Int, val position: Position, val program: IntCodeEmulator2019): Comparable<Node> {
+        fun isEnd(): Boolean {
+            return steps > 0 && program.getLastOutput() == 2L
+        }
 
-        class Node(val steps: Int, val position: Position, val program: IntCodeEmulator2019): Comparable<Node> {
-            fun isEnd(): Boolean {
-                return steps > 0 && program.getLastOutput() == 2L
-            }
-
-            fun neighbors(): Sequence<Node> {
-                val (x, y) = position
-                return sequence {
-                    for ((direction, dx, dy) in listOf(
-                        Triple(1L, 0, -1),
-                        Triple(2L, 0, 1),
-                        Triple(3L, -1, 0),
-                        Triple(4L, 1, 0),
-                    )) {
-                        val copy = program.copy()
-                        copy.addInput(direction)
-                        copy.tick()
-                        if (copy.getLastOutput() != 0L) {
-                            yield(Node(steps + 1, Position(x + dx, y - dy), copy))
-                        }
+        fun neighbors(): Sequence<Node> {
+            val (x, y) = position
+            return sequence {
+                for ((direction, dx, dy) in listOf(
+                    Triple(1L, 0, -1),
+                    Triple(2L, 0, 1),
+                    Triple(3L, -1, 0),
+                    Triple(4L, 1, 0),
+                )) {
+                    val copy = program.copy()
+                    copy.addInput(direction)
+                    copy.tick()
+                    if (copy.getLastOutput() != 0L) {
+                        yield(Node(steps + 1, Position(x + dx, y - dy), copy))
                     }
                 }
             }
-
-            override fun compareTo(other: Node): Int {
-                return steps.compareTo(other.steps)
-            }
         }
 
-        fun exploreMap(): Node {
-            val openHeap = PriorityQueue<Node>()
-            val visited = mutableSetOf<Position>()
-            openHeap.add(Node(0, Position(0, 0), IntCodeEmulator2019(program)))
-            while (openHeap.isNotEmpty()) {
-                val node = openHeap.remove()
-                if (node.isEnd()) {
-                    return node
-                }
-                visited.add(node.position)
-                for (neighbor in node.neighbors()) {
-                    if (visited.contains(neighbor.position)) {
-                        continue
-                    }
-                    openHeap.add(neighbor)
-                }
-            }
-            throw IllegalStateException()
+        override fun compareTo(other: Node): Int {
+            return steps.compareTo(other.steps)
         }
+    }
 
-        fun fillMap2(node: Node): Int {
-            val openHeap = PriorityQueue<Node>()
-            val visited = mutableMapOf<Position, Int>()
-            openHeap.add(Node(0, node.position, node.program))
-            while (openHeap.isNotEmpty()) {
-                val node = openHeap.remove()
-                visited[node.position] = node.steps
-                for (neighbor in node.neighbors()) {
-                    if (visited.contains(neighbor.position)) {
-                        continue
-                    }
-                    openHeap.add(neighbor)
-                }
+    private fun exploreMap(startNode: Node): Node {
+        val openHeap = PriorityQueue<Node>()
+        val visited = mutableMapOf<Position, Node>()
+        openHeap.add(Node(0, startNode.position, startNode.program))
+        while (openHeap.isNotEmpty()) {
+            val node = openHeap.remove()
+            if (node.isEnd()) {
+                return node
             }
-            return visited.values.max()
+            visited[node.position] = node
+            for (neighbor in node.neighbors()) {
+                if (visited.contains(neighbor.position)) {
+                    continue
+                }
+                openHeap.add(neighbor)
+            }
         }
+        return visited.values.maxBy { it.steps }
+    }
+
+    private fun findOxygenSystem(): Node {
+        val startNode = Node(0, Position(0, 0), IntCodeEmulator2019(lines.first()))
+        return exploreMap(startNode)
     }
 
     override fun part1(): Int {
-        return Droid(lines.first()).exploreMap().steps
+        return findOxygenSystem().steps
     }
 
     override fun part2(): Int {
-        val oxygenSystem = Droid(lines.first()).exploreMap()
-        return Droid(lines.first()).fillMap2(oxygenSystem)
+        val oxygenSystem = findOxygenSystem()
+        return exploreMap(oxygenSystem).steps
     }
 }
