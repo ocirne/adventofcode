9eead570afffbd95cc3f6e2564763ad5822b0546
Enrico Pilz <kontakt@enricopilz.de>
2024-01-21 19:24:26 +0100

    kt 2019/16 refactor

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day16.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day16.kt
@@ -5,44 +5,37 @@ import kotlin.math.absoluteValue
 
 class Day16(val lines: List<String>) : AocChallenge(2019, 16) {
 
-    fun basePattern(x: Int, y: Int): Int {
-        return listOf(0, 1, 0, -1)[(x+1) / y % 4]
-    }
+    private val basePattern = arrayOf(0, 1, 0, -1)
 
-    fun newSignal(signal: List<Int>): List<Int> {
+    private fun nextSignal(signal: IntArray): IntArray {
         return IntRange(1, signal.size).map { y ->
-            signal.mapIndexed { x, value2 ->
-                basePattern(x, y) * value2
+            signal.mapIndexed { x, value ->
+                basePattern[(x+1) / y % 4] * value
             }.sum().absoluteValue % 10
-        }
+        }.toIntArray()
     }
 
-    fun foo(signalString: String, phases: Int=100): String {
-        var signal = signalString.toCharArray().map { it.digitToInt() }
+    fun naive(signalString: String, phases: Int=100): String {
+        var signal = signalString.map { it.digitToInt() }.toIntArray()
         repeat(phases) {
-            signal = newSignal(signal)
+            signal = nextSignal(signal)
         }
         return signal.take(8).joinToString("")
     }
 
     override fun part1(): String {
-        return foo(lines.first())
-    }
-    
-    fun foo2(signalString: String, phases: Int=100): String {
-        val skip = signalString.take(7).toInt()
-        val signal = signalString.repeat(10000).toCharArray().drop(skip).map { it.digitToInt() }.reversed().toIntArray()
-        repeat(100) {
-            var s = 0
-            for (i in 0 until signal.size) {
-                s += signal[i]
-                signal[i] = s % 10
-            }
-        }
-        return signal.reversed().take(8).joinToString("")
+        return naive(lines.first())
     }
 
     override fun part2(): String {
-        return foo2(lines.first())
+        val signalString = lines.first()
+        val skip = signalString.take(7).toInt()
+        val signal = signalString.repeat(10000).drop(skip).map { it.digitToInt() }.toIntArray()
+        repeat(100) {
+            (signal.size-2 downTo 0).forEach { i ->
+                signal[i] = (signal[i] + signal[i+1]) % 10
+            }
+        }
+        return signal.take(8).joinToString("")
     }
 }
--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day16Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day16Test.kt
@@ -9,21 +9,13 @@ internal class Day16Test : AocTest(Day16::class, loadExample = false) {
     @Test
     fun `examples year 2019 day 16 part 1`() {
         subject as Day16
-        subject.foo("12345678", 1) shouldBe "48226158"
-        subject.foo("12345678", 2) shouldBe "34040438"
-        subject.foo("12345678", 3) shouldBe "03415518"
-        subject.foo("12345678", 4) shouldBe "01029498"
+        subject.naive("12345678", 1) shouldBe "48226158"
+        subject.naive("12345678", 2) shouldBe "34040438"
+        subject.naive("12345678", 3) shouldBe "03415518"
+        subject.naive("12345678", 4) shouldBe "01029498"
 
-        subject.foo("80871224585914546619083218645595") shouldBe "24176176"
-        subject.foo("19617804207202209144916044189917") shouldBe "73745418"
-        subject.foo("69317163492948606335995924319873") shouldBe "52432133"
-    }
-
-    @Test
-    fun `examples year 2019 day 16 part 2`() {
-        subject as Day16
-        subject.foo2("03036732577212944063491565474664") shouldBe "84462026"
-        subject.foo2("02935109699940807407585447034323") shouldBe "78725270"
-        subject.foo2("03081770884921959731165446850517") shouldBe "53553731"
+        subject.naive("80871224585914546619083218645595") shouldBe "24176176"
+        subject.naive("19617804207202209144916044189917") shouldBe "73745418"
+        subject.naive("69317163492948606335995924319873") shouldBe "52432133"
     }
 }
