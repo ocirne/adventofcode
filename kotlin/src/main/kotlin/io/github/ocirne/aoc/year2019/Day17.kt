a15c060f25bbcbe9ca4e1393a7ed0223079fe959
Enrico Pilz <kontakt@enricopilz.de>
2024-01-22 21:47:53 +0100

    kt 2019/17 part2 pen and paper

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day17.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day17.kt
@@ -4,41 +4,53 @@ import io.github.ocirne.aoc.AocChallenge
 
 class Day17(val lines: List<String>) : AocChallenge(2019, 17) {
 
-    private fun runProgram(): Map<Pair<Long, Long>, Char> {
-        val program = IntCodeEmulator2019(lines.first())
-        val cameraOutput = mutableMapOf<Pair<Long, Long>, Char>()
+    private fun runProgram(program: IntCodeEmulator2019, print: Boolean=false): Map<Pair<Long, Long>, Char> {
+        val scaffolds = mutableMapOf<Pair<Long, Long>, Char>()
         var x = 0L
         var y = 0L
         while (program.tick() != IntCodeEmulator2019.Companion.ReturnCode.STOP) {
-            val c = program.getLastOutput().toInt()
-            if (c == 10) {
+            val c = program.getLastOutput().toInt().toChar()
+            if (print) {
+                print(c)
+            }
+            if (c == '\n') {
                 y += 1
                 x = 0
             } else {
-                if (c == 35) {
-                    cameraOutput[x to y] = c.toChar()
+                if (c == '#') {
+                    scaffolds[x to y] = c
                 }
                 x += 1
             }
         }
-        return cameraOutput
+        return scaffolds
     }
 
-    fun foo(cameraOutput: Map<Pair<Long, Long>, Char>): Long {
-        return cameraOutput.filterKeys { (x, y) ->
-            cameraOutput[x - 1 to y] == '#' &&
-                    cameraOutput[x + 1 to y] == '#' &&
-                    cameraOutput[x to y - 1] == '#' &&
-                    cameraOutput[x to y + 1] == '#'
+    fun findIntersections(scaffolds: Map<Pair<Long, Long>, Char>): Long {
+        return scaffolds.filterKeys { (x, y) ->
+            scaffolds[x - 1 to y] == '#' &&
+            scaffolds[x + 1 to y] == '#' &&
+            scaffolds[x to y - 1] == '#' &&
+            scaffolds[x to y + 1] == '#'
         }.keys.sumOf { (x, y) -> x * y }
     }
 
     override fun part1(): Long {
-        val cameraOutput = runProgram()
-        return foo(cameraOutput)
+        val scaffolds = runProgram(IntCodeEmulator2019(lines.first()))
+        return findIntersections(scaffolds)
     }
 
     override fun part2(): Int {
-        return -1
+        val penAndPaper = """B,A,B,C,A,B,C,A,B,C
+            R,8,R,4,L,12
+            L,12,L,12,L,6,L,6
+            L,12,L,6,R,12,R,8
+            n
+            """
+        val vacuum = IntCodeEmulator2019(lines.first())
+        vacuum.program[0L] = 2L
+        penAndPaper.filter { it != ' ' }.forEach { vacuum.addInput(it.code.toLong()) }
+        runProgram(vacuum)
+        return vacuum.getLastOutput().toInt()
     }
 }
--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day17Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day17Test.kt
@@ -7,26 +7,22 @@ import org.junit.jupiter.api.Test
 
 internal class Day17Test : AocTest(Day17::class, loadExample = false) {
 
-    private fun foo(): Map<Pair<Long, Long>, Char> {
+    private fun exampleToScaffolds(): Map<Pair<Long, Long>, Char> {
         val lines = loadExample(2019, "17")
-        val result = mutableMapOf<Pair<Long, Long>, Char>()
+        val scaffolds = mutableMapOf<Pair<Long, Long>, Char>()
         lines.forEachIndexed { y, line ->
             line.forEachIndexed { x, c ->
                 if (c == '#') {
-                    result[x.toLong() to y.toLong()] = c
+                    scaffolds[x.toLong() to y.toLong()] = c
                 }
             }
         }
-        return result
+        return scaffolds
     }
 
     @Test
     fun `examples year 2019 day 17 part 1`() {
         subject as Day17
-        subject.foo(foo()) shouldBe 76
-    }
-
-    @Test
-    fun `examples year 2019 day 17 part 2`() {
+        subject.findIntersections(exampleToScaffolds()) shouldBe 76
     }
 }
