de1f4d2f5814dddbf2eaf2ab6cd25162250ee931
Enrico Pilz <kontakt@enricopilz.de>
2024-01-23 21:36:48 +0100

    kt 2019/19 part2 + refactor

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day19.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day19.kt
@@ -4,24 +4,65 @@ import io.github.ocirne.aoc.AocChallenge
 
 class Day19(val lines: List<String>) : AocChallenge(2019, 19) {
 
-    override fun part1(): Int {
-        var total = 0
-        for (y in 0L .. 49L) {
-            for (x in 0L .. 49L) {
-                val program = IntCodeEmulator2019(lines.first())
-                program.addInput(x)
-                program.addInput(y)
-                program.tick()
-                val c = program.getLastOutput().toInt()
-                print(if (c == 1) '#' else '.')
-                if (c == 1) total ++
-            }
-            println()
-        }
-        return total
+    private val m = 10_000L
+
+    private fun beamOracle(x: Long, y: Long): Int {
+        val program = IntCodeEmulator2019(lines.first())
+        program.addInput(x)
+        program.addInput(y)
+        program.tick()
+        return program.getLastOutput().toInt()
     }
 
-    override fun part2(): Int {
-        return -1
+    override fun part1(): Int {
+        return LongRange(0L, 49L).sumOf { x ->
+            LongRange(0L, 49L).sumOf { y ->
+                beamOracle(x, y)
+            }
+        }
+    }
+
+    private fun checkResult(x: Long, y: Long): Boolean {
+        return beamOracle(x+99, y) == 1 && beamOracle(x+100, y) == 0 &&
+                beamOracle(x, y+99) == 1 && beamOracle(x, y+100) == 0
+    }
+
+    private fun findParameters(): List<Long> {
+        return sequence {
+            var inBeam = false
+            for (y in 0L..m) {
+                val c = beamOracle(m, y)
+                if (inBeam) {
+                    if (c == 0) {
+                        yield(y - 1)
+                        break
+                    }
+                } else {
+                    if (c == 1) {
+                        yield(y)
+                        inBeam = true
+                    }
+                }
+            }
+        }.take(2).toList()
+    }
+
+    private fun findSuitableSpots(cx: Long, cy: Long): Sequence<Long> {
+        return sequence {
+            for (x in cx - 5..cx + 5) {
+                for (y in cy - 5..cy + 5) {
+                    if (checkResult(x, y)) {
+                        yield(x * m + y)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun part2(): Long {
+        val (a, b) = findParameters()
+        val cx = 99 * (m + a) / ( b - a)
+        val cy = a * (cx + 99) / m
+        return findSuitableSpots(cx, cy).min()
     }
 }
