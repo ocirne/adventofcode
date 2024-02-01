14bf7a6f6e9fe3aa6362bd4041a2db0d13cc8b40
Enrico Pilz <kontakt@enricopilz.de>
2024-02-01 21:47:29 +0100

    kt 2019/24 part2

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day24.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day24.kt
@@ -6,7 +6,13 @@ typealias Bugs = Set<Pair<Int, Int>>
 
 typealias MutableBugs = MutableSet<Pair<Int, Int>>
 
+typealias PlutoBugs = Set<Triple<Int, Int, Int>>
+
+typealias MutablePlutoBugs = MutableSet<Triple<Int, Int, Int>>
+
 fun mutableBugs(): MutableBugs = mutableSetOf()
+
+fun mutablePlutoBugs(): MutablePlutoBugs = mutableSetOf()
 
 class Day24(val lines: List<String>) : AocChallenge(2019, 24) {
 
@@ -74,10 +80,8 @@ class Day24(val lines: List<String>) : AocChallenge(2019, 24) {
         var bugs = readBugs()
         val seen = mutableSetOf<Int>()
         while (true) {
-          //  printBugs(bugs)
             val score = calculateScore(bugs)
             if (score in seen) {
-                println(seen.size)
                 return score
             }
             seen.add(score)
@@ -85,7 +89,136 @@ class Day24(val lines: List<String>) : AocChallenge(2019, 24) {
         }
     }
 
+    fun plutonize(bugs: Bugs): PlutoBugs {
+        return bugs.map { (x, y) -> Triple(x, y, 0) }.toSet()
+    }
+
+    data class Foo(val dx: Int,
+                   val dy: Int,
+                   val o: (Int, Int) -> Boolean,
+                   val i: (Int, Int) -> Boolean,
+                   val ob: Pair<Int, Int>) {
+        fun isLookAtOuterLevel(x: Int, y: Int): Boolean {
+            return o(x, y)
+        }
+
+        fun outerLevelBug(z: Int): Triple<Int, Int, Int> {
+            val (obx, oby) = ob
+            return Triple(obx, oby, z-1)
+        }
+
+        fun isLookAtInnerLevel(x: Int, y: Int): Boolean {
+            return i(x, y)
+        }
+
+        fun innerLevelBugs(z: Int): Sequence<Triple<Int, Int, Int>> {
+            return sequence {
+                if (dx == -1) {  // left
+                    for (i in 0..4) {
+                        yield(Triple(4, i, z + 1))
+                    }
+                }
+                if (dx == 1) {  // right
+                    for (i in 0..4) {
+                        yield(Triple(0, i, z + 1))
+                    }
+                }
+                if (dy == -1) {  // up
+                    for (i in 0..4) {
+                        yield(Triple(i, 4, z + 1))
+                    }
+                }
+                if (dy == 1) {  // down
+                    for (i in 0..4) {
+                        yield(Triple(i, 0, z + 1))
+                    }
+                }
+            }
+        }
+    }
+
+    private val NSWE_PLUTO = listOf(
+        // up
+        Foo(0, -1, {x, y -> y == 0}, {x, y -> x == 2 && y == 3}, 2 to 1),
+        // down
+        Foo(0, 1, {x, y -> y == 4}, {x, y -> x == 2 && y == 1}, 2 to 3),
+        // left
+        Foo(-1, 0, {x, y -> x == 0}, {x, y -> x == 3 && y == 2}, 1 to 2),
+        // right
+        Foo(1, 0, {x, y -> x == 4}, {x, y -> x == 1 && y == 2}, 3 to 2),
+    )
+
+    private fun countAdjacentBugsPluto(bugs: PlutoBugs, x: Int, y: Int, z: Int): Int {
+        var total = 0
+        for (a in NSWE_PLUTO) {
+            if (a.isLookAtOuterLevel(x, y)) {
+                if (a.outerLevelBug(z) in bugs) {
+                    total += 1
+                }
+            } else if (a.isLookAtInnerLevel(x, y)) {
+                for (b in a.innerLevelBugs(z)) {
+                    if (b in bugs) {
+                        total += 1
+                    }
+                }
+            } else { // same level
+                if (Triple(x+a.dx, y+a.dy, z) in bugs) {
+                    total += 1
+                }
+            }
+        }
+        return total
+    }
+
+    private fun nextStatePluto(bugs: PlutoBugs, i: Int): PlutoBugs {
+        val nextBugs = mutablePlutoBugs()
+        IntRange(0, 4).forEach { x ->
+            IntRange(0, 4).forEach { y ->
+                IntRange(-i, i).forEach { z ->
+                    if (!(x == 2 && y == 2)) {
+                        val adjacent = countAdjacentBugsPluto(bugs, x, y, z)
+                        val t = Triple(x, y, z)
+                        if (t in bugs && adjacent == 1) {
+                            nextBugs.add(t)
+                        }
+                        if (t !in bugs && adjacent in 1..2) {
+                            nextBugs.add(t)
+                        }
+                    }
+                }
+            }
+        }
+        return nextBugs
+    }
+
+    private fun printPlutoBugs(bugs: PlutoBugs) {
+        println()
+        for (z in -7 .. 7) {
+            println("z = $z:")
+            for (y in 0..4) {
+                for (x in 0..4) {
+                    print(if (Triple(x, y, z) in bugs) '#' else '.')
+                }
+                println()
+            }
+            println()
+        }
+    }
+
+    private fun countBugs(bugs: PlutoBugs): Int {
+        return bugs.size
+    }
+
+    fun simulate(minutes: Int): Int {
+        var bugs = plutonize(readBugs())
+        repeat(minutes) {
+            bugs = nextStatePluto(bugs, it+1)
+        }
+        printPlutoBugs(bugs)
+        return countBugs(bugs)
+    }
+
     override fun part2(): Int {
-        return -1
+        return simulate(200)
     }
 }
--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day24Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day24Test.kt
@@ -1,5 +1,14 @@
 package io.github.ocirne.aoc.year2019
 
 import io.github.ocirne.aoc.AocTest
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 
-internal class Day24Test : AocTest(Day24::class, expectedPart1 = 2129920)
+internal class Day24Test : AocTest(Day24::class, expectedPart1 = 2129920) {
+
+    @Test
+    fun `examples year 2019 day 24 part 2`() {
+        subject as Day24
+        subject.simulate(10) shouldBe 99
+    }
+}
