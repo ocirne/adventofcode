d20bee3fb13af34c4f3b71782e70fdba614ece37
Enrico Pilz <kontakt@enricopilz.de>
2024-02-01 18:38:13 +0100

    kt 2019/22 putzen

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day22.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day22.kt
@@ -2,8 +2,7 @@ package io.github.ocirne.aoc.year2019
 
 import io.github.ocirne.aoc.AocChallenge
 import java.math.BigInteger
-import java.math.BigInteger.ONE
-import java.math.BigInteger.valueOf
+import java.math.BigInteger.*
 
 class Day22(val lines: List<String>) : AocChallenge(2019, 22) {
 
@@ -13,177 +12,75 @@ class Day22(val lines: List<String>) : AocChallenge(2019, 22) {
 
         abstract fun cutN(acc: T, n: Int): T
 
-        abstract fun dealWithIncrementP(acc: T, p: Int): T
+        abstract fun dealWithIncrementP(acc: T, n: Int): T
 
         fun applyOneLine(shuffle: String, acc: T): T {
-            return when {
-                shuffle == "deal into new stack" -> dealIntoNewStack(acc)
-                shuffle.startsWith("cut") -> {
-                    val n = shuffle.split(' ').last().toInt()
-                    cutN(acc, n)
-                }
-
-                shuffle.startsWith("deal with increment") -> {
-                    val p = shuffle.split(' ').last().toInt()
-                    dealWithIncrementP(acc, p)
-                }
-                else -> throw IllegalArgumentException(shuffle)
+            if (shuffle.startsWith("deal into new stack")) {
+                return dealIntoNewStack(acc)
+            }
+            val n = shuffle.split(' ').last().toInt()
+            return if (shuffle.startsWith("cut")) {
+                cutN(acc, n)
+            } else { // must be startsWith("deal with increment")
+                dealWithIncrementP(acc, n)
             }
         }
 
         fun shuffle(lines: List<String>, accIn: T): T {
-            var acc = accIn
-            for (line in lines) {
-                acc = applyOneLine(line, acc)
-            }
-            return acc
+            return lines.fold(accIn) { acc, line -> applyOneLine(line, acc) }
         }
     }
 
-    private class NaiveShuffler: Shuffler<List<Int>>() {
+    private class IntShuffler(val m: Int): Shuffler<Pair<Int, Int>>() {
 
-        override fun dealIntoNewStack(acc: List<Int>): List<Int> {
-            return acc.reversed()
+        override fun dealIntoNewStack(acc: Pair<Int, Int>): Pair<Int, Int> {
+            val (a, b) = acc
+            return (-a).mod(m) to (-b-1).mod(m)
         }
 
-        override fun cutN(acc: List<Int>, n: Int): List<Int> {
-            assert(n != 0)
-            return if (n > 0) {
-                acc.drop(n) + acc.take(n)
-            } else {
-                acc.drop(acc.size + n) + acc.take(acc.size + n)
-            }
+        override fun cutN(acc: Pair<Int, Int>, n: Int): Pair<Int, Int> {
+            val (a, b) = acc
+            return a to (b - n).mod(m)
         }
 
-        override fun dealWithIncrementP(acc: List<Int>, p: Int): List<Int> {
-            val nextDeck = acc.toList().toMutableList()
-            acc.indices.forEach { i ->
-                nextDeck[(i * p) % acc.size] = acc[i]
-            }
-            return nextDeck
+        override fun dealWithIncrementP(acc: Pair<Int, Int>, n: Int): Pair<Int, Int> {
+            val (a, b) = acc
+            return (n*a).mod(m) to (n*b).mod(m)
         }
     }
 
-    private class MathShuffler(val m: Int): Shuffler<Int>() {
+    private class BigIntegerShuffler(val m: BigInteger): Shuffler<Pair<BigInteger, BigInteger>>() {
 
-        override fun dealIntoNewStack(acc: Int): Int {
-            return (- acc - 1).mod(m)
+        override fun dealIntoNewStack(acc: Pair<BigInteger, BigInteger>): Pair<BigInteger, BigInteger> {
+            val (a, b) = acc
+            return (-a).mod(m) to (-b- ONE).mod(m)
         }
 
-        override fun cutN(acc: Int, n: Int): Int {
-            return (acc - n).mod(m)
+        override fun cutN(acc: Pair<BigInteger, BigInteger>, n: Int): Pair<BigInteger, BigInteger> {
+            val (a, b) = acc
+            return a to (b - valueOf(n.toLong())).mod(m)
         }
 
-        override fun dealWithIncrementP(acc: Int, p: Int): Int {
-            return (acc * p).mod(m)
-        }
-    }
-
-    private class MathShuffler2(val m: Long): Shuffler<Pair<Long, Long>>() {
-
-        /**
-         * x1 = a1 * x0 + b1
-         * x2 = -x1 -1 = -1 * x1 - 1
-         *
-         * x2 = -1 * x1 - 1 = -1 * (a1 * x0 + b1) - 1 = -1 * a1 * x0 - b1 - 1 = -a1 * x0 + (-b1 - 1)
-         */
-        override fun dealIntoNewStack(acc: Pair<Long, Long>): Pair<Long, Long> {
-            val (a1, b1) = acc
-            return (-a1).mod(m) to (-b1-1).mod(m)
-        }
-
-        /**
-         * x1 = a1 * x0 + b1
-         * x2 = x1 - n
-         *
-         * x2 = x1 - n = (a1 * x0 + b1) - n = a1 * x0 + (b1 - n)
-         */
-        override fun cutN(acc: Pair<Long, Long>, n: Int): Pair<Long, Long> {
-            val (a1, b1) = acc
-            return a1 to (b1 - n).mod(m)
-        }
-
-        /**
-         * x1 = a1 * x0 + b1
-         * x2 = p * x1
-         *
-         * x2 = p * x1 = p * (a1 * x0 + b1) = p*a1 * x0 + p * b1
-         *
-         */
-        override fun dealWithIncrementP(acc: Pair<Long, Long>, p: Int): Pair<Long, Long> {
-            val (a1, b1) = acc
-            return (p*a1).mod(m) to (p*b1).mod(m)
+        override fun dealWithIncrementP(acc: Pair<BigInteger, BigInteger>, n: Int): Pair<BigInteger, BigInteger> {
+            val (a, b) = acc
+            val bn = valueOf(n.toLong())
+            return (bn*a).mod(m) to (bn*b).mod(m)
         }
     }
 
-    fun naiveOneLine(shuffle: String, deck: IntRange): List<Int> {
-        return NaiveShuffler().applyOneLine(shuffle, deck.toList())
-    }
-
-    fun naiveShuffler(deck: IntRange): List<Int> {
-        return NaiveShuffler().shuffle(lines, deck.toList())
-    }
-
-    fun foo() {
-        val a = 9778
-        val b = 1192
-        val deck = (0..10_006).toList()
-        val shuffledDeck = naiveShuffler(0..10_006)
-        for (i in 0..10_006) {
-            assert(a * i + b == shuffledDeck[i])
-        }
-    }
-
-    fun barCheck(deck: List<Int>, a: Int, b: Int): Boolean {
-        for (i in 0..10_006) {
-            val left = (a * i + b) % 10_007
-            val right = MathShuffler(10_007).shuffle(lines, i)
-//            val right = deck[(a * i + b) % 10_007]
-            if (left != right) {
-                println("$i: $left != $right")
-           //     return false
-            }
-        }
-        return true
-    }
-
-    fun bar() {
-        val shuffledDeck = naiveShuffler(0..10_006)
-        for (a in 3190..3190) {
-            for (b in 180..180) {
-                if (barCheck(shuffledDeck, a, b)) {
-                    println("$a $b")
-                }
-            }
-        }
-    }
-
-    override fun part1(): Long {
-        val m = 10007L
-        val index = 2019L
-        // foo()
-        //bar()
-        // return naiveShuffler(0..10_006).indexOf(2019)
-        //return MathShuffler(10_007).shuffle(lines, 2019)
-        val (a, b) = MathShuffler2(m).shuffle(lines, 1L to 0L)
-
+    override fun part1(): Int {
+        val m = 10007
+        val index = 2019
+        val (a, b) = IntShuffler(m).shuffle(lines, 1 to 0)
         return (a * index + b).mod(m)
     }
 
     override fun part2(): BigInteger {
-        val mL = 119315717514047
-        val m = valueOf(mL)
+        val m = valueOf(119315717514047)
         val c = valueOf(101741582076661)
         val x = valueOf(2020L)
-        val (aL, oL) = MathShuffler2(mL).shuffle(lines, 1L to 0L)
-        val a = valueOf(aL)
-        val b = valueOf(oL)
+        val (a, b) = BigIntegerShuffler(m).shuffle(lines, ONE to ZERO)
         val i = a.modInverse(m)
-        val i2 = (i - ONE).modInverse(m)
-        val r1 = x * i.modPow(c, m) % m
-        val r2 = i.modPow(c + ONE, m) - i
-        val r4 = (r2 * i2) % m
-        val t = (r1 - b * r4).mod(m)
-        return t
+        return (x * i.modPow(c, m) - b * ((i.modPow(c + ONE, m) - i) * (i - ONE).modInverse(m))).mod(m)
     }
 }
--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day22Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2019/Day22Test.kt
@@ -7,41 +7,67 @@ import org.junit.jupiter.api.Test
 
 internal class Day22Test : AocTest(Day22::class, loadExample = false) {
 
+    private class NaiveShuffler: Day22.Shuffler<List<Int>>() {
+
+        override fun dealIntoNewStack(acc: List<Int>): List<Int> {
+            return acc.reversed()
+        }
+
+        override fun cutN(acc: List<Int>, n: Int): List<Int> {
+            assert(n != 0)
+            return if (n > 0) {
+                acc.drop(n) + acc.take(n)
+            } else {
+                acc.drop(acc.size + n) + acc.take(acc.size + n)
+            }
+        }
+
+        override fun dealWithIncrementP(acc: List<Int>, n: Int): List<Int> {
+            val nextDeck = acc.toList().toMutableList()
+            acc.indices.forEach { i ->
+                nextDeck[(i * n) % acc.size] = acc[i]
+            }
+            return nextDeck
+        }
+    }
+
+    private fun naiveOneLine(shuffle: String, deck: IntRange): List<Int> {
+        return NaiveShuffler().applyOneLine(shuffle, deck.toList())
+    }
+
+    private fun naiveShuffler(lines: List<String>, deck: IntRange): List<Int> {
+        return NaiveShuffler().shuffle(lines, deck.toList())
+    }
+
     @Test
     fun `can deal into new stack`() {
-        subject as Day22
-        subject.naiveOneLine("deal into new stack", 0..9) shouldBe
+        naiveOneLine("deal into new stack", 0..9) shouldBe
                 listOf(9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
     }
 
     @Test
     fun `can cut 3`() {
-        subject as Day22
-        subject.naiveOneLine("cut 3", 0..9) shouldBe
+        naiveOneLine("cut 3", 0..9) shouldBe
                 listOf(3, 4, 5, 6, 7, 8, 9, 0, 1, 2)
     }
 
     @Test
     fun `can cut -4`() {
-        subject as Day22
-        subject.naiveOneLine("cut -4", 0..9) shouldBe
+        naiveOneLine("cut -4", 0..9) shouldBe
                 listOf(6, 7, 8, 9, 0, 1, 2, 3, 4, 5)
     }
 
     @Test
     fun `can deal with increment p`() {
-        subject as Day22
-        subject.naiveOneLine("deal with increment 3", 0..9) shouldBe
+        naiveOneLine("deal with increment 3", 0..9) shouldBe
                 listOf(0, 7, 4, 1, 8, 5, 2, 9, 6, 3)
     }
 
     @Test
     fun `examples year 2019 day 22 part 1`() {
-        Day22(loadExample(2019, "22a")).naiveShuffler(0..9) shouldBe listOf(0, 3, 6, 9, 2, 5, 8, 1, 4, 7)
-        Day22(loadExample(2019, "22b")).naiveShuffler(0..9) shouldBe listOf(3, 0, 7, 4, 1, 8, 5, 2, 9, 6)
-        Day22(loadExample(2019, "22c")).naiveShuffler(0..9) shouldBe listOf(6, 3, 0, 7, 4, 1, 8, 5, 2, 9)
-        Day22(loadExample(2019, "22d")).naiveShuffler(0..9) shouldBe listOf(9, 2, 5, 8, 1, 4, 7, 0, 3, 6)
+        naiveShuffler(loadExample(2019, "22a"), 0..9) shouldBe listOf(0, 3, 6, 9, 2, 5, 8, 1, 4, 7)
+        naiveShuffler(loadExample(2019, "22b"), 0..9) shouldBe listOf(3, 0, 7, 4, 1, 8, 5, 2, 9, 6)
+        naiveShuffler(loadExample(2019, "22c"), 0..9) shouldBe listOf(6, 3, 0, 7, 4, 1, 8, 5, 2, 9)
+        naiveShuffler(loadExample(2019, "22d"), 0..9) shouldBe listOf(9, 2, 5, 8, 1, 4, 7, 0, 3, 6)
     }
-
-// a b   3190 180
 }
