ef5e7b10a5515a2ae3be9b22c64aef8cd416b71d
Enrico Pilz <kontakt@enricopilz.de>
2024-02-06 20:49:22 +0100

    kt 2019/21 part1

---
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2019/Day21.kt
@@ -0,0 +1,58 @@
+package io.github.ocirne.aoc.year2019
+
+import io.github.ocirne.aoc.AocChallenge
+
+class Day21(val lines: List<String>) : AocChallenge(2019, 21) {
+
+    private fun runProgram(program: IntCodeEmulator2019, print: Boolean=false): Long {
+        while (true) {
+            val rc = program.tick()
+            if (rc == IntCodeEmulator2019.Companion.ReturnCode.STOP) {
+                break
+            }
+            else if (rc == IntCodeEmulator2019.Companion.ReturnCode.NEED_INPUT) {
+                break
+            }
+            else {
+                val lo = program.getLastOutput()
+                if (lo > 255) {
+                    return lo
+                }
+                val c = lo.toInt().toChar()
+                if (print) {
+                    print("$c")
+                }
+            }
+        }
+        return -1
+    }
+
+    private fun runScript(script: String): Long {
+        val program = IntCodeEmulator2019(lines.first())
+        script.forEach { program.addInput(it.code.toLong()) }
+        return runProgram(program)
+    }
+
+    override fun part1(): Long {
+        val script = """
+            NOT T T
+            AND A T
+            AND B T
+            AND C T
+            NOT T J
+            AND D J
+            WALK
+
+        """.trimIndent()
+        return runScript(script)
+    }
+
+    override fun part2(): Long {
+        val script = """
+            ...
+            RUN
+
+        """.trimIndent()
+        return runScript(script)
+    }
+}
