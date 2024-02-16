8d4750273b2bab171d06389b476e2171fcbf3754
Enrico Pilz <kontakt@enricopilz.de>
2024-02-16 19:52:50 +0100

    py 2022/17 part2

--- python/src/aoc/year2022/day17.py
+++ python/src/aoc/year2022/day17.py
@@ -37,7 +37,7 @@ def can_move(chamber, part, p):
 
 def print_chamber(chamber):
     print("----")
-    for y in range(20, -1, -1):
+    for y in range(70, -1, -1):
         for x in range(7):
             if complex(x, y) in chamber:
                 print("#", end="")
@@ -81,8 +81,92 @@ def part2(lines):
     1514285714288
     """
 
+    T = 1000000000000
+
+    print(len(lines[0]))
+    jet_pattern = cycle(lines[0])
+    jet_index = 0
+    chamber = set()
+    height = 0
+    last_part_index = -1
+    last_height = 0
+    for part_index, part in enumerate(islice(cycle(parts), 30)):
+        p = complex(2, height + 3)
+        while True:
+            # jet
+            jet = next(jet_pattern)
+            #            if (part_index % 5) == 0 and (jet_index % len(lines[0]) == 0):
+            jet_index = jet_index + 1
+            if jet == "<" and can_move(chamber, part, p - 1):
+                p -= 1
+            elif jet == ">" and can_move(chamber, part, p + 1):
+                p += 1
+            # falling
+            if can_move(chamber, part, p - 1j):
+                p -= 1j
+            else:
+                break
+        for c in part.coordinates:
+            chamber.add(p + c)
+        height = int(max(c.imag for c in chamber)) + 1
+        if part_index % 1735 == 140:
+            #            if (T % (part_index - last_part_index) == 0):
+            print(
+                "det kenn ick",
+                part_index,
+                jet_index,
+                part_index % 5,
+                jet_index % len(lines[0]),
+                "-->",
+                (part_index - last_part_index),
+                height,
+                "(%d)" % (height - last_height),
+            )
+            last_part_index = part_index
+            last_height = height
+    #        if (part_index % 5, jet_index % len(lines[0])) in seen:
+    #            print('kenn ick', part_index, jet_index, part_index % 5, jet_index % len(lines[0]), '-->',
+    #            (part_index - last_part_index), height, '(%d)' % (height - last_height))
+    #            print(part_index - seen[(part_index % 5, jet_index % len(lines[0]))])
+    #        seen[(part_index % 5, jet_index % len(lines[0]))] = part_index
+
+    # det kenn ick 965 5512 0 32 --> 5 1468 (11)
+    # det kenn ick 970 5539 0 19 --> 5 1475 (7)
+    # det kenn ick 975 5571 0 11 --> 5 1482 (7)
+    # det kenn ick 980 5598 0 38 --> 5 1492 (10)
+    # det kenn ick 985 5625 0 25 --> 5 1498 (6)
+    # det kenn ick 990 5654 0 14 --> 5 1504 (6)
+    # det kenn ick 995 5686 0 6 --> 5 1510 (6)
+
+    print(T % 700)  # 400
+    assert (T - 400) % 35 == 0
+    anzahl = (T - 400) // 35
+    print(anzahl * 53 + 608)
+    print(1514285714288)
+
+    print("--")
+    m = 1735  # 35
+    print(T % m)
+    i = 2711
+    print("i", i)
+    base = 2842  # 209 # 26
+    # r = T % m
+    r = 1875
+    print("r", r)
+    assert (T - r) % m == 0
+    anzahl = (T - r) // m
+    print("anzahl", anzahl)
+    print(anzahl * i + base)
+    print(1514285714288)
+
+    #    print_chamber(chamber)
+    return height, jet_index
+
 
 if __name__ == "__main__":
+    print(part2(load_example(__file__, "17")))
+
     data = load_input(__file__, 2022, "17")
-    print(part1(data))
+
+#    print(part1(data))
 #    print(part2(data))
