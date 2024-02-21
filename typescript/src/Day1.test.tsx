be395567f230da265c33e2316b5af89393a9ae02
Enrico Pilz <kontakt@enricopilz.de>
2024-02-21 22:25:20 +0100

    ts 2015/1

---
+++ typescript/src/Day1.test.tsx
@@ -0,0 +1,45 @@
+
+//n opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
+
+
+
+import Day1 from './Day1';
+
+test('basics', () => {
+    const day1 = new Day1('');
+    expect(day1.part1('(')).toBe(1);
+    expect(day1.part1(')')).toBe(-1);
+});
+
+test('part1', () => {
+    const day1 = new Day1('');
+
+    // (()) and ()() both result in floor 0.
+    expect(day1.part1('(())')).toBe(0);
+    expect(day1.part1('()()')).toBe(0);
+    
+    // ((( and (()(()( both result in floor 3.
+    expect(day1.part1('(((')).toBe(3);
+    expect(day1.part1('(()(()(')).toBe(3);
+
+    // ))((((( also results in floor 3.
+    expect(day1.part1('))(((((')).toBe(3);
+
+    // ()) and ))( both result in floor -1 (the first basement level).
+    expect(day1.part1('())')).toBe(-1);
+    expect(day1.part1('))(')).toBe(-1);
+
+    // ))) and )())()) both result in floor -3.
+    expect(day1.part1(')))')).toBe(-3);
+    expect(day1.part1(')())())')).toBe(-3);
+});
+
+test('part2', () => {
+    const day1 = new Day1('');
+
+    // ) causes him to enter the basement at character position 1. 
+    expect(day1.part2(')')).toBe(1);
+
+    // ()()) causes him to enter the basement at character position 5.
+    expect(day1.part2('()())')).toBe(5);
+});
