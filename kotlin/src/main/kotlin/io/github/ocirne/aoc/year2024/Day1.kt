5886c119b9ff98fb6dae8d500755fd3d43acf137
Enrico Pilz <kontakt@enricopilz.de>
2024-12-01 15:10:30 +0100

    kt 2024/1 refactor

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day1.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day1.kt
@@ -5,22 +5,21 @@ import kotlin.math.abs
 
 class Day1(val lines: List<String>) : AocChallenge(2024, 1) {
 
-    val regex = Regex("\\s+")
+    private val whitespace = Regex("\\s+")
+
+    private fun extractList(index: Int): List<Int> {
+        return lines.map { line -> line.split(whitespace)[index].toInt() }.sorted()
+    }
+
+    private val leftList = extractList(0)
+    private val rightList = extractList(1)
 
     override fun part1(): Int {
-//        println(lines)
-        val firstList = lines.map { line -> line.split(' ').get(0).toInt() }.sorted()
-        val secondList = lines.map { line -> line.split(regex).get(1).toInt() }.sorted()
-//        println(firstList)
-//        println(secondList)
-        return firstList.zip(secondList).sumOf { (f, s) -> abs(f - s) }
+        return leftList.zip(rightList).sumOf { (l, r) -> abs(r - l) }
     }
 
     override fun part2(): Int {
-        val firstList = lines.map { line -> line.split(' ').get(0).toInt() }
-        val secondList = lines.map { line -> line.split(regex).get(1).toInt() }.groupingBy { it }.eachCount()
-        println(firstList)
-        println(secondList)
-        return firstList.sumOf { f -> f * secondList.getOrDefault(f, 0) }
+        val rightCounts = rightList.groupingBy { it }.eachCount()
+        return leftList.sumOf { x -> x * rightCounts.getOrDefault(x, 0) }
     }
 }
