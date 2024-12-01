3c9ab14260c777118ca35e5db36fa37048a2fc89
Enrico Pilz <kontakt@enricopilz.de>
2024-12-01 12:02:31 +0100

    kt 2024/1 part 2

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day1.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day1.kt
@@ -8,15 +8,19 @@ class Day1(val lines: List<String>) : AocChallenge(2024, 1) {
     val regex = Regex("\\s+")
 
     override fun part1(): Int {
-        println(lines)
+//        println(lines)
         val firstList = lines.map { line -> line.split(' ').get(0).toInt() }.sorted()
         val secondList = lines.map { line -> line.split(regex).get(1).toInt() }.sorted()
-        println(firstList)
-        println(secondList)
+//        println(firstList)
+//        println(secondList)
         return firstList.zip(secondList).sumOf { (f, s) -> abs(f - s) }
     }
 
     override fun part2(): Int {
-        return -1
+        val firstList = lines.map { line -> line.split(' ').get(0).toInt() }
+        val secondList = lines.map { line -> line.split(regex).get(1).toInt() }.groupingBy { it }.eachCount()
+        println(firstList)
+        println(secondList)
+        return firstList.sumOf { f -> f * secondList.getOrDefault(f, 0) }
     }
 }
--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2024/Day1Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2024/Day1Test.kt
@@ -1,11 +1,5 @@
 package io.github.ocirne.aoc.year2024
 
 import io.github.ocirne.aoc.AocTest
-import org.junit.jupiter.api.Test
 
-internal class Day1Test: AocTest(Day1::class, expectedPart1 = 11) {
-
-    @Test
-    fun `examples year 2024 day 1 part 2`() {
-    }
-}
+internal class Day1Test: AocTest(Day1::class, expectedPart1 = 11, expectedPart2 = 31)
