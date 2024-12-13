f11b22fb3b8c3382d711397d2143dd691ac50dc9
Enrico Pilz <kontakt@enricopilz.de>
2024-12-13 20:55:16 +0100

    kt 2024/4 wip

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day4.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day4.kt
@@ -4,19 +4,11 @@ import io.github.ocirne.aoc.AocChallenge
 
 class Day4(val lines: List<String>) : AocChallenge(2024, 4) {
 
-    val w = lines.first().length
-    val h = lines.size
-
     override fun part1(): Int {
         return -1
     }
 
     override fun part2(): Int {
-        for (x in 0 until w) {
-            for (y in 0 until h) {
-
-            }
-        }
-        return 1
+        return -1
     }
 }
--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2024/Day4Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2024/Day4Test.kt
@@ -2,4 +2,4 @@ package io.github.ocirne.aoc.year2024
 
 import io.github.ocirne.aoc.AocTest
 
-internal class Day4Test: AocTest(Day4::class, expectedPart1 = 18, expectedPart2 = 9)
+internal class Day4Test: AocTest(Day4::class, expectedPart1 = -1, expectedPart2 = -1)
