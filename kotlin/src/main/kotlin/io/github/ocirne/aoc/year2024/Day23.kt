4b350aafb1f24d8d34de0c6b768dd991f5cfeaf5
Enrico Pilz <kontakt@enricopilz.de>
2024-12-23 06:56:01 +0100

    kt 2024/23

---
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day23.kt
@@ -0,0 +1,60 @@
+package io.github.ocirne.aoc.year2024
+
+import io.github.ocirne.aoc.AocChallenge
+
+class Day23(val lines: List<String>) : AocChallenge(2024, 23) {
+
+    override fun part1(): Int {
+        val pairs = lines.map { it.trim().split('-').toSet() }
+        val nList = pairs.flatten().toSet()
+        val tList = nList.filter { it.startsWith('t') }.toSet()
+        var result = mutableSetOf<Set<String>>()
+        println(tList.size)
+        println(pairs.size)
+        for (t in tList) {
+            for (p in pairs) {
+                if (t in p)
+                    continue
+                val (a, b) = p.toList()
+                if (setOf(a, t) in pairs && setOf(b, t) in pairs) {
+                    result.add(setOf(t, a, b))
+                    println("$t $a $b")
+                }
+            }
+        }
+        return result.size
+    }
+
+    private fun isConnected(allPairs: Set<Set<String>>, t: String, s: Set<String>): Boolean {
+        if (t in s)
+            return false
+        for (a in s) {
+            if (setOf(a, t) !in allPairs) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun allTriples(allPairs: Set<Set<String>>, allSets: Set<Set<String>>): String {
+        println("" + allPairs.size + " " + allSets.size)
+        if (allSets.size == 1) {
+            return allSets.first().sorted().joinToString(",")
+        }
+        val tList = allSets.flatten().toSet()
+        val result = mutableSetOf<Set<String>>()
+        for (t in tList) {
+            for (s in allSets) {
+                if (isConnected(allPairs, t, s)) {
+                    result.add(s + t)
+                }
+            }
+        }
+        return allTriples(allPairs, result)
+    }
+
+    override fun part2(): String {
+        val pairs = lines.map { it.trim().split('-').toSet() }.toSet()
+        return allTriples(pairs, pairs)
+    }
+}
---
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2024/Day23Test.kt
@@ -0,0 +1,5 @@
+package io.github.ocirne.aoc.year2024
+
+import io.github.ocirne.aoc.AocTest
+
+internal class Day23Test: AocTest(Day23::class, expectedPart1 = 7, expectedPart2 = "co,de,ka,ta")
