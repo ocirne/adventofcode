3513996b7d6d20b39d56c41d40b0692fba8200ab
Enrico Pilz <kontakt@enricopilz.de>
2024-12-29 16:50:23 +0100

    kt 2024/22 part2

--- kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day22.kt
+++ kotlin/src/main/kotlin/io/github/ocirne/aoc/year2024/Day22.kt
@@ -1,6 +1,7 @@
 package io.github.ocirne.aoc.year2024
 
 import io.github.ocirne.aoc.AocChallenge
+import kotlin.math.max
 
 class Day22(val lines: List<String>) : AocChallenge(2024, 22) {
 
@@ -21,13 +22,61 @@ class Day22(val lines: List<String>) : AocChallenge(2024, 22) {
         var total = 0L
         for (line in lines) {
             val x = foo(line.toLong())
-            println(x)
+  //          println(x)
             total += x
         }
         return total
     }
 
+    fun bar(x: Long): Sequence<Pair<String, Int>> {
+        return sequence {
+            var last_value = x
+            val queue = mutableListOf<Int>()
+            repeat(4) {
+                val value = evolveNumber(last_value)
+                val delta = value.mod(10) - last_value.mod(10)
+                queue.add(delta)
+                last_value = value
+//                println("[init] delta " + delta + ", queue " + queue)
+            }
+            yield(queue.joinToString(",") to last_value.mod(10))
+            repeat(1996) {
+                val value = evolveNumber(last_value)
+                val delta = value.mod(10) - last_value.mod(10)
+                queue.removeFirst()
+                queue.add(delta)
+                last_value = value
+                yield(queue.joinToString(",") to value.mod(10))
+            }
+        }
+    }
+
     override fun part2(): Int {
-        return -1
+        val totalMap = mutableMapOf<String, Int>()
+        for (line in lines) {
+            val localBest = mutableMapOf<String, Int>()
+            for ((queue, value) in bar(line.toLong())) {
+//                println("" + queue + " -- " + value)
+                if (queue !in localBest) {
+//                    println("" + queue + " " + value)
+//                    localBest.computeIfPresent(queue) { _, b -> max(value, b) }
+//                } else {
+                    localBest.put(queue, value)
+                }
+            }
+//            println("best " + best)
+//            println("foo " + best["-2,1,-1,3"])
+            for ((key, value) in localBest) {
+                if (key in totalMap) {
+                    totalMap[key] = totalMap[key]!! + value
+                } else {
+                    totalMap[key] = value
+                }
+            }
+//            total += x
+        }
+//        println("totalMap " + totalMap)
+//        println("bar " + totalMap["-2,1,-1,3"])
+        return totalMap.values.max()
     }
 }
--- kotlin/src/test/kotlin/io/github/ocirne/aoc/year2024/Day22Test.kt
+++ kotlin/src/test/kotlin/io/github/ocirne/aoc/year2024/Day22Test.kt
@@ -1,5 +1,26 @@
 package io.github.ocirne.aoc.year2024
 
 import io.github.ocirne.aoc.AocTest
+import io.github.ocirne.aoc.loadExample
+import io.kotest.matchers.maps.shouldNotContainKey
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
 
-internal class Day22Test: AocTest(Day22::class, expectedPart1 = 37327623, expectedPart2 = -1)
+internal class Day22Test: AocTest(Day22::class, expectedPart1 = 37327623) {
+
+    @Test
+    fun `examples year 2024 day 22 part 2`() {
+        val subject = Day22(loadExample(2024, "22b"))
+        subject.part2() shouldBe 23
+    }
+
+    @Test
+    fun `examples year 2024 day 22 part 2 bar`() {
+        subject as Day22
+        val key = "-2,1,-1,3"
+        subject.bar(1).toMap()[key] shouldBe 7
+        subject.bar(2).toMap()[key] shouldBe 7
+        subject.bar(3).toMap() shouldNotContainKey key
+        subject.bar(2024).toMap()[key] shouldBe 9
+    }
+}
