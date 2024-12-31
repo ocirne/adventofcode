624e7d24e5570d394a5ccb7528a237a8252fa57c
Enrico Pilz <kontakt@enricopilz.de>
2024-12-31 10:10:19 +0100

    py 2024/21 refactor

--- python/src/aoc/year2024/day21.py
+++ python/src/aoc/year2024/day21.py
@@ -1,57 +1,58 @@
-from functools import lru_cache
-
 from aoc.util import load_input, load_example
 
+from collections import namedtuple
+from functools import lru_cache
 from itertools import permutations
 
-positionsNumericKeypad = {
-    '1' : (0, 2),
-    '2' : (1, 2),
-    '3' : (2, 2),
-    '4' : (0, 1),
-    '5' : (1, 1),
-    '6' : (2, 1),
-    '7' : (0, 0),
-    '8' : (1, 0),
-    '9' : (2, 0),
-    '0' : (1, 3),
-    'A' : (2, 3),
+Position = namedtuple('Position', ['x', 'y'])
+Direction = namedtuple('Direction', ['dx', 'dy'])
+
+
+positions_numeric_keypad = {
+    '1': Position(0, 2),
+    '2': Position(1, 2),
+    '3': Position(2, 2),
+    '4': Position(0, 1),
+    '5': Position(1, 1),
+    '6': Position(2, 1),
+    '7': Position(0, 0),
+    '8': Position(1, 0),
+    '9': Position(2, 0),
+    '0': Position(1, 3),
+    'A': Position(2, 3),
 }
 
-positionsDirectionalKeypad = {
-    '^' : (1, 0),
-    'A' : (2, 0),
-    '<' : (0, 1),
-    'v' : (1, 1),
-    '>' : (2, 1),
+positions_directional_keypad = {
+    '^': Position(1, 0),
+    'A': Position(2, 0),
+    '<': Position(0, 1),
+    'v': Position(1, 1),
+    '>': Position(2, 1),
 }
 
 NSWE = {
-    '<': (-1, 0),
-    '>': (1, 0),
-    '^': (0, -1),
-    'v': (0, 1),
+    '<': Direction(-1, 0),
+    '>': Direction(1, 0),
+    '^': Direction(0, -1),
+    'v': Direction(0, 1),
 }
 
-def allowedNumerical(pSrc, pTgt, move):
-    px, py = pSrc
+
+def allowed(keypad, p_src, move):
+    px, py = p_src
     for m in move:
-        dx, dy = NSWE[m]
-        px += dx
-        py += dy
-        if (px, py) not in positionsNumericKeypad.values():
+        px += NSWE[m].dx
+        py += NSWE[m].dy
+        if (px, py) not in keypad.values():
             return False
-    if (px, py) != pTgt:
-        raise
     return True
 
-## Konkrete Angebote, um von src nach tgt zu kommen
-def numericKeyboard(srcChar, tgtChar):
-    pSrc = positionsNumericKeypad[srcChar]
-    pTgt = positionsNumericKeypad[tgtChar]
-    dx = pTgt[0] - pSrc[0]
-    dy = pTgt[1] - pSrc[1]
-#    print(srcChar, pSrc, tgtChar, pTgt, dx, dy)
+
+def moves_on_keypad(keypad, src_char, tgt_char):
+    p_src = keypad[src_char]
+    p_tgt = keypad[tgt_char]
+    dx = p_tgt.x - p_src.x
+    dy = p_tgt.y - p_src.y
     moves = []
     if dx < 0:
         moves.extend(-dx * ['<'])
@@ -61,83 +62,35 @@ def numericKeyboard(srcChar, tgtChar):
         moves.extend(-dy * ['^'])
     elif dy > 0:
         moves.extend(dy * ['v'])
-#    print("n moves",  moves)
-    angebote = set(permutations(moves))
-#    print("angebote", angebote)
-    return (a for a in angebote if allowedNumerical(pSrc, pTgt, a))
+    return (''.join(a) for a in set(permutations(moves)) if allowed(keypad, p_src, a))
 
 
-def allowedDirectional(pSrc, pTgt, move):
-    px, py = pSrc
-    for m in move:
-        dx, dy = NSWE[m]
-        px += dx
-        py += dy
-        if (px, py) not in positionsDirectionalKeypad.values():
-            return False
-    if (px, py) != pTgt:
-        raise
-    return True
-
-## Anzahl der Moves, um von src nach tgt zu kommen */
-def directionalKeyboard(srcChar, tgtChar):
-    pSrc = positionsDirectionalKeypad[srcChar]
-    pTgt = positionsDirectionalKeypad[tgtChar]
-    dx = pTgt[0] - pSrc[0]
-    dy = pTgt[1] - pSrc[1]
-    moves = []
-    if dx < 0:
-        moves.extend(-dx * ['<'])
-    elif dx > 0:
-        moves.extend(dx * ['>'])
-    if dy < 0:
-        moves.extend(-dy * ['^'])
-    elif dy > 0:
-        moves.extend(dy * ['v'])
- #   print("d moves", moves)
-    return (a for a in set(permutations(moves)) if allowedDirectional(pSrc, pTgt, a))
+def count_moves_keypad(depth, keypad_sequence, f):
+    return sum(f(depth, s, t) for s, t in zip(keypad_sequence, keypad_sequence[1:]))
 
 
 @lru_cache(maxsize=None)
-def bestDirectional(depth, s, t):
-    bestSize = 10000000000000000000000000000
-    for angebot in directionalKeyboard(s, t):
-        if depth == 0:
-            nextLevelSize = len(angebot) + 1
-        else:
-            nextLevelSize = countMovesDirectional1(depth-1, ['A'] + list(angebot) + ['A'])
-        if bestSize > nextLevelSize:
-            bestSize = nextLevelSize
-    return bestSize
+def best_directional(depth, s, t):
+    if depth == 0:
+        return len(next(moves_on_keypad(positions_directional_keypad, s, t))) + 1
+    return min(count_moves_keypad(depth-1, 'A' + angebot + 'A', best_directional)
+               for angebot in moves_on_keypad(positions_directional_keypad, s, t))
 
-
-def countMovesDirectional1(depth, foo):
-    result = 0
-    for s, t in zip(foo, foo[1:]):
-        result += bestDirectional(depth, s, t)
-    return result
 
 @lru_cache(maxsize=None)
-def bestNumerical(depth, s, t):
-    bestSize = 100000000000000000000000000000
-    for angebot in numericKeyboard(s, t):
-        nextLevelSize = countMovesDirectional1(depth-1, ['A'] + list(angebot) + ['A'])
-        if bestSize > nextLevelSize:
-            bestSize = nextLevelSize
-    return bestSize
-
-def countMovesNumeric(depth, foo):
-    result = 0
-    for s, t in zip(foo, foo[1:]):
-        result += bestNumerical(depth, s, t)
-    return result
+def best_numerical(depth, s, t):
+    return min(count_moves_keypad(depth-1, 'A' + angebot + 'A', best_directional)
+               for angebot in moves_on_keypad(positions_numeric_keypad, s, t))
 
 
-def foo(s):
-    result = countMovesNumeric(2, ['A'] + s)
-    print(''.join(result))
-    return len(result)
+def shortest_button_sequence(line, depth):
+    length = count_moves_keypad(depth, 'A' + line, best_numerical)
+    value = int(line[:-1])
+    return length * value
 
+
+def sum_all_button_sequences(lines, depth):
+    return sum(shortest_button_sequence(line, depth) for line in lines)
 
 
 def part1(lines):
@@ -145,27 +98,17 @@ def part1(lines):
     >>> part1(load_example(__file__, "21"))
     126384
     """
-    total = 0
-    for line in lines:
-        length = foo(list(line))
-        value = int(line[:-1])
-        print("line", line, "length", length, "value", value)
-        total += length * value
-    return total
+    return sum_all_button_sequences(lines, 2)
 
 
 def part2(lines):
-    total = 0
-    for line in lines:
-        length = countMovesNumeric(25, ['A'] + list(line))
-        value = int(line[:-1])
-        print("line", line, "length", length, "value", value)
-        total += length * value
-    return total
+    return sum_all_button_sequences(lines, 25)
+
 
 if __name__ == "__main__":
     data = load_input(__file__, 2024, "21")
-    #data = load_example(__file__, "21")
-    #print(part1(data))
+    print(part1(data))
+    print(134120)
+    print("-----")
     print(part2(data))
-    print('1757129124022 (too low)')
+    print(167389793580400)
