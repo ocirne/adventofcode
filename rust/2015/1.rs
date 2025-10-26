d7a653d0e6aafd224dceb32db8bce155d2af75b7
Enrico Pilz <kontakt@enricopilz.de>
2025-10-26 20:38:17 +0100

    rs 2015/2

--- rust/2015/1.rs
+++ rust/2015/1.rs
@@ -6,19 +6,38 @@ fn main() {
     println!("Hello");
 
     let data = fs::read_to_string(filename).expect("Should be able to read file");
-    let mut i = 0;
+    let mut floor = 0;
     for c in data.chars() {
         match c {
             '(' => {
-                println!("open");
-                i += 1;
+                //println!("open");
+                floor += 1;
             }
             ')' => {
-                println!("close");
-                i -= 1;
+                //println!("close");
+                floor -= 1;
             }
             other => {}
         }
     }
-    println!("{i}");
+    println!("{floor}");
+
+    let mut floor = 0;
+    for (index, c) in data.chars().enumerate() {
+        match c {
+            '(' => {
+                println!("open");
+                floor += 1;
+            }
+            ')' => {
+                println!("close");
+                floor -= 1;
+            }
+            other => {}
+        }
+        if floor < 0 {
+            println!("{}", index + 1);
+            break;
+        }
+    }
 }
