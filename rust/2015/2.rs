ca29dd2f20314f18eb42c29c4763830abb993f81
Enrico Pilz <kontakt@enricopilz.de>
2025-10-26 22:26:41 +0100

    rs 2015/2 refactor

--- rust/2015/2.rs
+++ rust/2015/2.rs
@@ -1,23 +1,25 @@
 use std::fs;
 
-fn part1(lines: &Vec<&str>) -> usize {
-    let mut total = 0;
-    for line in lines {
-        if line.is_empty() {
-            continue;
-        }
-        let mut n: Vec<usize> = line
-            .split('x')
-            .map(|s| s.parse::<usize>().unwrap())
-            .collect();
-        n.sort();
-        let (l, w, h) = (n[0], n[1], n[2]);
-        total += 3 * l * w + 2 * l * h + 2 * w * h;
-    }
-    total
+struct Dimensions {
+    l: usize,
+    w: usize,
+    h: usize,
 }
 
-fn part2(lines: &Vec<&str>) -> usize {
+impl Dimensions {
+    fn part1(&self) -> usize {
+        3 * self.l * self.w + 2 * self.l * self.h + 2 * self.w * self.h
+    }
+
+    fn part2(&self) -> usize {
+        2 * self.l + 2 * self.w + self.l * self.w * self.h
+    }
+}
+
+fn solve<F>(lines: &Vec<&str>, f: F) -> usize
+where
+    F: Fn(Dimensions) -> usize,
+{
     let mut total = 0;
     for line in lines {
         if line.is_empty() {
@@ -28,8 +30,12 @@ fn part2(lines: &Vec<&str>) -> usize {
             .map(|s| s.parse::<usize>().unwrap())
             .collect();
         n.sort();
-        let (l, w, h) = (n[0], n[1], n[2]);
-        total += 2 * l + 2 * w + l * w * h;
+        let d = Dimensions {
+            l: n[0],
+            w: n[1],
+            h: n[2],
+        };
+        total += f(d);
     }
     total
 }
@@ -38,6 +44,6 @@ fn main() {
     const FILENAME: &str = "../../../adventofcode-input/resources/2015/2/input";
     let data = fs::read_to_string(FILENAME).expect("file error");
     let lines = data.split('\n').collect();
-    println!("{}", part1(&lines));
-    println!("{}", part2(&lines));
+    println!("{}", solve(&lines, |d| d.part1()));
+    println!("{}", solve(&lines, |d| d.part2()));
 }
