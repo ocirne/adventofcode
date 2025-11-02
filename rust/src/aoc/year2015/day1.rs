ace261353ccf190c9fd85b3ace351ade3f1a73b7
Enrico Pilz <kontakt@enricopilz.de>
2025-11-02 18:56:37 +0100

    rs 2015/1 add module tests

--- rust/src/aoc/year2015/day1.rs
+++ rust/src/aoc/year2015/day1.rs
@@ -24,3 +24,34 @@ pub fn part1(data: &str) -> isize {
 pub fn part2(data: &str) -> isize {
     solve(&data, true)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tests_part1() {
+        // (()) and ()() both result in floor 0
+        assert_eq!(part1("(())"), 0);
+        assert_eq!(part1("()()"), 0);
+        // ((( and (()(()( both result in floor 3
+        assert_eq!(part1("((("), 3);
+        assert_eq!(part1("(()(()("), 3);
+        // ))((((( also results in floor 3
+        assert_eq!(part1("))((((("), 3);
+        // ()) and ))( both result in floor -1 (the first basement level)
+        assert_eq!(part1("())"), -1);
+        assert_eq!(part1("))("), -1);
+        // ))) and )())()) both result in floor -3
+        assert_eq!(part1(")))"), -3);
+        assert_eq!(part1(")())())"), -3);
+    }
+
+    #[test]
+    fn tests_parts() {
+        // ) causes him to enter the basement at character position 1
+        assert_eq!(part2(")"), 1);
+        // ()()) causes him to enter the basement at character position 5
+        assert_eq!(part2("()())"), 5);
+    }
+}
