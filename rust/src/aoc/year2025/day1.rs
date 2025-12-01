083746c6eca7bf9b29be5c071922212f3635e8c6
Enrico Pilz <kontakt@enricopilz.de>
2025-12-01 20:22:37 +0100

    rs 2025/1 part2

--- rust/src/aoc/year2025/day1.rs
+++ rust/src/aoc/year2025/day1.rs
@@ -22,17 +22,37 @@ pub fn part1(data: &str) -> usize {
 }
 
 pub fn part2(data: &str) -> isize {
-    -1
+    let mut result = 0;
+    let mut p = 50;
+    for line in data.lines() {
+        if line.starts_with("L") {
+            let d = line.strip_prefix("L").unwrap().parse::<isize>().unwrap();
+            for i in 0..d {
+                p -= 1;
+                p %= 100;
+                if p == 0 {
+                    result += 1;
+                }
+            }
+        } else if line.starts_with("R") {
+            let d = line.strip_prefix("R").unwrap().parse::<isize>().unwrap();
+            for i in 0..d {
+                p += 1;
+                p %= 100;
+                if p == 0 {
+                    result += 1;
+                }
+            }
+        }
+    }
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-
-    #[test]
-    fn tests_part1() {
-        const INPUT: &str = "L68
+    const INPUT: &str = "L68
 L30
 R48
 L5
@@ -43,10 +63,14 @@ L99
 R14
 L82
 ";
+
+    #[test]
+    fn tests_part1() {
         assert_eq!(part1(INPUT), 3);
     }
 
     #[test]
     fn tests_part2() {
+        assert_eq!(part2(INPUT), 6);
     }
 }
