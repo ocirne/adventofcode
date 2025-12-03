6c8359f463de4e014261a305fdc7dd1ca080deb5
Enrico Pilz <kontakt@enricopilz.de>
2025-12-03 19:58:28 +0100

    rs 2025/3 refactor

--- rust/src/aoc/year2025/day3.rs
+++ rust/src/aoc/year2025/day3.rs
@@ -1,45 +1,32 @@
-use std::ops::Index;
-
 pub fn yd() -> (usize, usize) {
     (2025, 3)
 }
 
-fn battery_two(line: &str) -> usize {
-    let digits: Vec<u32> = line.chars().map(|c| c.to_digit(10).unwrap()).collect();
-    println!("{digits:?}");
-    let r1 = &digits[..(digits.len() - 1)];
-    let d1 = r1.iter().max().unwrap();
-    println!("d1 {d1}");
-    let i1 = digits.iter().position(|x| x == d1).unwrap();
-    println!("i1 {i1}");
-    let r2 = &digits[(i1+1)..];
-    let d2 = r2.iter().max().unwrap();
-    println!("d2 {d2}");
-    (d1 * 10 + d2) as usize
-}
-
-fn rec(digits: &[u32], r: usize, acc: usize) -> usize {
-    println!("{digits:?} {r}");
+fn rec(digits: &[usize], r: usize, acc: usize) -> usize {
     if r == 0 {
         return acc
     }
     let range = &digits[..(digits.len() - r + 1)];
-    let d = range.iter().max().unwrap();
-    let i = digits.iter().position(|x| x == d).unwrap();
-    rec(&digits[(i+1)..], r - 1, acc * 10 + (*d as usize))
+    let digit = range.iter().max().unwrap();
+    let index = digits.iter().position(|x| x == digit).unwrap();
+    rec(&digits[(index+1)..], r - 1, acc * 10 + digit)
 }
 
-fn battery_twelve(line: &str) -> usize {
-    let digits: Vec<u32> = line.chars().map(|c| c.to_digit(10).unwrap()).collect();
-    rec(digits.as_slice(), 12, 0)
+fn battery(line: &str, n: usize) -> usize {
+    let digits: Vec<usize> = line.chars().map(|c| c.to_digit(10).unwrap() as usize).collect();
+    rec(digits.as_slice(), n, 0)
+}
+
+fn solve(data: &str, n: usize) -> usize {
+    data.trim().lines().map(|line| battery(line, n)).sum()
 }
 
 pub fn part1(data: &str) -> usize {
-    data.trim().lines().map(battery_two).sum()
+    solve(data, 2)
 }
 
 pub fn part2(data: &str) -> usize {
-    data.trim().lines().map(battery_twelve).sum()
+    solve(data, 12)
 }
 
 #[cfg(test)]
@@ -48,17 +35,17 @@ mod tests {
 
     #[test]
     fn tests_part1() {
-        assert_eq!(battery_two("987654321111111"), 98);
-        assert_eq!(battery_two("811111111111119"), 89);
-        assert_eq!(battery_two("234234234234278"), 78);
-        assert_eq!(battery_two("818181911112111"), 92);
+        assert_eq!(battery("987654321111111", 2), 98);
+        assert_eq!(battery("811111111111119", 2), 89);
+        assert_eq!(battery("234234234234278", 2), 78);
+        assert_eq!(battery("818181911112111", 2), 92);
     }
 
     #[test]
     fn tests_part2() {
-        assert_eq!(battery_twelve("987654321111111"), 987654321111);
-        assert_eq!(battery_twelve("811111111111119"), 811111111119);
-        assert_eq!(battery_twelve("234234234234278"), 434234234278);
-        assert_eq!(battery_twelve("818181911112111"), 888911112111);
+        assert_eq!(battery("987654321111111", 12), 987654321111);
+        assert_eq!(battery("811111111111119", 12), 811111111119);
+        assert_eq!(battery("234234234234278", 12), 434234234278);
+        assert_eq!(battery("818181911112111", 12), 888911112111);
     }
 }
